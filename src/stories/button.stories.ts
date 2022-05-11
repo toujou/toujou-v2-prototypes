ee6f707e932c1453ee9037a53898b19ec219db5e
Luis Pato <lpato@dfau.de>
2022-05-11 11:21:11 +0200

    feat: Add controls to the button element

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -2,11 +2,45 @@ import { Meta } from '@storybook/web-components';
 
 export default {
     title: 'Toujou/Button',
+    argTypes: {
+        buttonType: {
+            name: 'Button type (accepted: "primary", "secondary", "font")',
+            type: 'string',
+        },
+        isShadow: {
+            name: 'Shadow button',
+            type: 'boolean',
+        },
+        isExpanded: {
+            name: 'Expanded button',
+            type: 'boolean',
+        },
+        isDisabled: {
+            name: 'Disabled',
+            type: 'boolean',
+            required: true,
+        },
+        buttonText: {
+            name: 'Button text',
+            type: 'string',
+            required: true,
+        },
+    }
 } as Meta;
 
-export const Primary = () => `<button class="button button--primary">Primary button</button>`
-export const Secondary = () => `<button class="button button--secondary">Secondary button</button>`
-export const Font = () => `<button class="button button--font">Font button</button>`
-export const PrimaryShadow = () => `<button class="button button--primary button--shadow">Primary shadow button</button>`
-export const SecondaryShadow = () => `<button class="button button--secondary button--shadow">Secondary shadow button</button>`
-export const FontShadow = () => `<button class="button button--secondary button--shadow">Font shadow button</button>`
+
+const Template = (args) => {
+    const buttonClasses = `button button--${args.buttonType} ${args.isShadow ? 'button--shadow' : ''} ${args.isExpanded ? 'button--expanded' : ''}`
+
+    return `<button class="${buttonClasses}" ${args.isDisabled ? 'disabled' : ''}>${args.buttonText}</button>`;
+}
+
+export const Button = Template.bind({});
+Button.args = {
+    buttonType: 'primary',
+    isShadow: false,
+    isExpanded: false,
+    isDisabled: false,
+    buttonText: "Nice button",
+}
+
