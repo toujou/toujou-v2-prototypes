7314a20cfe31583540ef06a7936cd96c6dbd9ec9
Luis Pato <lpato@dfau.de>
2022-05-30 06:57:02 +0200

    feat: Add basic button styles and option to change button variants Card: https://app.clickup.com/t/2g3w288

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -1,6 +1,7 @@
 import { Meta } from '@storybook/web-components';
 // @ts-ignore
 import { withXD } from "storybook-addon-xd-designs";
+import {ToujouTopbar} from "../toujou-topbar/toujou-topbar.stories";
 
 export default {
     title: 'COMPONENTS/Button',
@@ -10,13 +11,34 @@ export default {
             artboardUrl: 'https://xd.adobe.com/view/c5b5ae33-8b35-4536-be9f-a39f5a3c447f-90e6/screen/94662024-4698-4543-9535-08e86b001d5d/Desktop',
         }
     },
-    argTypes: {}
+    argTypes: {
+        buttonVariant: {
+            table: {
+                category: "Button Settings",
+                defaultValue: { summary: 'primary' },
+            },
+            name: 'Button variant',
+            description: "Set the button's variant",
+            options: ['primary', 'secondary', 'font'],
+            control: { type: 'radio' },
+            defaultValue: ['primary'],
+            required: true,
+        },
+    }
 } as Meta;
 
 const Template = (args) => {
+    let buttonClasses = 'button';
+    buttonClasses += ` button--${args.buttonVariant}`;
+
     return `       
-        <button class="button">Test</button>
+        <button class="${buttonClasses}">Test</button>
     `
 };
 
 export const Button = Template.bind({});
+
+// @ts-ignore
+ToujouTopbar.args = {
+    buttonVariant: 'primary',
+}
