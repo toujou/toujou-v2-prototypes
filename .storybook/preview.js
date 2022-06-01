6ca7846821798d23201ca44aa0c849d393a5543f
Luis Pato <lpato@dfau.de>
2022-06-01 11:53:21 +0200

    feat: Add stories and styles for the text block, text block thirds and text block with media inside and beside Card: https://app.clickup.com/t/2je5ekg

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,30 @@ import "../src/components/toujou-breadcrumb/toujou-breadcrumb";
 import "../src/styles/StorybookStyles.css";
 import { badgeCustomConfig } from "./badgeCustomConfig.js";
 
+// const toujouViewports = {
+//   kindleFire2: {
+//     name: 'Kindle Fire 2',
+//     styles: {
+//       width: '600px',
+//       height: '963px',
+//     },
+//   },
+//   kindleFireHD: {
+//     name: 'Kindle Fire HD',
+//     styles: {
+//       width: '533px',
+//       height: '801px',
+//     },
+//   },
+//   toujouMobile: {
+//     name: 'Toujou Mobile',
+//     styles: {
+//       width: '640px',
+//       height: '801px',
+//     },
+//   },
+// }
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -13,5 +37,8 @@ export const parameters = {
     expanded: true,
   },
   badgesConfig: badgeCustomConfig,
+  // viewport: {
+  //   viewports: toujouViewports
+  // },
   // layout: 'fullscreen' // This removes that padding on the storybook canvas
 }
---
+++ src/components/text-elements/01.text-blocks.stories.ts
@@ -0,0 +1,96 @@
+import {Meta} from '@storybook/web-components';
+import { withXD } from "storybook-addon-xd-designs";
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../.storybook/badgeCustomConfig.js'
+
+export default {
+    title: 'COMPONENTS/Text Elements',
+    decorators: [withXD],
+    parameters: {
+        design: {
+            artboardUrl: 'https://xd.adobe.com/view/57945f95-9455-4031-a7b6-76e6a7ec74a6-02c7/screen/edaf3b92-9a40-4082-9504-de34d306f6cf/Desktop',
+        },
+        badges: [TOUJOU_BADGES.TESTING]
+    },
+} as Meta;
+
+
+const Template = () => {
+    return `
+        <div class="text-block">
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h1>Left aligned H1 headline</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="text-block text-block--2">
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="text-block text-block--3">
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="text-block text-block--4">
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="text-block__column">
+                <div class="text-block__content">
+                    <h3>Left aligned H3 headline</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+export const TextBlocks = Template.bind({});
+
+
