f8591cbaf7d927ada40dbfa9197068b4b581c946
Luis Pato <lpato@dfau.de>
2022-06-02 09:18:59 +0200

    feat: Add story and styles for the textarea element Card: https://app.clickup.com/t/2je6p7n

--- src/components/forms/05.textarea.stories.ts
+++ src/components/forms/05.textarea.stories.ts
@@ -10,12 +10,30 @@ export default {
         design: {
             artboardUrl: 'https://xd.adobe.com/view/57945f95-9455-4031-a7b6-76e6a7ec74a6-02c7/screen/9b61f9cb-3a4b-4df5-9706-2991633fc53e/Desktop',
         },
-        badges: [TOUJOU_BADGES.PROGRESS]
+        badges: [TOUJOU_BADGES.TESTING]
     },
-    argTypes: {}
+    argTypes: {
+        state: {
+            table: {
+                category: "Inputs settings",
+                defaultValue: { summary: 'default' },
+            },
+            name: 'Input state',
+            description: "Set the visible input state",
+            options: ['default', 'disabled', 'success', 'error'],
+            control: { type: 'radio' },
+            defaultValue: ['default'],
+            required: true,
+        },
+    }
 } as Meta;
 
-const Template = () => {
+const Template = (args) => {
+    const disabledAttribute = args.state === 'disabled' ? 'disabled' : '';
+    const inputGroupDisabledClass = args.state === 'disabled' ? 'input-group--disabled' : '';
+    const inputGroupErrorClass = args.state === 'error' ? 'input-group--has-error' : '';
+    const inputGroupSuccessClass = args.state === 'success' ? 'input-group--has-success' : '';
+
     return `
         <form
             enctype="multipart/form-data"
@@ -25,7 +43,7 @@ const Template = () => {
             action="#"
             novalidate="true">
 	        
-	        <div class="input-group ">
+	        <div class="input-group input-group--type-textarea ${inputGroupDisabledClass} ${inputGroupErrorClass} ${inputGroupSuccessClass}">
 				<label class="input-label" for="testform-1000091-textarea-1">Textarea</label>
 				<span class="input__description">This is a description</span>
                 <textarea
@@ -33,8 +51,10 @@ const Template = () => {
                     data-pristine-required-message="The given subject was empty."
                     class="xxlarge form-control input input--textarea"
                     id="testform-1000091-textarea-1"
-                    name="tx_form_formframework[testform-1000091][textarea-1]">
+                    name="tx_form_formframework[testform-1000091][textarea-1]"
+                    ${disabledAttribute}>
                 </textarea>
+                <span class="pristine-error form__error">The given subject was empty.</span>
 	        </div>
         </form>
     `;
@@ -43,4 +63,6 @@ const Template = () => {
 export const TextareaInputs = Template.bind({});
 
 // @ts-ignore
-TextareaInputs.args = {}
+TextareaInputs.args = {
+    state: 'default'
+}
