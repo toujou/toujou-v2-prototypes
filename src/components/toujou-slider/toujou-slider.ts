65d3474d91484f89505fc9459824d6d61359dfcf
Luis Pato <lpato@dfau.de>
2022-06-15 07:39:19 +0200

    feat: Add nvmrc file, add toujou slider initialization, start adding slider autoplay and counter options Card: https://app.clickup.com/t/2kpd1pn

--- src/components/toujou-slider/toujou-slider.ts
+++ src/components/toujou-slider/toujou-slider.ts
@@ -1,5 +1,5 @@
-import { html, LitElement } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { LitElement, css, unsafeCSS } from 'lit'
+import { customElement, property, state } from 'lit/decorators.js'
 // @ts-ignore
 import Splide from "@splidejs/splide";
 
@@ -9,22 +9,102 @@ import splideSliderStyles from "@splidejs/splide/dist/css/splide.min.css";
 @customElement('toujou-slider')
 export class ToujouSlider extends LitElement {
 
-    // public static styles = [ toujouSliderStyles ];
+    // public static styles = [ splideSliderStyles ];
+    public static styles = css`${unsafeCSS(splideSliderStyles)}`
+
+    @property({ type: Object })
+    splideContainer: Element | null = null;
+
+    @property({ type: Boolean, attribute: 'show-counter' })
+    showCounter: Boolean = false;
+
+    @property({ type: Boolean, attribute: 'auto-play' })
+    autoplay: Boolean = false;
+
+    @property()
+    private _currentIndexCounterElement: HTMLElement | null = null;
+
+    @property()
+    private _totalSlidesCounterElement: HTMLElement | null = null;
+
+    @state()
+    private _numberOfSlides: Number = 0;
+
+    @state()
+    private _activeSlideIndex: Number = 1;
+
+    @property()
+    private splideSlider: any;
 
     constructor() {
         super();
-    }
 
-    render() {
-        return html`
-            <style>${splideSliderStyles}</style>
-        `
+        this._handlePaginationUpdate = this._handlePaginationUpdate.bind(this);
+        this._handleSliderMount = this._handleSliderMount.bind(this);
     }
 
     createRenderRoot() { return this;}
 
     firstUpdated() {
-        new Splide(this).mount();
+        this.splideContainer = this.querySelector('.splide') || null;
+
+        if (!this.splideContainer) {
+            console.error("TOUJOU: Could not get necessary elements to initialize the slider");
+            return;
+        } else {
+            this._initSlider();
+        }
+    }
+
+    _initSlider() {
+        const sliderOptions = {
+            classes: {
+                pagination: 'splide__pagination toujou-slider-bullets',
+                page: 'splide__pagination__page toujou-slider-bullets__bullet',
+            },
+            type: 'fade',
+            rewind: true,
+            autoplay: this.autoplay,
+            pauseOnHover: this.autoplay,
+            pauseOnFocus: this.autoplay,
+        }
+
+        // @ts-ignore
+        this.splideSlider = new Splide(this.splideContainer, sliderOptions);
+
+        this.splideSlider!.on( 'mounted', this._handleSliderMount);
+        this.splideSlider!.on( 'moved', this._handlePaginationUpdate);
+
+        this.splideSlider!.mount();
+        console.log(this.splideSlider);
+    }
+
+    _handleSliderMount(event: Event) {
+        this._getCounterInfo();
+        this._initiCounter();
+
+        console.log('====>', event);
+    }
+
+    _initiCounter() {
+        this._numberOfSlides = this.splideSlider.Components.Elements.slides.length;
+        this._updateSliderCounter();
+    }
+
+    _getCounterInfo() {
+        this._currentIndexCounterElement = this.querySelector('.toujou-slider-count__current');
+        this._totalSlidesCounterElement = this.querySelector('.toujou-slider-count__total');
+    }
+
+
+    _handlePaginationUpdate(event: number) {
+        this._activeSlideIndex = event + 1;
+        this._updateSliderCounter()
+    }
+
+    _updateSliderCounter() {
+        this._currentIndexCounterElement!.textContent = String(this._activeSlideIndex);
+        this._totalSlidesCounterElement!.textContent = String(this._numberOfSlides);
     }
 }
 
--- src/stories/02. elements/toujou-slider/toujou-slider.stories.ts
+++ src/stories/02. elements/toujou-slider/toujou-slider.stories.ts
@@ -1,4 +1,4 @@
-import { Meta } from '@storybook/web-components';
+import { Story, Meta } from '@storybook/web-components';
 import { withXD } from "storybook-addon-xd-designs";
 // @ts-ignore
 import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
@@ -10,7 +10,7 @@ export default {
     decorators: [withXD],
     parameters: {
         design: {
-            artboardUrl: 'https://xd.adobe.com/view/3d1d0926-79e2-4ae4-aa1c-162dcd451a3a-684e/Desktop',
+            artboardUrl: 'https://xd.adobe.com/view/3d1d0926-79e2-4ae4-aa1c-162dcd451a3a-684e/screen/61e6af1e-563f-4ce8-92ba-ac3b191c5f0c/Desktop',
         },
         badges: [TOUJOU_BADGES.TESTING],
         docs: {
@@ -41,41 +41,110 @@ export default {
             defaultValue: 'false',
             required: true,
         },
+        showCount: {
+            table: {
+                category: "Slider settings",
+                defaultValue: { summary: true },
+            },
+            name: 'Show counter',
+            description: "Show the counter element",
+            control: { type: 'boolean' },
+            defaultValue: true,
+            required: true,
+        },
+        autoplay: {
+            table: {
+                category: "Slider settings",
+                defaultValue: { summary: 'false' },
+            },
+            name: 'autoplay',
+            description: "Slide automatically",
+            control: { type: 'boolean' },
+            defaultValue: 'false',
+            required: true,
+        },
+        autoplayInterval: {
+            table: {
+                category: "Slider settings",
+                defaultValue: { summary: 6 },
+            },
+            name: 'autoplay interval',
+            description: "Interval for the auto slide function",
+            control: { type: 'number' },
+            defaultValue: 6,
+            required: true,
+        },
     }
 } as Meta;
 
+interface ToujouSliderStoryProps {
+    designColor: string;
+    showCount: boolean;
+    isFullwidth: boolean;
+    autoplay: boolean;
+    autoplayInterval: number;
+}
 
-const Template = (args) => {
+const Template: Story<ToujouSliderStoryProps> = (args: ToujouSliderStoryProps) => {
     return `       
-        <toujou-slider class="splide toujou-slider" aria-label="Splide Basic HTML Example!!" slider-design="${args.designColor}" ${args.isFullwidth ? 'slider-fullwidth' : ''}>
-            <div class="splide__arrows">
-                <button class="splide__arrow splide__arrow--prev toujou-slider-control toujou-slider-control--prev" aria-label="Previous slide">
-                    <toujou-icon class="toujou-icon toujou-slider-control__icon toujou-slider-control__icon--prev" icon-name="arrow-left"></toujou-icon>
-                </button>
-                <button class="splide__arrow splide__arrow--next toujou-slider-control toujou-slider-control--next" aria-label="Next slide">
-                    <toujou-icon class="toujou-icon toujou-slider-control__icon toujou-slider-control__icon--next" icon-name="arrow-right"></toujou-icon>
-                </button>
-              </div>
-              
-              <div class="splide__track toujou-slider__track">
+        <toujou-slider
+            class="toujou-slider"
+            aria-label="Splide Basic HTML Example!!"
+            slider-design="${args.designColor}" ${args.isFullwidth ? 'slider-fullwidth' : ''}
+            ${args.showCount ? 'show-count' : ''}
+            ${args.autoplay ? 'auto-play' : ''}>
+            <div class="splide toujou-slider__slider">
+                <div class="splide__arrows">
+                    <button class="splide__arrow splide__arrow--prev toujou-slider-control toujou-slider-control--prev" aria-label="Previous slide">
+                        <toujou-icon class="toujou-icon toujou-slider-control__icon toujou-slider-control__icon--prev" icon-name="arrow-left"></toujou-icon>
+                    </button>
+                    <button class="splide__arrow splide__arrow--next toujou-slider-control toujou-slider-control--next" aria-label="Next slide">
+                        <toujou-icon class="toujou-icon toujou-slider-control__icon toujou-slider-control__icon--next" icon-name="arrow-right"></toujou-icon>
+                    </button>
+                </div>
+                
+                <div class="splide__track toujou-slider__track">
                     <ul class="splide__list toujou-slider__list">
-                        <li class="splide__slide toujou-slider__slide">
+                        <li class="splide__slide toujou-slider__slide" data-splide-interval="${args.autoplayInterval * 1000}">
                             <toujou-single-media class="toujou-single-media toujou-slider__item" slot="slider-item">
                                 <img class="toujou-single-media__image" src="https://picsum.photos/1600/900" alt="Nice image">
                             </toujou-single-media>
                         </li>
-                        <li class="splide__slide toujou-slider__slide">
+                        <li class="splide__slide toujou-slider__slide" data-splide-interval="${args.autoplayInterval * 1000}">
                             <toujou-single-media class="toujou-single-media toujou-slider__item" slot="slider-item">
-                                <img class="toujou-single-media__image" src="https://picsum.photos/1600/900" alt="Nice image">
+                                <img class="toujou-single-media__image" src="https://picsum.photos/1601/900" alt="Nice image">
                             </toujou-single-media>
                         </li>
-                        <li class="splide__slide toujou-slider__slide">
+                        <li class="splide__slide toujou-slider__slide" data-splide-interval="${args.autoplayInterval * 1000}">
                             <toujou-single-media class="toujou-single-media toujou-slider__item" slot="slider-item">
-                                <img class="toujou-single-media__image" src="https://picsum.photos/1600/900" alt="Nice image">
+                                <img class="toujou-single-media__image" src="https://picsum.photos/1602/900" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide toujou-slider__slide" data-splide-interval="${args.autoplayInterval * 1000}">
+                            <toujou-single-media class="toujou-single-media toujou-slider__item" slot="slider-item">
+                                <img class="toujou-single-media__image" src="https://picsum.photos/1603/900" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide toujou-slider__slide" data-splide-interval="${args.autoplayInterval * 1000}">
+                            <toujou-single-media class="toujou-single-media toujou-slider__item" slot="slider-item">
+                                <img class="toujou-single-media__image" src="https://picsum.photos/1604/900" alt="Nice image">
                             </toujou-single-media>
                         </li>
                     </ul>
+                </div>      
+                <div class="splide__progress toujou-slider-progress">
+                    <div class="splide__progress__bar toujou-slider-progress__bar"></div>
+                </div>
             </div>
+            
+            
+            <toujou-slider-count class="toujou-slider-count">
+                <span class="toujou-slider-count__current"></span>
+                <span class="toujou-slider-count__separator">of</span>
+                <span class="toujou-slider-count__total"></span>
+            </toujou-slider-count>
+                
+            
         </toujou-slider>
     `;
 };
@@ -84,6 +153,9 @@ export const Slider = Template.bind({});
 
 Slider.args = {
     designColor: 'default',
+    showCount: true,
     isFullwidth: false,
+    autoplay: false,
+    autoplayInterval: 6,
 }
 
