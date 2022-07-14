9abd8649540d95991567c776adaf7c03e79f8cc2
Luis Pato <lpato@dfau.de>
2022-07-14 12:21:38 +0200

    feat: start adding photswipe to the gallery element Card: https://app.clickup.com/t/2kpd1pn

--- src/components/toujou-gallery/toujou-gallery.ts
+++ src/components/toujou-gallery/toujou-gallery.ts
@@ -1,12 +1,8 @@
 import { LitElement } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
-// import PhotoSwipeLightbox from 'photoswipe/lightbox';
-// import PhotoSwipeStyles from 'photoswipe/dist/photoswipe.css';
-
-// import PhotoSwipe from 'photoswipe';
-// import splideSliderStyles from "@splidejs/splide/dist/css/splide.min.css";
-// import PhotoSwipeStyles from 'photoswipe/src/photoswipe.css';
+import PhotoSwipeLightbox from 'photoswipe/dist/photoswipe-lightbox.esm.js';
+import PhotoSwipe from 'photoswipe';
 
 @customElement('toujou-gallery')
 export class ToujouGallery extends LitElement {
@@ -14,30 +10,25 @@ export class ToujouGallery extends LitElement {
 
     constructor() {
         super();
-        // console.log('aaa', PhotoSwipeLightbox);
+        console.log('aaddddda', PhotoSwipe);
     }
 
-    // connectedCallback() {
-    //     super.connectedCallback();
-    //
-    //     console.log('connected callback');
-    //     const GL = new PhotoSwipe({
-    //         gallery: 'toujou-gallery',
-    //         children: 'toujou-gallery-item',
-    //         pswpModule: PhotoSwipe
-    //     })
-    //
-    //     GL.init();
-    //
-    //     const lb = new PhotoSwipeLightbox({
-    //         gallery: 'toujou-gallery',
-    //         children: 'a',
-    //         pswModule: () => import('photoswipe')
-    //     })
-    //     console.log('222', lb);
-    //     lb.init()
-    // }
+    connectedCallback() {
+        super.connectedCallback();
+
+        const lb = new PhotoSwipeLightbox({
+            gallery: `#gallery--getting-started`,
+            children: 'a',
+            pswpModule: PhotoSwipe
+        })
+
+        console.log('LS', lb)
+
+        lb.init();
+    }
 }
+
+
 
 declare global {
     interface HTMLElementTagNameMap {
--- src/stories/02. elements/gallery/gallery.stories.ts
+++ src/stories/02. elements/gallery/gallery.stories.ts
@@ -25,15 +25,48 @@ const Template = () => {
     return `
         <h1>Gallery!!!</h1>
         <toujou-gallery id="gallery1" class="gallery">
-            <a href="#" class="gallery-item">
-                <img src="https://picsum.photos/1800" alt="beautiful image" class="content-card__image">
-            </a>
-            <a href="#" class="gallery-item">
-                <img src="https://picsum.photos/1800" alt="beautiful image" class="content-card__image">
-            </a>
-            <a href="#" class="gallery-item">
-                <img src="https://picsum.photos/1800" alt="beautiful image" class="content-card__image">
-            </a>
+            <div class="pswp-gallery pswp-gallery--single-column" id="gallery--getting-started">
+              <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg" 
+                data-pswp-width="1669" 
+                data-pswp-height="2500" 
+                target="_blank">
+                <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg" alt="" />
+              </a>
+              <!-- cropped thumbnail: -->
+              <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/7/img-2500.jpg" 
+                data-pswp-width="1875" 
+                data-pswp-height="2500" 
+                data-cropped="true" 
+                target="_blank">
+                <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/7/img-200.jpg" alt="" />
+                Cropped
+              </a>
+              <!-- data-pswp-src with custom URL in href -->
+              <a href="https://unsplash.com" 
+                data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg"
+                data-pswp-width="2500" 
+                data-pswp-height="1666" 
+                target="_blank">
+                <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg" alt="" />
+              </a>
+              <!-- Without thumbnail: -->
+              <a href="http://example.com" 
+                data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/5/img-2500.jpg"
+                data-pswp-width="2500" 
+                data-pswp-height="1668" 
+                target="_blank">
+                No thumbnail
+              </a>
+              <!-- wrapped with any element: -->
+              <div>
+                <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-2500.jpg"
+                  data-pswp-width="2500" 
+                  data-pswp-height="1667" 
+                  target="_blank">
+                  <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-200.jpg" alt="" />
+                </a>
+              </div>
+            </div>
         </toujou-gallery>
     `;
 };
