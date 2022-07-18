82aefa691c7c0188d5ee3c50329f277d6aa00252
Luis Pato <lpato@dfau.de>
2022-07-18 11:26:03 +0200

    feat: Change header position when vertical position is set on desktop devices Card:https://app.clickup.com/t/2rgwwa1

--- types/components/toujou-gallery/toujou-gallery.d.ts
+++ types/components/toujou-gallery/toujou-gallery.d.ts
@@ -1,9 +1,6 @@
 import { LitElement } from 'lit';
 export declare class ToujouGallery extends LitElement {
-    static styles: import("lit").CSSResult;
-    private lightbox;
-    constructor();
-    createRenderRoot(): this;
+    protected createRenderRoot(): Element | ShadowRoot;
     connectedCallback(): void;
 }
 declare global {
