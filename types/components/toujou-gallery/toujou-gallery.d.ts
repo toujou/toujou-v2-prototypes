aacc3fdbd59b5812815af717a6a1356e10c3860b
Luis Pato <lpato@dfau.de>
2022-07-19 06:40:26 +0200

    docs: Add gallery element documentation Card: https://app.clickup.com/t/2kpd1pn

--- types/components/toujou-gallery/toujou-gallery.d.ts
+++ types/components/toujou-gallery/toujou-gallery.d.ts
@@ -1,7 +1,18 @@
 import { LitElement } from 'lit';
 export declare class ToujouGallery extends LitElement {
+    private lightbox;
+    private get galleryID();
     protected createRenderRoot(): Element | ShadowRoot;
     connectedCallback(): void;
+    /**
+     * Register a new UI element: the caption
+     */
+    _handleUiRegister: () => void;
+    /**
+     * Reacto to change on the lightbox so we can add the correct caption text
+     * @param {HTMLElement} el
+     */
+    _handleChange: (el: HTMLElement) => void;
 }
 declare global {
     interface HTMLElementTagNameMap {
