361f8231be18b5377a2df6a3a0a16b6267b8cc03
Luis Pato <lpato@dfau.de>
2022-07-20 15:35:56 +0200

    chore: build assets

--- types/components/toujou-sliding-nav/toujou-sliding-nav.d.ts
+++ types/components/toujou-sliding-nav/toujou-sliding-nav.d.ts
@@ -1,6 +1,25 @@
-import { LitElement } from 'lit';
-export declare class ToujouSlidingNav extends LitElement {
+export declare class ToujouSlidingNav extends HTMLElement {
+    startingActiveLevel: number;
+    maximalLevel: number;
+    _backButtonEl: HTMLButtonElement | null;
+    _mainNavEl: HTMLUListElement | null;
+    _subtitleEl: HTMLParagraphElement | null;
+    _subtitleText: string;
+    _activeLevel: number | null;
+    _chevrons: HTMLSpanElement[];
+    _currentOpenItems: HTMLLIElement[];
+    ATTRIBUTES: {
+        NAV_OPEN_LEVEL: string;
+        LI_OPEN_ITEM: string;
+    };
     constructor();
+    set activeLevel(value: number | null);
+    get activeLevel(): number | null;
+    connectedCallback(): void;
+    _getElements(): void;
+    _updateUI(): void;
+    _handleBackButtonClick(): void;
+    _handleChevronClick(event: Event): void;
 }
 declare global {
     interface HTMLElementTagNameMap {
