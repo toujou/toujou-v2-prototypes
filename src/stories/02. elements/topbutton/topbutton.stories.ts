81e2f4786bfc183b1f2b367915ecf5d73126d512
Luis Pato <lpato@dfau.de>
2022-07-21 12:09:38 +0200

    feat: Add story, styles and docs for the topbutton Card: https://app.clickup.com/t/2t1y8xx

---
+++ src/stories/02. elements/topbutton/topbutton.stories.ts
@@ -0,0 +1,25 @@
+import { Story, Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+// @ts-ignore
+import topbuttonDocs from './topbutton.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/Topbutton',
+    parameters: {
+        badges: [TOUJOU_BADGES.DONE],
+        docs: {
+            page: topbuttonDocs,
+        },
+    },
+} as Meta;
+
+const Template: Story = () => {
+    return `
+        <toujou-top-button title="Nach oben" aria-label="Nach oben" class="topbutton topbutton--visible" role="button" aria-hidden="false" visible="">
+            <toujou-icon class="icon" icon-name="arrow-up" icon-size="l" icon-color="background"></toujou-icon>
+        </toujou-top-button>
+    `;
+};
+
+export const Topbutton = Template.bind({});
