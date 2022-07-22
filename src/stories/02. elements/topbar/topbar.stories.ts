17dde285fc9947957c830b065d210057f830c038
Tobias Wojtylak <twojtylak@dfau.de>
2022-07-22 17:10:55 +0200

    fix: add padding-top to main element
    relates: https://app.clickup.com/t/2t7tdkt

--- src/stories/02. elements/topbar/topbar.stories.ts
+++ src/stories/02. elements/topbar/topbar.stories.ts
@@ -293,24 +293,25 @@ const Template: Story<TopbarStoryProps> = (args: TopbarStoryProps) => {
             </nav>
 
         </toujou-topbar>   
-        
-        <toujou-text-block class="text-block" text-block-column-count="1">
-            <toujou-text-block-column class="text-block-column">
-                <div class="text-block__content">
-                    <h1>Left aligned H1 headline</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </toujou-text-block-column>
-        </toujou-text-block>
-        
-        <toujou-text-block class="text-block" text-block-column-count="1">
-            <toujou-text-block-column class="text-block-column">
-                <div class="text-block__content">
-                    <h1>Left aligned H1 headline</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </toujou-text-block-column>
-        </toujou-text-block>
+        <main>
+            <toujou-text-block class="text-block" text-block-column-count="1">
+                <toujou-text-block-column class="text-block-column">
+                    <div class="text-block__content">
+                        <h1>Left aligned H1 headline</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </toujou-text-block-column>
+            </toujou-text-block>
+            
+            <toujou-text-block class="text-block" text-block-column-count="1">
+                <toujou-text-block-column class="text-block-column">
+                    <div class="text-block__content">
+                        <h1>Left aligned H1 headline</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </toujou-text-block-column>
+            </toujou-text-block>
+        </main>
     `;
 };
 
