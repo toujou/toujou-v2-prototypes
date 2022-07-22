c34f9b75b7ec543d459b2c3dda51f4b2724dcd83
Tobias Wojtylak <twojtylak@dfau.de>
2022-07-22 11:47:02 +0200

    fix: define link styles globally
    relates: https://app.clickup.com/t/2t7u5k7

--- src/stories/03. pages/direct-child-elements/direct-child-elements.stories.ts
+++ src/stories/03. pages/direct-child-elements/direct-child-elements.stories.ts
@@ -59,6 +59,12 @@ const Template = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             
             <h1 class="font--gamma">This is a font--gamma headline</h1>
+            
+            <ul>
+                <li>
+                    <a href="#">List with link </a>
+                </li>
+            </ul>
         </main>
     `;
 };
