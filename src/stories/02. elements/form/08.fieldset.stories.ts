95dadb457be5615da58213bb1b97bc51978a9b97
Luis Pato <lpato@dfau.de>
2022-08-15 14:34:30 +0200

    feat: Add story and styles for the fieldset element Card: https://app.clickup.com/t/2vj3va5

---
+++ src/stories/02. elements/form/08.fieldset.stories.ts
@@ -0,0 +1,125 @@
+import { Story, Meta } from '@storybook/web-components';
+import { withXD } from "storybook-addon-xd-designs";
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js';
+// @ts-ignore
+import formDocs from './form.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/Forms',
+    decorators: [withXD],
+    parameters: {
+        design: {
+            artboardUrl: 'https://xd.adobe.com/view/57945f95-9455-4031-a7b6-76e6a7ec74a6-02c7/screen/9b61f9cb-3a4b-4df5-9706-2991633fc53e/Desktop',
+        },
+        badges: [TOUJOU_BADGES.PROGRESS],
+        docs: {
+            page: formDocs,
+        }
+    },
+    argTypes: {
+        state: {
+            table: {
+                category: "Inputs settings",
+                defaultValue: { summary: 'default' },
+            },
+            name: 'Input state',
+            description: "Set the visible input state",
+            options: ['default', 'disabled', 'success', 'error'],
+            control: { type: 'radio' },
+            defaultValue: ['default'],
+            required: true,
+        },
+    }
+} as Meta;
+
+interface ToujouFieldsetStoryProps {
+    state: string;
+}
+
+const Template: Story<ToujouFieldsetStoryProps> = (args: ToujouFieldsetStoryProps) => {
+    const disabledAttribute = args.state === 'disabled' ? 'is-disabled' : ''
+
+    return `
+        <style>
+            body {
+                background-color: var(--color-bg);
+            }
+        </style>
+        <form
+            enctype="multipart/form-data"
+            method="post" 
+            class="form" 
+            id="testform-1000091" 
+            action="#"
+            novalidate="true">
+    
+            <fieldset id="testform-1000091-fieldset-1" class="fieldset" ${args.state === 'disabled' ? 'is-disabled' : ''}>
+                <legend class="fieldset__legend">Fieldset super name</legend>
+    
+                <toujou-input-group class="input-group input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                    <label for="testform-1000091-text-1" class="input-label">Nice label</label>
+                    <span class="input-description">This is a description</span>
+                    <input placeholder="This is a placeholder"
+                           required="required"
+                           data-pristine-required-message-de="The given subject was empty."
+                           class="input input--text"
+                           id="testform-1000091-text-1"
+                           type="text" name="tx_form_formframework[testform-1000091][text-1]"
+                           value=""
+                           ${disabledAttribute}>
+                    <span class="pristine-error form__error">This is an error message!</span>
+                </toujou-input-group>
+                
+                <toujou-input-group class="input-group input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                    <label for="testform-1000091-text-2" class="input-label">Nice label</label>
+                    <span class="input-description">This is a description</span>
+                    <input placeholder="This is a placeholder"
+                           required="required"
+                           data-pristine-required-message-de="The given subject was empty."
+                           class="input input--text"
+                           id="testform-1000091-text-2"
+                           type="text" name="tx_form_formframework[testform-1000091][text-2]"
+                           value=""
+                           ${disabledAttribute}>
+                    <span class="pristine-error form__error">This is an error message!</span>
+                </toujou-input-group>
+                
+                <toujou-input-group class="input-group input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                    <label for="testform-1000091-text-3" class="input-label">Nice label</label>
+                    <span class="input-description">This is a description</span>
+                    <input placeholder="This is a placeholder"
+                           required="required"
+                           data-pristine-required-message-de="The given subject was empty."
+                           class="input input--text"
+                           id="testform-1000091-text-3"
+                           type="text" name="tx_form_formframework[testform-1000091][text-3]"
+                           value=""
+                           ${disabledAttribute}>
+                    <span class="pristine-error form__error">This is an error message!</span>
+                </toujou-input-group>
+                
+                <toujou-input-group class="input-group input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                    <label for="testform-1000091-text-4" class="input-label">Nice label</label>
+                    <span class="input-description">This is a description</span>
+                    <input placeholder="This is a placeholder"
+                           required="required"
+                           data-pristine-required-message-de="The given subject was empty."
+                           class="input input--text"
+                           id="testform-1000091-text-4"
+                           type="text" name="tx_form_formframework[testform-1000091][text-4]"
+                           value=""
+                           ${disabledAttribute}>
+                    <span class="pristine-error form__error">This is an error message!</span>
+                </toujou-input-group>
+            
+            </fieldset>
+        </form>
+    `;
+};
+
+export const Fieldset = Template.bind({});
+
+Fieldset.args = {
+    state: 'default'
+}
