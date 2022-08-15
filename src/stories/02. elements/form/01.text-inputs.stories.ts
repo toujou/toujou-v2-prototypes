82114fef5683f117a87730d2938b5511e40ea175
Luis Pato <lpato@dfau.de>
2022-08-15 13:29:09 +0200

    feat: Move old form stories and styles to a backup folder, start adding story and styles for text inputs Card: https://app.clickup.com/t/2vj3va5

--- src/stories/02. elements/form/01.text-inputs.stories.ts
+++ src/stories/02. elements/form/01.text-inputs.stories.ts
@@ -12,7 +12,7 @@ export default {
         design: {
             artboardUrl: 'https://xd.adobe.com/view/57945f95-9455-4031-a7b6-76e6a7ec74a6-02c7/screen/9b61f9cb-3a4b-4df5-9706-2991633fc53e/Desktop',
         },
-        badges: [TOUJOU_BADGES.DONE],
+        badges: [TOUJOU_BADGES.PROGRESS],
         docs: {
             page: formDocs,
         }
@@ -53,97 +53,75 @@ const Template: Story<ToujouTextInputsStoryProps> = (args: ToujouTextInputsStory
             id="testform-1000091" 
             action="#"
             novalidate="true">
-
-        	<toujou-input-group
-        	    class="input-group ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''}"
-        	    ${args.state === 'disabled' ? 'is-disabled' : ''}
-        	    input-group-type="text">			
-				<label class="input-label" for="testform-1000091-text-1">Ihr Name</label>
-				<toujou-form-description class="form-description">Enter your name so we can print out the documents correctly.</toujou-form-description>
-        		<input
-                    placeholder="Max Mustermann"
-                    data-pristine-required-message="The given subject was empty."
-                    class="input input--text"
-                    id="testform-1000091-text-1"
-                    type="text"
-                    name="tx_form_formframework[testform-1000091][text-1]"
-                    value=""
-                    required="required"
-                    ${disabledAttribute}>
-                <toujou-form-error class="pristine-error form__error">The given subject was empty.</span>
-	        </toujou-input-group>
-
-            <toujou-input-group
-                class="input-group ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''}"
-                ${args.state === 'disabled' ? 'is-disabled' : ''}
-                input-group-type="email">
-                <label class="input-label" for="testform-1000091-email-1">Email address</label>
-                <toujou-form-description class="form-description">This is a description</toujou-form-description>
-                <input
-                    placeholder="This is a placeholder"
-                    data-pristine-required-message="The given subject was empty."
-                    class=" form-control input input--text"
-                    id="testform-1000091-email-1"
-                    type="email"
-                    name="tx_form_formframework[testform-1000091][email-1]"
-                    value=""
-                    ${disabledAttribute}>
-                <toujou-form-error class="pristine-error form__error">The given subject was empty.</span>
+            
+            <toujou-input-group class="input-group input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                <label for="testform-1000091-text-1" class="input-label">Nice label</label>
+                <span class="input-description">This is a description</span>
+                <input placeholder="This is a placeholder"
+                       required="required"
+                       data-pristine-required-message-de="The given subject was empty."
+                       class="input input--text"
+                       id="testform-1000091-text-1"
+                       type="text" name="tx_form_formframework[testform-1000091][text-1]"
+                       value=""
+                       ${disabledAttribute}>
             </toujou-input-group>
             
-            <toujou-input-group
-                class="input-group ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''}"
-                ${args.state === 'disabled' ? 'is-disabled' : ''}
-                input-group-type="telephone">
-                <label class="input-label" for="testform-1000091-telephone-1">Telephone number</label>
-                <toujou-form-description class="form-description">This is a description</toujou-form-description>
-                <input
-                    placeholder="This is a placeholder"
-                    data-pristine-required-message="The given subject was empty."
-                    class=" form-control input input--text"
-                    id="testform-1000091-telephone-1"
-                    type="tel"
-                    name="tx_form_formframework[testform-1000091][telephone-1]"
-                    value=""
-                    ${disabledAttribute}>
-                <toujou-form-error class="pristine-error form__error">The given subject was empty.</span>
+            <toujou-input-group class="input-group input-group--email input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                <label for="testform-1000091-email-1" class="input-label">Email address</label>
+                <span class="input-description">This is a description</span>
+                <input placeholder="This is a placeholder"
+                       required="required"
+                       data-pristine-required-message="The given subject was empty."
+                       class="form-control input input--text"
+                       id="testform-1000091-email-1"
+                       type="email"
+                       name="tx_form_formframework[testform-1000091][email-1]"
+                       value=""
+                       ${disabledAttribute}>
             </toujou-input-group>
-	
-            <toujou-input-group
-                class="input-group ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''}"
-                ${args.state === 'disabled' ? 'is-disabled' : ''}
-                input-group-type="url">
-                <label class="input-label" for="testform-1000091-url-1">URL</label>
-                <toujou-form-description class="form-description">This is a description</toujou-form-description>
-                <input
-                    placeholder="This is a placeholder"
-                    data-pristine-required-message="The given subject was empty."
-                    class=" form-control input input--text"
-                    id="testform-1000091-url-1"
-                    type="url"
-                    name="tx_form_formframework[testform-1000091][url-1]"
-                    value=""
-                    ${disabledAttribute}>
-                <toujou-form-error class="pristine-error form__error">The given subject was empty.</span>
+            
+            <toujou-input-group class="input-group input-group--telephone input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                <label for="testform-1000091-email-1" class="input-label">Telephone number</label>
+                <span class="input-description">This is a description</span>
+                <input placeholder="This is a placeholder"
+                       required="required"
+                       data-pristine-required-message="The given subject was empty."
+                       class=" form-control input input--text"
+                       id="testform-1000091-telephone-1"
+                       type="tel"
+                       name="tx_form_formframework[testform-1000091][telephone-1]"
+                       value=""
+                       ${disabledAttribute}>
             </toujou-input-group>
-	
-            <toujou-input-group
-                class="input-group ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''}"
-                ${args.state === 'disabled' ? 'is-disabled' : ''}
-                input-group-type="number">
-                <label class="input-label" for="testform-1000091-number-1">Number</label>
-                <toujou-form-description class="form-description">This is a descripton</toujou-form-description>
-                <input
-                    step="1"
-                    placeholder="This is a placeholder"
-                    data-pristine-required-message="The given subject was empty."
-                    class="form-control input input--text"
-                    id="testform-1000091-number-1"
-                    type="number"
-                    name="tx_form_formframework[testform-1000091][number-1]"
-                    value=""
-                    ${disabledAttribute}>
-                <toujou-form-error class="pristine-error form__error">The given subject was empty.</span>
+
+            <toujou-input-group class="input-group input-group--url input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                <label for="testform-1000091-url-1" class="input-label">URL</label>
+                <span class="input-description">This is a description</span>
+                <input placeholder="This is a placeholder"
+                       required="required"
+                       data-pristine-required-message="The given subject was empty."
+                       class=" form-control input input--text"
+                       id="testform-1000091-url-1"
+                       type="url"
+                       name="tx_form_formframework[testform-1000091][url-1]"
+                       value=""
+                       ${disabledAttribute}>
+            </toujou-input-group>
+            
+            <toujou-input-group class="input-group input-group--number input-group--required ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                <label for="testform-1000091-number-1" class="input-label">Number</label>
+                <span class="input-description">This is a description</span>
+                <input placeholder="This is a placeholder"
+                       step="1"
+                       required="required"
+                       data-pristine-required-message="The given subject was empty."
+                       class=" form-control input input--text"
+                       id="testform-1000091-number-1"
+                       type="number"
+                       name="tx_form_formframework[testform-1000091][number-1]"
+                       value=""
+                       ${disabledAttribute}>
             </toujou-input-group>
         </form>
     `;
