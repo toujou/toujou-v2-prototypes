d8c738851d3f737906341057f20519fc803284a5
Luis Pato <lpato@dfau.de>
2022-08-17 15:21:22 +0200

    Updates

---
+++ assets/es.regexp.flags.5a4168c1.js
@@ -0,0 +1,2 @@
+import{a5 as e,a6 as a,a7 as t,a8 as s}from"./iframe.2b7c0b61.js";var o=e,i=a,l=t,g=s,r=RegExp.prototype,c=o&&g(function(){return Object.getOwnPropertyDescriptor(r,"flags").get.call({dotAll:!0,sticky:!0})!=="sy"});c&&i(r,"flags",{configurable:!0,get:l});
+//# sourceMappingURL=es.regexp.flags.5a4168c1.js.map
