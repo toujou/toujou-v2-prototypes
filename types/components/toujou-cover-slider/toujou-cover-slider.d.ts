bb89711dd90b8302392dfa4893e3bde63871aaa1
Luis Pato <lpato@dfau.de>
2022-08-18 11:30:30 +0200

    chore: build assets

---
+++ types/components/toujou-cover-slider/toujou-cover-slider.d.ts
@@ -0,0 +1,46 @@
+import { LitElement } from "lit";
+export declare class ToujouCoverSlider extends LitElement {
+    splideContainer: HTMLElement | null;
+    private splideSlider;
+    private _maxCardWidth;
+    private _cardGap;
+    private _cardWidthWithGap;
+    private _sliderWidth;
+    private _slidesThatFitIntoScreen;
+    private _numberOfSlidesInSlider;
+    private _isSlide;
+    private _resizeObserver;
+    constructor();
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    /**
+     * Disable shadow root
+     */
+    createRenderRoot(): this;
+    firstUpdated(): void;
+    /**
+     * Initialize the cover slider element with the correct options
+     * @private
+     */
+    private _initSlider;
+    /**
+     * Update the slider options by destroying it and creating it again
+     */
+    private _updateSliderOptions;
+    /**
+     * Get new values and update the cover slider UI accordingly
+     */
+    private _updateUI;
+    /**
+     * Get current values of the css properties and update private variables
+     */
+    private _updateCustomProperties;
+    /**
+     * Calculate how many cards can fit on the screen
+     */
+    private _calculateSlidesThatFitIntoScreen;
+    /**
+     * Calculate padding percentage, so we can show half a card on the right side of the screen
+     */
+    private _calculatePadding;
+}
