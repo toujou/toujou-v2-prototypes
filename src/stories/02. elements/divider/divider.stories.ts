640d950af910c0a9be13625f6f75e4a17cf0e54c
Luis Pato <lpato@dfau.de>
2022-08-22 13:23:18 +0200

    feat: Add story and styles for the divider element Card: https://app.clickup.com/t/2vqpbvw

---
+++ src/stories/02. elements/divider/divider.stories.ts
@@ -0,0 +1,45 @@
+import { Story, Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+// @ts-ignore
+import dividerDocs from './divider.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/Divider',
+    parameters: {
+        badges: [TOUJOU_BADGES.TESTING],
+        docs: {
+            page: dividerDocs,
+        },
+    },
+    argTypes: {
+        dividerType: {
+            table: {
+                category: "Divider Settings",
+                defaultValue: { summary: 'primary' },
+            },
+            name: 'Divider type',
+            description: "Choose between the different divider types",
+            options: ['background', 'primary', 'secondary', 'grey'],
+            control: { type: 'radio' },
+            defaultValue: ['false'],
+            required: true,
+        },
+    }
+} as Meta;
+
+interface DividerStoryProps {
+    dividerType: string,
+}
+
+const Template: Story<DividerStoryProps> = (args: DividerStoryProps) => {
+    return `
+        <hr class="divider" divider-type="${args.dividerType}">
+    `
+};
+
+export const Divider = Template.bind({});
+
+Divider.args = {
+    dividerType: 'primary',
+}
