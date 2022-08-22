6d19965e5d30612256378286edfead7b65a94a55
Luis Pato <lpato@dfau.de>
2022-08-22 14:59:39 +0200

    test: Add e2e tests for the table element Card: https://app.clickup.com/t/2mybka8

---
+++ src/tests/table/table.cy.ts
@@ -0,0 +1,82 @@
+/// <reference types="cypress" />
+
+const colorBg = Cypress.env('colorBg');
+const colorFont = Cypress.env('colorFont');
+const colorPrimary = Cypress.env('colorPrimary');
+const colorFontO10 = Cypress.env('colorFontO10');
+
+describe('table', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-table--table');
+    });
+
+    it('has correct class', () => {
+        cy.get('table.table').should('exist')
+    });
+
+    it('has table container parent', () => {
+        cy.get('table.table').parent().should('have.prop', 'tagName').should('eq', 'TOUJOU-TABLE-CONTAINER');
+        cy.get('table.table').parent().invoke('attr', 'class').should('eq', 'table-container');
+    });
+
+    it('table container parent has correct styles', () => {
+        cy.get('table.table').parent().should('have.css', 'display', 'block');
+        cy.get('table.table').parent().should('have.css', 'overflow-x', 'auto');
+        cy.get('table.table').parent().should('have.css', 'margin-bottom', '32px');
+    });
+
+    it('table element has correct styles', () => {
+        cy.get('table.table tr:nth-child(1)').should('have.css', 'background-color', colorFontO10);
+        cy.get('table.table tr:nth-child(2)').should('have.css', 'background-color', colorBg);
+        cy.get('table.table tr:nth-child(3)').should('have.css', 'background-color', colorFontO10);
+        cy.get('table.table tr:nth-child(4)').should('have.css', 'background-color', colorBg);
+
+        cy.get('table.table tr td').should('have.css', 'color', colorFont);
+        cy.get('table.table tr th').should('have.css', 'color', colorPrimary);
+    });
+
+    it('table caption and summary have correct styles', () => {
+        cy.get('table.table .table__caption').should('have.css', 'color', colorFont);
+        cy.get('table.table .table__caption').should('have.css', 'margin-bottom', '8px');
+        cy.get('table.table .table__caption').should('have.css', 'font-size', '16px');
+        cy.get('table.table .table__summary').should('have.css', 'color', colorFont);
+        cy.get('table.table .table__summary').should('have.css', 'font-size', '14px');
+    });
+})
+
+describe('table - centered', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-table--table&args=tableAlignment:center');
+    });
+
+    it('text has correct alignment', () => {
+        cy.get('table.table').should('have.css', 'text-align', 'center');
+        cy.get('table.table .table__caption').should('have.css', 'text-align', 'center');
+    });
+})
+
+describe('table - left aligned', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-table--table&args=tableAlignment:left');
+    });
+
+    it.only('text has correct alignment', () => {
+        cy.get('table.table').should('have.css', 'text-align', 'left');
+        cy.get('table.table .table__caption').should('have.css', 'text-align', 'left');
+    });
+})
+
+describe('table - right aligned', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-table--table&args=tableAlignment:right');
+    });
+
+    it.only('text has correct alignment', () => {
+        cy.get('table.table').should('have.css', 'text-align', 'right');
+        cy.get('table.table .table__caption').should('have.css', 'text-align', 'right');
+    });
+})
+
+
+
+export {}
