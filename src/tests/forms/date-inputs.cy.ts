1b3ce66e6ca0ca95dd9461dd00b2570f2981e46f
Luis Pato <lpato@dfau.de>
2022-08-24 09:01:49 +0200

    test: Add basic e2e tests for the date inputs Card: https://app.clickup.com/t/2mybka8

---
+++ src/tests/forms/date-inputs.cy.ts
@@ -0,0 +1,86 @@
+/// <reference types="cypress" />
+
+const colorFont = Cypress.env('colorFont');
+const colorFontLight = Cypress.env('colorFontLight');
+const colorPrimary = Cypress.env('colorPrimary');
+const colorSuccess = Cypress.env('colorSuccess');
+const colorError = Cypress.env('colorError');
+const colorBg = Cypress.env('colorBg');
+
+describe('Forms / date inputs', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-forms--date-inputs');
+        // @ts-ignore
+        cy.get('body').resetRealHover();
+    });
+
+    it('input date has correct structure', () => {
+        cy.get('.input-group--date .input-label').should('exist');
+        cy.get('.input-group--date .input-description').should('exist');
+        cy.get('.input-group--date .input--date').should('exist');
+        cy.get('.input-group--date .input--date').invoke('attr', 'pattern').should('exist');
+        cy.get('.input-group--date .input--date').invoke('attr', 'type').should('eq', 'date');
+        cy.get('.input-group--date .form__error').should('exist');
+    });
+
+    it('input date jquery has correct structure', () => {
+        cy.get('.input-group--date-jquery .input-label').should('exist');
+        cy.get('.input-group--date-jquery .input-description').should('exist');
+        cy.get('.input-group--date-jquery .date-dummy').invoke('attr', 'class').should('contain', 'date-dummy--empty');
+        cy.get('.input-group--date-jquery .date-dummy').invoke('attr', 'class').should('contain', 'input--date');
+        cy.get('.input-group--date-jquery .date-dummy').invoke('attr', 'class').should('contain', 'flatpickr-input');
+        cy.get('.input-group--date-jquery .input--date').invoke('attr', 'type').should('eq', 'text');
+        cy.get('.input-group--date-jquery .input--date').invoke('attr', 'readonly').should('eq', 'readonly');
+        cy.get('.input-group--date-jquery input[type="hidden"').should('exist');
+        cy.get('.input-group--date-jquery input[type="hidden"').invoke('attr', 'value').should('eq', 'Y-m-d');
+        cy.get('.input-group--date-jquery .form__error').should('exist');
+    });
+
+    it('input date has correct styles', () => {
+        cy.get('.input-group--date .input--date').then((inputDate) => {
+            expect(inputDate).to.exist;
+            expect(inputDate).to.have.css('position').equal('relative');
+            expect(inputDate).to.have.css('border').equal(`1px solid ${colorFontLight}`);
+            expect(inputDate).to.have.css('background-color').equal(colorBg);
+            expect(inputDate).to.have.css('padding').equal('24px 8px 8px');
+            expect(inputDate).to.have.css('color').equal(colorFont);
+            expect(inputDate).to.have.css('font-size').equal('16px');
+            expect(inputDate).to.have.css('font-weight').equal('600');
+        });
+    });
+
+    it('input date jquery has correct styles', () => {
+        cy.get('.input-group--date-jquery .input--date').then((inputDateJQuery) => {
+            expect(inputDateJQuery).to.exist;
+            expect(inputDateJQuery).to.have.css('position').equal('relative');
+            expect(inputDateJQuery).to.have.css('border').equal(`1px solid ${colorFontLight}`);
+            expect(inputDateJQuery).to.have.css('background-color').equal(colorBg);
+            expect(inputDateJQuery).to.have.css('padding').equal('24px 8px 8px');
+            expect(inputDateJQuery).to.have.css('color').equal(colorFont);
+            expect(inputDateJQuery).to.have.css('font-size').equal('16px');
+            expect(inputDateJQuery).to.have.css('font-weight').equal('600');
+        });
+    });
+
+    it('input date has correct hover styles', () => {
+        cy.get('.input-group--date .input--date').realHover();
+        cy.get('.input-group--date .input--date').should('have.css', 'border', `1px solid ${colorFont}`)
+    });
+
+    it('input jquery date has correct hover styles', () => {
+        cy.get('.input-group--date-jquery .input--date').realHover();
+        cy.get('.input-group--date-jquery .input--date').should('have.css', 'border', `1px solid ${colorFont}`)
+    });
+
+    it('input date has correct focus styles', () => {
+        cy.get('.input-group--date .input--date').focus();
+        cy.get('.input-group--date .input--date').should('have.css', 'border', `1px solid ${colorPrimary}`)
+    });
+
+    it('input date jquery has correct focus styles', () => {
+        cy.get('.input-group--date-jquery .input--date').focus();
+        cy.get('.input-group--date-jquery .input--date').should('have.css', 'border', `1px solid ${colorPrimary}`)
+    });
+});
+
+export {}
