1c9eba23652c88aef9058288f97537b2b88ec9f5
Luis Pato <lpato@dfau.de>
2022-08-24 07:14:57 +0200

    test: Add e2e tests for the radio elements Card: https://app.clickup.com/t/2mybka8

--- src/tests/forms/radio-inputs.cy.ts
+++ src/tests/forms/radio-inputs.cy.ts
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 
 const colorFont = Cypress.env('colorFont');
+const colorFontLight = Cypress.env('colorFontLight');
+const colorPrimary = Cypress.env('colorPrimary');
+const colorPrimaryLight = Cypress.env('colorPrimaryLight');
+const colorSuccess = Cypress.env('colorSuccess');
+const colorError = Cypress.env('colorError');
 
 describe('Forms / radio input', () => {
     beforeEach(() => {
@@ -19,7 +24,7 @@ describe('Forms / radio input', () => {
         cy.get('.input-group--radio .form__error').should('exist');
     });
 
-    it.only('select input has correct styles', () => {
+    it('select input has correct styles', () => {
         cy.get('.radio-group').then((radioGroup) => {
             expect(radioGroup).to.have.css('display').equal('grid');
             expect(radioGroup).to.have.css('grid-gap').equal('0px 8px');
@@ -37,68 +42,87 @@ describe('Forms / radio input', () => {
         })
     });
 
-    // it('select has correct hover styles', () => {
-    //     cy.get('.select__wrapper .select').realHover();
-    //     cy.get('.select__wrapper .select').should('have.css', 'border', `1px solid ${colorFont}`)
-    // });
-    //
-    // it('select has correct focus styles', () => {
-    //     cy.get('.select__wrapper .select').focus();
-    //     cy.get('.select__wrapper .select').should('have.css', 'border', `1px solid ${colorPrimary}`)
-    // });
+    it('select has correct hover styles', () => {
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorPrimary}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').realHover()
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorPrimaryLight}`);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorFont}`);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').realHover()
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorPrimary}`);
+    });
+
+    it('select has correct focus styles', () => {
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorPrimary}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'outline', 'rgb(0, 0, 0) none 0px');
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').focus();
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorPrimary}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'outline', `${colorPrimary} solid 2px`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'outline-offset', '2px');
+    });
 });
 
-// describe('Forms / radio input - disabled', () => {
-//     beforeEach(() => {
-//         cy.visit('/iframe.html?viewMode=story&id=components-forms--radio-inputs&args=state:disabled');
-//         // @ts-ignore
-//         cy.get('body').resetRealHover();
-//     });
-//
-//     it('select has correct disabled styles', () => {
-//         cy.get('.input-group--single-select').invoke('attr', 'class').should('contain', 'input-group--disabled');
-//         cy.get('.input-group--single-select .select').should('have.css', 'color', colorFontLight)
-//         cy.get('.input-group--single-select .select').should('have.css', 'border', `1px solid ${colorFontLight}`);
-//         cy.get('.input-group--single-select .select').should('have.css', 'pointer-events', 'none');
-//         // @ts-ignore
-//         cy.get('.input-group--single-select .select__wrapper').after('background-color').should('eq', colorFontLight);
-//     });
-//
-//     it('select has correct hover styles when disabled', () => {
-//         cy.get('.input-group--single-select .select').invoke('attr', 'disabled').should('exist');
-//         cy.get('.input-group--single-select .select').realHover();
-//         cy.get('.input-group--single-select .select').invoke('attr', 'disabled').should('exist');
-//         // @ts-ignore
-//         cy.get('.input-group--single-select .select__wrapper').after('background-color').should('eq', colorFontLight);
-//     });
-// });
-//
-// describe('Forms / radio input - success', () => {
-//     beforeEach(() => {
-//         cy.visit('/iframe.html?viewMode=story&id=components-forms--radio-inputs&args=state:success');
-//         // @ts-ignore
-//         cy.get('body').resetRealHover();
-//     });
-//
-//     it('input--text group has correct success styles', () => {
-//         cy.get('.input-group--single-select .select').should('have.css', 'color', colorFont)
-//         cy.get('.input-group--single-select .select').should('have.css', 'border', `1px solid ${colorSuccess}`);
-//         cy.get('.input-group--single-select .select').should('have.css', 'outline', `${colorSuccess} solid 1px`);
-//     });
-// });
-//
-// describe('Forms / radio input - error', () => {
-//     beforeEach(() => {
-//         cy.visit('/iframe.html?viewMode=story&id=components-forms--radio-inputs&args=state:error');
-//         // @ts-ignore
-//         cy.get('body').resetRealHover();
-//     });
-//
-//     it('input--text group has correct success styles', () => {
-//         cy.get('.input-group--single-select .select').should('have.css', 'color', colorFont)
-//         cy.get('.input-group--single-select .select').should('have.css', 'border', `1px solid ${colorError}`);
-//         cy.get('.input-group--single-select .select').should('have.css', 'outline', `${colorError} solid 1px`);
-//     });
-// });
+describe('Forms / radio input - disabled', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-forms--radio-inputs&args=state:disabled');
+        // @ts-ignore
+        cy.get('body').resetRealHover();
+    });
+
+    it('radio options have correct disabled styles', () => {
+        cy.get('.input-group--radio').invoke('attr', 'class').should('contain', 'input-group--disabled');
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorFontLight}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label').should('have.css', 'color', colorFont);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label').should('have.css', 'pointer-events', 'none');
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorFontLight}`);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label').should('have.css', 'color', colorFont);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label').should('have.css', 'pointer-events', 'none');
+    });
+
+    it('radio options have correct hover styles when disabled', () => {
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label').realHover()
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorFontLight}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label').should('have.css', 'color', colorFont);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label').realHover()
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorFontLight}`);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label').should('have.css', 'color', colorFont);
+    });
+});
+
+describe('Forms / radio input - success', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-forms--radio-inputs&args=state:success');
+        // @ts-ignore
+        cy.get('body').resetRealHover();
+    });
+
+    it('radio group has correct success styles', () => {
+        cy.get('.input-group--radio').invoke('attr', 'class').should('contain', 'input-group--has-success');
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorSuccess}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label').should('have.css', 'color', colorFont);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorFont}`);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label').should('have.css', 'color', colorFont);
+    });
+});
+
+describe('Forms / radio input - error', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-forms--radio-inputs&args=state:error');
+        // @ts-ignore
+        cy.get('body').resetRealHover();
+    });
+
+    it('radio group has correct error styles', () => {
+        cy.get('.input-group--radio').invoke('attr', 'class').should('contain', 'input-group--has-error');
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorError}`);
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label').should('have.css', 'color', colorFont);
+        // @ts-ignore
+        cy.get('.radio-group__option:nth-child(3) .radio-group__option-label .radio').after('background-color').should('eq', colorError);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').should('have.css', 'border', `2px solid ${colorError}`);
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label').should('have.css', 'color', colorFont);
+        // @ts-ignore
+        cy.get('.radio-group__option:nth-child(4) .radio-group__option-label .radio').after('background-color').should('eq', 'rgba(0, 0, 0, 0)');
+        cy.get('.radio-group.input-group--has-error .form__error').should('have.css', 'display', 'block');
+    });
+});
 
 export {}
