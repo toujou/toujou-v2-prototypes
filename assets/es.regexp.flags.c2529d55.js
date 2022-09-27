5a66b0f278a40721e17068f616e05fa5721f68f8
Luis Pato <lpato@dfau.de>
2022-09-27 13:46:34 +0200

    Updates

---
+++ assets/es.regexp.flags.c2529d55.js
@@ -0,0 +1,2 @@
+import{a5 as e,a6 as a,a7 as t,a8 as s}from"./iframe.b52a91b6.js";var o=e,i=a,l=t,g=s,r=RegExp.prototype,c=o&&g(function(){return Object.getOwnPropertyDescriptor(r,"flags").get.call({dotAll:!0,sticky:!0})!=="sy"});c&&i(r,"flags",{configurable:!0,get:l});
+//# sourceMappingURL=es.regexp.flags.c2529d55.js.map
