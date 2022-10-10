9b69684a72d27fe1391889d703078e07e80d35a1
Luis Pato <lpato@dfau.de>
2022-10-10 12:27:36 +0200

    feat[kojo]: Add story, styles and docs for the content card with text element Card: https://app.clickup.com/t/30xtztm

---
+++ src/stories/02. elements/content-card-with-text/content-card-with-text.stories.ts
@@ -0,0 +1,87 @@
+import { Story, Meta } from '@storybook/web-components';
+import { withXD } from "storybook-addon-xd-designs";
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+
+// @ts-ignore
+import contentCardWithTextDocs from '../content-card-with-text/content-card-with-text.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/Content Card',
+    decorators: [withXD],
+    parameters: {
+        design: {
+            artboardUrl: 'https://xd.adobe.com/view/3d1d0926-79e2-4ae4-aa1c-162dcd451a3a-684e/screen/f783e300-5752-4a6a-8ed9-05047ec22750/Desktop',
+        },
+        badges: [TOUJOU_BADGES.DONE],
+        docs: {
+            page: contentCardWithTextDocs,
+        }
+    },
+    argTypes: {
+        direction: {
+            table: {
+                category: "Content card settings",
+                defaultValue: { direction: 'left'},
+            },
+            name: 'Direction',
+            description: "Set the column where the text will be shown",
+            options: ['left', 'right'],
+            control: { type: 'radio' },
+            defaultValue: ['left'],
+            required: true,
+        },
+    }
+} as Meta;
+
+interface ContentCardWithTextStoryProps {
+    direction: string
+}
+
+function renderTextColumn() {
+    return `
+        <h1>Please insert your Heading here!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis blandit turpis cursus in hac habitasse. Integer quis auctor elit sed vulputate mi sit amet mauris. Dictum at tempor commodo ullamcorper. At auctor urna nunc id cursus metus. Non blandit massa enim nec dui nunc mattis enim ut. Placerat vestibulum lectus mauris ultrices eros in cursus. Id porta nibh venenatis cras sed. Est velit egestas dui id ornare arcu odio ut. Praesent semper feugiat nibh sed pulvinar proin gravida. Nec dui nunc mattis enim ut tellus elementum sagittis. Urna duis convallis convallis tellus. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Adipiscing enim eu turpis egestas pretium. At elementum eu facilisis sed odio morbi quis commodo odio. Diam quis enim lobortis scelerisque fermentum dui faucibus. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Sed faucibus turpis in eu mi bibendum neque egestas. Elementum eu facilisis sed odio morbi quis. Quisque id diam vel quam elementum pulvinar etiam non quam.</p>
+    `;
+}
+
+function renderCardColumn() {
+    return `
+        <toujou-content-card-grid class="content-card-grid" fullwidth>
+            <a href="/" class="content-card" card-direction="vertical" card-variant="default" fullwidth>
+                <figure class="content-card__figure">
+                    <img class="content-card__image" src="https://picsum.photos/640/640">
+                </figure>
+    
+                <div class="content-card__content">
+                    <h3 class="content-card__title">Lorem Ipsum</h3>
+                    <p class=" content-card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <span class="content-card__button">
+                        <toujou-icon class="icon" icon-name="arrow-right" icon-color="primary"></toujou-icon>
+                        more
+                    </span>
+                </div>
+            </a>
+        </toujou-content-card-grid>
+    `;
+}
+
+const Template: Story<ContentCardWithTextStoryProps> = (args: ContentCardWithTextStoryProps) => {
+    return `
+        <toujou-grid class="grid" number-of-columns="2" grid-type="default" column-layout="default">
+             <toujou-grid-column class="grid-column">          
+                ${args.direction === 'left' ? renderTextColumn() : renderCardColumn()}
+            </toujou-grid-column>
+            <toujou-grid-column class="grid-column">
+                ${args.direction === 'left' ? renderCardColumn() : renderTextColumn()}
+            </toujou-grid-column>
+        </toujou-grid>
+    `;
+};
+
+export const ContentCardWithText = Template.bind({});
+
+ContentCardWithText.args = {
+    direction: 'left',
+}
+
