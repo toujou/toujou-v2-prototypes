573b2eb7b3b3db674551c9c50dddfa92b1e34971
Luis Pato <lpato@dfau.de>
2022-11-10 12:21:49 +0100

    chore[storybook]: Build assets Card: https://app.clickup.com/t/306y8ty

--- dist/js/toujou-details.js
+++ dist/js/toujou-details.js
@@ -1,4 +1,4 @@
-import{r as u,s as m,$ as c}from"./lit-element-f92abbea.js";import{n as p}from"./custom-element-fb5c5745.js";import{e as d}from"./property-19bdaa8a.js";import"./query-assigned-elements-39dd8e72.js";const h=u`
+import{r as u,s as c,$ as m}from"./lit-element-f92abbea.js";import{n as p}from"./custom-element-fb5c5745.js";import{e as n}from"./property-19bdaa8a.js";import"./query-assigned-elements-39dd8e72.js";const h=u`
   :host {
     --toujou-details-border: var(--border-width-normal) solid var(--color-font-light);
     
@@ -45,10 +45,13 @@ import{r as u,s as m,$ as c}from"./lit-element-f92abbea.js";import{n as p}from".
   
   @media (hover: hover) {
     .details__summary:hover {
+      --details-title-color: var(--details-title-active-color);
+      --details-chevron-color: var(--details-chevron-active-color);
+      
       cursor: pointer;
     }
   }
-`;var g=Object.defineProperty,v=Object.getOwnPropertyDescriptor,i=(t,s,r,a)=>{for(var e=a>1?void 0:a?v(s,r):s,l=t.length-1,n;l>=0;l--)(n=t[l])&&(e=(a?n(s,r,e):n(e))||e);return a&&e&&g(s,r,e),e};let o=class extends m{constructor(){super(),this.accordionParentTag="toujou-details-accordion",this.isAccordionItem=!1,this.detailsEl=null,this.isOpen=!1}connectedCallback(){super.connectedCallback(),this.isAccordionItem=!!this.closest(this.accordionParentTag),this.isAccordionItem&&this.dispatchEvent(new CustomEvent("toujou-details-connected",{bubbles:!0,composed:!0,detail:{detailsEl:this}}))}firstUpdated(){var t;this.detailsEl=this.shadowRoot.querySelector(".details"),this.isOpen=((t=this.detailsEl)==null?void 0:t.hasAttribute("open"))||!1}render(){return c`
+`;var v=Object.defineProperty,g=Object.getOwnPropertyDescriptor,i=(t,s,r,a)=>{for(var e=a>1?void 0:a?g(s,r):s,l=t.length-1,d;l>=0;l--)(d=t[l])&&(e=(a?d(s,r,e):d(e))||e);return a&&e&&v(s,r,e),e};let o=class extends c{constructor(){super(),this.accordionParentTag="toujou-details-accordion",this.isAccordionItem=!1,this.detailsEl=null,this.isOpen=!1}connectedCallback(){super.connectedCallback(),this.isAccordionItem=!!this.closest(this.accordionParentTag),this.isAccordionItem&&this.dispatchEvent(new CustomEvent("toujou-details-connected",{bubbles:!0,composed:!0,detail:{detailsEl:this}}))}firstUpdated(){var t;this.detailsEl=this.shadowRoot.querySelector(".details"),this.isOpen=((t=this.detailsEl)==null?void 0:t.hasAttribute("open"))||!1}render(){return m`
             <details class="details" ?open="${this.isOpen}" @toggle="${this._handleDetailsChange}">
                 <summary class="details__summary">
                     <slot name="summary"></slot>
@@ -58,5 +61,5 @@ import{r as u,s as m,$ as c}from"./lit-element-f92abbea.js";import{n as p}from".
                     <slot name="content"></slot>
                 </section>
             </details>
-        `}_handleDetailsChange(t){const s=t.target;this.isOpen=s.hasAttribute("open")||!1,this.isAccordionItem&&this.dispatchEvent(new CustomEvent("toujou-details-toggle",{bubbles:!0,composed:!0,detail:{detailsEl:this,state:this.isOpen}}))}};o.styles=[h];i([d({type:Boolean})],o.prototype,"isAccordionItem",2);i([d({type:HTMLDetailsElement})],o.prototype,"detailsEl",2);i([d({type:Boolean,reflect:!0,attribute:"is-open"})],o.prototype,"isOpen",2);o=i([p("toujou-details")],o);
+        `}_handleDetailsChange(t){const s=t.target;this.isOpen=s.hasAttribute("open")||!1,this.isAccordionItem&&this.dispatchEvent(new CustomEvent("toujou-details-toggle",{bubbles:!0,composed:!0,detail:{detailsEl:this,state:this.isOpen}}))}};o.styles=[h];i([n({type:Boolean})],o.prototype,"isAccordionItem",2);i([n({type:HTMLDetailsElement})],o.prototype,"detailsEl",2);i([n({type:Boolean,reflect:!0,attribute:"is-open"})],o.prototype,"isOpen",2);o=i([p("toujou-details")],o);
 //# sourceMappingURL=toujou-details.js.map
