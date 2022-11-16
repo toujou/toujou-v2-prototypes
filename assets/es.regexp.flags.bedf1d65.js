d9f2d394c904eda974c51f9b6edb984017e14d13
Luis Pato <lpato@dfau.de>
2022-11-16 06:58:47 +0100

    Updates

---
+++ assets/es.regexp.flags.bedf1d65.js
@@ -0,0 +1,2 @@
+import{a5 as e,a6 as a,a7 as t,a8 as s}from"./iframe.b9a3766e.js";var o=e,i=a,l=t,g=s,r=RegExp.prototype,c=o&&g(function(){return Object.getOwnPropertyDescriptor(r,"flags").get.call({dotAll:!0,sticky:!0})!=="sy"});c&&i(r,"flags",{configurable:!0,get:l});
+//# sourceMappingURL=es.regexp.flags.bedf1d65.js.map
