1f660499c68ac84b99f8f0e1903683f94806a67d
Luis Pato <lpato@dfau.de>
2022-11-28 10:11:13 +0100

    test[storybook]: Improve details element e2e tests Card: https://app.clickup.com/t/2mybka8

--- src/tests/details/details.cy.ts
+++ src/tests/details/details.cy.ts
@@ -9,53 +9,53 @@ describe('toujou-details', () => {
 
     it('opens / closes on title click', () => {
         /* open on title click */
-        cy.get('toujou-details').invoke('attr', 'is-open').should('not.exist');
-        cy.get('.details__title').click();
+        cy.get('#details-1').invoke('attr', 'is-open').should('not.exist');
+        cy.get(`#details-1 .details__title`).click();
         cy.get('toujou-details').invoke('attr', 'is-open').should('exist');
 
         /* close on title click */
-        cy.get('.details__title').click();
-        cy.get('toujou-details').invoke('attr', 'is-open').should('not.exist');
+         cy.get('#details-1 .details__title').click();
+         cy.get('#details-1').invoke('attr', 'is-open').should('not.exist');
     })
 
     it('opens / closes on chevron click', () => {
         /* open on chevron click */
-        cy.get('toujou-details').invoke('attr', 'is-open').should('not.exist')
-        cy.get('toujou-icon').click();
-        cy.get('toujou-details').invoke('attr', 'is-open').should('exist')
+        cy.get('#details-1').invoke('attr', 'is-open').should('not.exist')
+        cy.get('#details-1 toujou-icon').click();
+        cy.get('#details-1').invoke('attr', 'is-open').should('exist')
 
         /* close on chevron click */
-        cy.get('toujou-icon').click();
-        cy.get('toujou-details').invoke('attr', 'is-open').should('not.exist');
+        cy.get('#details-1 toujou-icon').click();
+        cy.get('#details-1').invoke('attr', 'is-open').should('not.exist');
     })
 
     it('opens / closes on list click', () => {
         /* open on chevron click */
-        cy.get('toujou-details').invoke('attr', 'is-open').should('not.exist');
-        cy.get('.details__subtitle ').click();
-        cy.get('toujou-details').invoke('attr', 'is-open').should('exist');
+        cy.get('#details-1').invoke('attr', 'is-open').should('not.exist');
+        cy.get('#details-1 .details__subtitle ').click();
+        cy.get('#details-1').invoke('attr', 'is-open').should('exist');
 
         /* close on chevron click */
-        cy.get('.details__subtitle ').click();
-        cy.get('toujou-details').invoke('attr', 'is-open').should('not.exist');
+        cy.get('#details-1 .details__subtitle ').click();
+        cy.get('#details-1').invoke('attr', 'is-open').should('not.exist');
     })
 
     it('title has correct color when closed', () => {
-        cy.get('.details__title').should('have.css', 'color', colors.colorFontDark);
+        cy.get('#details-1 .details__title').should('have.css', 'color', colors.colorFontDark);
     })
 
     it('title has correct color when open', () => {
-        cy.get('.details__title').click();
-        cy.get('.details__title').should('have.css', 'color', colors.colorPrimary);
+        cy.get('#details-1 .details__title').click();
+        cy.get('#details-1 .details__title').should('have.css', 'color', colors.colorPrimary);
     })
 
     it('chevron has correct color when closed', () => {
-        cy.get('.details__chevron').should('have.css', 'background-color', colors.colorFontDark);
+        cy.get('#details-1 .details__chevron').should('have.css', 'background-color', colors.colorFontDark);
     })
 
     it('chevron has correct color when open', () => {
-        cy.get('.details__title').click();
-        cy.get('.details__chevron').should('have.css', 'background-color', colors.colorPrimary);
+        cy.get('#details-1 .details__title').click();
+        cy.get('#details-1 .details__chevron').should('have.css', 'background-color', colors.colorFontDark);
     })
 })
 
