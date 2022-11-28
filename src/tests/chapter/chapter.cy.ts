08889bd1617d9999edb9b32690c85ed14f528138
Luis Pato <lpato@dfau.de>
2022-11-28 08:36:15 +0100

    test[storybook]: Add tests for the different chapter designs Card: https://app.clickup.com/t/2mybka8

--- src/tests/chapter/chapter.cy.ts
+++ src/tests/chapter/chapter.cy.ts
@@ -1,14 +1,15 @@
 /// <reference types="cypress" />
 
+const colors = Cypress.env('colors');
+
 describe('chapter', () => {
     beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter');
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:default');
     });
 
     it('has correct class and tag name', () => {
         cy.get('section').should('have.prop', 'tagName').should('eq', 'SECTION');
-        cy.get('section').invoke('attr', 'class').should('eq', 'chapter');
-        cy.get('section').invoke('attr', 'chapter-variant').should('exist');
+        cy.get('section').invoke('attr', 'class').should('eq', 'chapter chapter--default');
     });
 
     it('has correct styles', () => {
@@ -16,9 +17,146 @@ describe('chapter', () => {
             expect(chapter).to.have.css('padding-top').equal('120px');
             expect(chapter).to.have.css('padding-bottom').equal('120px');
             expect(chapter).to.have.css('margin').equal('0px');
-            const chapterCS = getComputedStyle(chapter[0]);
-            const chapterBgColorVar = chapterCS.getPropertyValue('--chapter-background-color');
-            expect(chapterBgColorVar).to.equal(' transparent');
+        });
+    });
+});
+
+describe('chapter default', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:default');
+    });
+
+    it('has correct default styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorPrimary);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorFont);
+        });
+    });
+});
+
+describe('chapter primary', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:primary');
+    });
+
+    it('has correct primary styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorPrimary);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorBg);
+        });
+    });
+});
+
+describe('chapter secondary', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:secondary');
+    });
+
+    it('has correct primary styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorSecondary);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorBg);
+        });
+    });
+});
+
+describe('chapter inverted', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:inverted');
+    });
+
+    it('has correct primary styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorFont);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorBg);
+        });
+    });
+});
+
+describe('chapter none', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:none');
+    });
+
+    it('has correct primary styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorPrimary);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorFont);
+        });
+    });
+});
+
+describe('chapter white', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:white');
+    });
+
+    it('has correct primary styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorPrimary);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorFont);
+        });
+    });
+});
+
+describe('chapter background', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-chapter--chapter&args=variant:background');
+    });
+
+    it('has correct primary styles', () => {
+        cy.get('.chapter').then((chapter) => {
+            expect(chapter).to.have.css('background-color').equal(colors.colorBg);
+        });
+
+        cy.get('.chapter h1').then((h1) => {
+            expect(h1).to.have.css('color').equal(colors.colorPrimary);
+        });
+
+        cy.get('.chapter p').then((text) => {
+            expect(text).to.have.css('color').equal(colors.colorFont);
         });
     });
 });
