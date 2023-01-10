0dd14bc5867afc6e9c581a6836a625f2f81bae8b
Luis Pato <lpato@dfau.de>
2023-01-10 12:53:04 +0100

    fix[storybook]: Remove extra margin from p element when they are first child on text blocks or teaser elements Card: https://app.clickup.com/t/861m3knxm

--- src/stories/02. elements/text-block/text-block-media-below.stories.ts
+++ src/stories/02. elements/text-block/text-block-media-below.stories.ts
@@ -45,7 +45,7 @@ const Template: Story<TextBlockMediaBelowStoryProps> = (args: TextBlockMediaBelo
                     <img src="https://picsum.photos/1200/1200" alt="beautiful image" class="text-block__image text-block__image--${args.imageAspectRatio}">
                 </figure>
                 <div class="text-block__content">
-                    <h3>Left aligned H3 headline</h3>
+<!--                    <h3>Left aligned H3 headline</h3>-->
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </toujou-text-block-column>
--- src/stories/02. elements/text-block/text-block-media.stories.ts
+++ src/stories/02. elements/text-block/text-block-media.stories.ts
@@ -46,7 +46,7 @@ const Template: Story<TextBlocksMediaStoryProps> = (args: TextBlocksMediaStoryPr
                     <figcaption class="text-block__figcaption">This is a figcaption for the image</figcaption>
                 </figure>
                 <div class="text-block__content">
-                    <h3>Left aligned H3 headline</h3>
+<!--                    <h3>Left aligned H3 headline</h3>-->
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </toujou-text-block-column>
