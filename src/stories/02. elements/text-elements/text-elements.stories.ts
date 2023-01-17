5ad510b9d8687ed3a701dab63bad643bdbd5d212
Luis Pato <lpato@dfau.de>
2023-01-17 13:04:01 +0100

    feat[storybook]: Add correct indent to lists Card: https://app.clickup.com/t/861m7n5q8

--- src/stories/02. elements/text-elements/text-elements.stories.ts
+++ src/stories/02. elements/text-elements/text-elements.stories.ts
@@ -44,6 +44,52 @@ const Template: Story<TableStoryProps> = (args: TableStoryProps) => {
                 </div>
             </toujou-text-block-column>
         </toujou-text-block>
+
+        <toujou-text-block class="text-block" text-block-column-count="1">
+            <toujou-text-block-column class="text-block-column">
+                <div class="text-block__content">
+                    <ul>
+                        <li>item one</li>
+                        <li>item two</li>
+                        <li>item three lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                        <li>item four
+                            <ul>
+                                <li>item four a</li>
+                                <li>item four b</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                                <li>item four c</li>
+                                <li>item four d</li>
+                                <li>item four e</li>
+                            </ul>
+                        </li>
+                        <li>item five</li>
+                    </ul>
+                    
+                    <ol>
+                        <li>item one</li>
+                        <li>item two</li>
+                        <li>item three lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                        <li>item four
+                            <ol>
+                                <li>item four a</li>
+                                <li>item four b</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                                <li>item four c</li>
+                                <li>item four d</li>
+                                <li>item four e</li>
+                            </ol>
+                        </li>
+                        <li>item five</li>
+                    </ol>
+                    
+                    <p>dsfhsdfhsfgjdfgj</p>
+                    <p>dsfhsdfhsfgjdfgj</p>
+                    <p>dsfhsdfhsfgjdfgj</p>
+                    <p>dsfhsdfhsfgjdfgj</p>
+                   
+                </div>
+            </toujou-text-block-column>
+        </toujou-text-block>
         
         <toujou-table-container class="table-container">
             <table text-alignment="${args.tableAlignment}" class="table">
