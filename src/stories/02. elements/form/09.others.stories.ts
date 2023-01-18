f313d59ef45bca9bdfae81ab8bbe652977f14396
Luis Pato <lpato@dfau.de>
2023-01-18 07:40:28 +0100

    feat[storybook]: Add correct markup to the fieldset element Card: https://app.clickup.com/t/861m8acf6

--- src/stories/02. elements/form/09.others.stories.ts
+++ src/stories/02. elements/form/09.others.stories.ts
@@ -67,7 +67,7 @@ const Template: Story<ToujouOtherInputStoryProps> = (args: ToujouOtherInputStory
                    <span class="pristine-error form__error">The given subject was empty.</span>
         	</toujou-input-group>
         	
-        	<toujou-input-group class="input-group inout-group--image-upload ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+        	<toujou-input-group class="input-group input-group--image-upload ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
 				<label class="input-label" for="testform-1000091-imageupload-1">Image upload</label>
 				<span class="input-description">This is a description</span>
 		        <input data-pristine-required-message-de="The given subject was empty."
@@ -98,6 +98,10 @@ const Template: Story<ToujouOtherInputStoryProps> = (args: ToujouOtherInputStory
                 </select>
                <span class="pristine-error form__error">The given subject was empty.</span>
 	        </toujou-input-group>
+	        
+	        <toujou-input-group class="input-group inout-group--static-text">
+	            <p class="form-static-text">This is static text! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </toujou-input-group>
     
         </form>
     `
