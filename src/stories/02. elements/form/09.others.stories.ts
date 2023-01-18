1b13b2881c00447b708cf1a7c4b501594d8fea2b
Luis Pato <lpato@dfau.de>
2023-01-18 07:47:51 +0100

    feat[storybook]: Add story and style the form static text element Card: https://app.clickup.com/t/861m8acf6

--- src/stories/02. elements/form/09.others.stories.ts
+++ src/stories/02. elements/form/09.others.stories.ts
@@ -100,7 +100,9 @@ const Template: Story<ToujouOtherInputStoryProps> = (args: ToujouOtherInputStory
 	        </toujou-input-group>
 	        
 	        <toujou-input-group class="input-group inout-group--static-text">
-	            <p class="form-static-text">This is static text! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	            <div class="form-static-text">
+	                <p class="form-static-text__text">This is static text! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </toujou-input-group>
     
         </form>
