95fd19e14343356e95383df87637591edfb2fc78
Luis Pato <lpato@dfau.de>
2023-01-23 09:00:30 +0100

    Updates

--- assets/es.regexp.flags-d7a1640d.js
+++
@@ -1,2 +0,0 @@
-import{d as e,aQ as t,K as a}from"./index-9c2660ab.js";import{ak as s}from"./index-681e4b07-2bc868fc.js";var o=e,i=s,l=t,g=a,r=RegExp.prototype,p=o&&g(function(){return Object.getOwnPropertyDescriptor(r,"flags").get.call({dotAll:!0,sticky:!0})!=="sy"});p&&i(r,"flags",{configurable:!0,get:l});
-//# sourceMappingURL=es.regexp.flags-d7a1640d.js.map
