a4dd897a03a15f1ecb36afa4d5257f1046fec833
Luis Pato <lpato@dfau.de>
2023-01-24 13:12:40 +0100

    feat[storybook]: Prevent click on open media-info from triggering click on the element behind it Card: https://app.clickup.com/t/863fxk3b2

--- src/components/toujou-media-info/toujou-media-info.ts
+++ src/components/toujou-media-info/toujou-media-info.ts
@@ -11,14 +11,38 @@ export class ToujouMediaInfo extends LitElement {
 
     render() {
         return html`
-            <slot name="open-button" class="open-button" @click="${this._toggleIsOpen}"></slot>
-            <slot name="close-button" class="close-button" @click="${this._toggleIsOpen}"></slot>
+            <slot name="open-button" class="open-button"></slot>
+            <slot name="close-button" class="close-button"></slot>
             <slot name="figcaption"></slot>
             <slot name="copyright"></slot>
         `
     }
 
-    private _toggleIsOpen() {
+    constructor() {
+        super();
+
+        this.addEventListener('click', this._handleClick);
+    }
+
+    /**
+     * Handle click on whole element to prevent click on element behind when open
+     * @param event
+     */
+    _handleClick = (event: any): void => {
+        event.preventDefault();
+        event.stopPropagation();
+        const clickedTarget = event.target as HTMLElement;
+
+        if (clickedTarget.closest('[slot="open-button"], [slot="close-button"]')) {
+            this._toggleIsOpen();
+        }
+    }
+
+    /**
+     * Toggle to "isOpen" property, which also reflects as an attribute on the element
+     * @private
+     */
+    private _toggleIsOpen(): void {
         this.isOpen = !this.isOpen;
     }
 }
