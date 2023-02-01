0494040e6c068c13c802164e2e21a4396326ebc6
Luis Pato <lpato@dfau.de>
2023-02-01 14:07:22 +0100

    fix(storybook): Add correct colors to the modal close button Card: https://app.clickup.com/t/863fz7h5m

--- src/stories/02. elements/modal/modal.stories.ts
+++ src/stories/02. elements/modal/modal.stories.ts
@@ -23,7 +23,7 @@ export default {
 const Template = () => {
     return `
         <h1>MODAL</h1>
-        <p>click on this <a href="iframe.html?viewMode=story&id=pages-modal-content--modal-content" target="toujou-modal">link</a> to open the content of the "Modal Content" page on a modal window.</p>
+        <p>click on this <a href="iframe.html?viewMode=story&id=tests-modal-content--modal-content" target="toujou-modal">link</a> to open the content of the "Modal Content" page on a modal window.</p>
     `;
 };
 
