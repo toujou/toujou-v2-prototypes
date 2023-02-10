200febf679b197b28c89653d92b3e0f9b978c0cb
Luis Pato <lpato@dfau.de>
2023-02-10 11:51:18 +0100

    feat(storybook): Add blog header story Card: https://app.clickup.com/t/861mb63nt

---
+++ src/stories/02. elements/blog-plugins/blog-plugin-header.stories.ts
@@ -0,0 +1,31 @@
+import { Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+
+// @ts-ignore
+import blogPluginsDocs from './blog-plugins.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/Blog Plugin',
+    parameters: {
+        badges: [TOUJOU_BADGES.TESTING],
+        docs: {
+            page: blogPluginsDocs,
+        },
+        layout: 'fullwidth'
+    },
+} as Meta;
+
+const Template = () => {
+    return `
+        <header class="header blog-header" image-height="default">
+            <img class="header__image" src="https://picsum.photos/2000" alt="quote image">
+
+            <div class="blog-header__content">
+                <h1 class="blog-header__content-title">This is the blog title</h1>
+            </div>            
+        </header>
+    `;
+};
+
+export const Header = Template.bind({});
