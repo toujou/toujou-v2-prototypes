c31a822c23b8515bc82786bd416a9f9481abb633
Luis Pato <lpato@dfau.de>
2023-02-13 07:13:02 +0100

    feat(storybook): Improve the blog plugin author styles, build assets Card: https://app.clickup.com/t/861mbvxh8

--- src/stories/02. elements/blog-plugins/blog-plugin-authors.stories.ts
+++ src/stories/02. elements/blog-plugins/blog-plugin-authors.stories.ts
@@ -45,22 +45,23 @@ const Template: Story<BlogPluginAuthorsStoryProps> = (args: BlogPluginAuthorsSto
                 <div class="post-author__meta">
                     <span class="post-author__name" itemprop="name">Han Solo</span>
                     <span class="post-author__title" itemprop="jobTitle">Pilot</span>
+                    <span class="post-author__meta-separator">/</span>
                     <span class="post-author__location" itemprop="homeLocation">Tatooine</span>
                 </div>
                 <p class="post-author__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
                 <toujou-social-media-bar class="social-media-bar post-author__socials" element-design="default" no-headline inline no-margins>
                     <div class="social-media-bar__links">
                         <a href="#" class="social-media-bar__link" aria-label="Our facebook account">
-                            <toujou-icon class="icon" icon-name="sm-facebook" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-facebook" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                         <a href="#" class="social-media-bar__link" aria-label="Our twitter account">
-                            <toujou-icon class="icon" icon-name="sm-twitter" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-twitter" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                         <a href="#" class="social-media-bar__link" aria-label="Our instagram account">
-                            <toujou-icon class="icon" icon-name="sm-instagram" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-instagram" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                         <a href="#" class="social-media-bar__link" aria-label="Our youtube account">
-                            <toujou-icon class="icon" icon-name="sm-youtube" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-youtube" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                     </div>
                 </toujou-social-media-bar>
@@ -75,22 +76,23 @@ const Template: Story<BlogPluginAuthorsStoryProps> = (args: BlogPluginAuthorsSto
                 <div class="post-author__meta">
                     <span class="post-author__name" itemprop="name">Han Solo</span>
                     <span class="post-author__title" itemprop="jobTitle">Pilot</span>
+                    <span class="post-author__meta-separator">/</span>
                     <span class="post-author__location" itemprop="homeLocation">Tatooine</span>
                 </div>
                 <p class="post-author__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
                 <toujou-social-media-bar class="social-media-bar post-author__socials" element-design="default" no-headline inline no-margins>
                     <div class="social-media-bar__links">
                         <a href="#" class="social-media-bar__link" aria-label="Our facebook account">
-                            <toujou-icon class="icon" icon-name="sm-facebook" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-facebook" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                         <a href="#" class="social-media-bar__link" aria-label="Our twitter account">
-                            <toujou-icon class="icon" icon-name="sm-twitter" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-twitter" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                         <a href="#" class="social-media-bar__link" aria-label="Our instagram account">
-                            <toujou-icon class="icon" icon-name="sm-instagram" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-instagram" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                         <a href="#" class="social-media-bar__link" aria-label="Our youtube account">
-                            <toujou-icon class="icon" icon-name="sm-youtube" icon-size="xl" icon-color="primary" aria-hidden=""></toujou-icon>
+                            <toujou-icon class="icon" icon-name="sm-youtube" icon-size="l" icon-color="primary" aria-hidden=""></toujou-icon>
                         </a>
                     </div>
                 </toujou-social-media-bar>
