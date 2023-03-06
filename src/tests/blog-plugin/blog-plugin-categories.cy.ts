d3ae36a1cbdcaeef4c994e5853168c05617f098f
Luis Pato <lpato@dfau.de>
2023-03-06 14:27:56 +0100

    test(storybook): Add e2e tests for the blog plugin categories list Card: https://app.clickup.com/t/2mybka8

---
+++ src/tests/blog-plugin/blog-plugin-categories.cy.ts
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('blog plugin: categories', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-blog-plugin--categories');
+        // @ts-ignore
+        cy.get('body').resetRealHover();
+    });
+
+    it('has correct structure', () => {
+        cy.get('.chips-list.categories-list').should('exist');
+        cy.get('.chips-list.categories-list .chips-list__title').should('exist');
+        cy.get('.chips-list.categories-list .chips-list__list').should('exist');
+        cy.get('.chips-list.categories-list .chips-list__list').children('.chips-list__item').should('have.length', 4);
+    });
+
+    it.only('has correct list styles', () => {
+        cy.get('.chips-list__list').should('have.css', 'list-style', 'outside none none');
+        cy.get('.chips-list__list').should('have.css', 'display', 'flex');
+        cy.get('.chips-list__list').should('have.css', 'flex-wrap', 'wrap');
+        cy.get('.chips-list__list').should('have.css', 'gap', '16px');
+        cy.get('.chips-list__list').should('have.css', 'margin', '0px');
+        cy.get('.chips-list__list').should('have.css', 'hyphens', 'auto');
+    });
+});
+
+export {}
