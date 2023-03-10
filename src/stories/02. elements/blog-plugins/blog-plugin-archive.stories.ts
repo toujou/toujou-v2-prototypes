a4ea878b3d21411b6cfa274c38e196d26e7d9902
Luis Pato <lpato@dfau.de>
2023-03-10 11:40:08 +0100

    test(storybook): Add a11y test to the blog-plugin archive Card: https://app.clickup.com/t/2mybka8

--- src/stories/02. elements/blog-plugins/blog-plugin-archive.stories.ts
+++ src/stories/02. elements/blog-plugins/blog-plugin-archive.stories.ts
@@ -21,42 +21,42 @@ const Template = () => {
             <h3 class="blog-archive__title">Archive</h3>
             <div class="blog-archive__years">
                 <div class="archive-year">
-                        <h6 class="archive-year__title">2022</h6>
-                        <ul class="archive-year__list">
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">Februar (2)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">Januar (5)</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="archive-year">
-                        <h6 class="archive-year__title">2021</h6>
-                        <ul class="archive-year__list">
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">Dezember (2)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">November (11)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">Oktober (4)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">September (17)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">August (3)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">Juli (8)</a>
-                            </li>
-                            <li class="archive-year__item">
-                                <a href="#" class="archive-year__link">Juni (12)</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <h4 class="archive-year__title">2022</h4>
+                    <ul class="archive-year__list">
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">Februar (2)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">Januar (5)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="archive-year">
+                    <h4 class="archive-year__title">2021</h4>
+                    <ul class="archive-year__list">
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">Dezember (2)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">November (11)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">Oktober (4)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">September (17)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">August (3)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">Juli (8)</a>
+                        </li>
+                        <li class="archive-year__item">
+                            <a href="#" class="archive-year__link">Juni (12)</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     `;
