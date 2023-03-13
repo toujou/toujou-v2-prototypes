10325ec02030c9fb21ceceded8558bcbeb0a6833
Luis Pato <lpato@dfau.de>
2023-03-13 11:12:59 +0100

    test(storybook): Add a11y tests for the cover slider element Card: https://app.clickup.com/t/85zrr49wf

---
+++ src/tests/cover-slider/a11y/cover-slider.a11y.cy.ts
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-axe" />
+
+describe('cover slider a11y', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-cover-slider--cover-slider');
+        cy.injectAxe();
+    });
+
+    it('has no detectable a11y violation on load', () => {
+        cy.checkA11y('.cover-slider', {
+            rules: {
+                'aria-allowed-role': { enabled: false }
+            }
+        });
+    });
+})
+
+describe('cover slider primary a11y', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-cover-slider--cover-slider&args=elementDesign:primary');
+        cy.injectAxe();
+    });
+
+    it('has no detectable a11y violation on load', () => {
+        cy.checkA11y('.cover-slider', {
+            rules: {
+                'aria-allowed-role': { enabled: false }
+            }
+        });
+    });
+})
+
+describe('cover slider secondary a11y', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-cover-slider--cover-slider&args=elementDesign:secondary');
+        cy.injectAxe();
+    });
+
+    it('has no detectable a11y violation on load', () => {
+        cy.checkA11y('.cover-slider', {
+            rules: {
+                'aria-allowed-role': { enabled: false }
+            }
+        });
+    });
+})
+
+describe('cover slider inverted a11y', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-cover-slider--cover-slider&args=elementDesign:inverted');
+        cy.injectAxe();
+    });
+
+    it('has no detectable a11y violation on load', () => {
+        cy.checkA11y('.cover-slider', {
+            rules: {
+                'aria-allowed-role': { enabled: false }
+            }
+        });
+    });
+})
+
+
+export {}
