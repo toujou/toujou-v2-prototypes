e050c1e6128358715ef57c02befe590947939f80
Luis Pato <lpato@dfau.de>
2023-03-13 08:31:22 +0100

    test(storybook): Add a11y tests for the code-block element Card: https://app.clickup.com/t/85zrrc95n

---
+++ src/tests/code-block/a11y/code-block.a11y.cy.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-axe" />
+
+describe('code block a11y', () => {
+    beforeEach(() => {
+        cy.visit('/iframe.html?viewMode=story&id=components-code-block--code-block');
+        cy.injectAxe();
+    });
+
+    it('has no detectable a11y violation on load', () => {
+        cy.checkA11y('.code-block');
+    });
+})
+
+
+export {}
