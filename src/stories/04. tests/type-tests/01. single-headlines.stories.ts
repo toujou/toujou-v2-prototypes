2532efb2d0395d9e0ae09c54535cdaee3e37471e
Luis Pato <lpato@dfau.de>
2023-05-22 09:53:19 +0200

    feat(storybook): Improve the type print styles Card: https://app.clickup.com/t/33kj3r6

--- src/stories/04. tests/type-tests/01. single-headlines.stories.ts
+++ src/stories/04. tests/type-tests/01. single-headlines.stories.ts
@@ -23,6 +23,7 @@ const Template = () => {
                     
                     <h5>Left aligned H5 headline</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, <a href="hhtp: sdgsdgsdgssdgs/dgs/dgs/g">consectetur adipiscing elit</a>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </toujou-text-block-column>
         </toujou-text-block>
