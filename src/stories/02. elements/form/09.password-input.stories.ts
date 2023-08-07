e1ea9d753ec576d43a1b764c0a7558c253be6b42
LuisPato <lpato@dfau.de>
2023-08-07 10:56:57 +0200

    feat(storybook): Add validation icons to the input-group-advanced-password

--- src/stories/02. elements/form/09.password-input.stories.ts
+++ src/stories/02. elements/form/09.password-input.stories.ts
@@ -74,6 +74,67 @@ const Template: Story<ToujouTextInputsStoryProps> = (args: ToujouTextInputsStory
                 </toujou-input-password-toggle>
                 <span class="pristine-error form__error">This is an error message!</span>
             </toujou-input-group>
+
+
+
+
+
+
+            <toujou-input-group class="input-group input-group-advanced-password input-group--advanced-password ${args.state === 'error' ? 'input-group--has-error' : ''} ${args.state === 'success' ? 'input-group--has-success' : ''} ${args.state === 'disabled' ? 'input-group--disabled' : ''}">
+                <div class="input-group-advanced-password__group">
+                    <label class="input-label" for="testform-2881-advancedpassword-1">Original password</label>
+                    <span class="input-description">This is the advanced password</span>
+
+                    <toujou-input-password-toggle class="input-password-toggle">
+                        <input
+                            placeholder="Enter matching passwords"
+                            data-element-type="AdvancedPassword"
+                            slot="input"
+                            class="input-medium form-control input input--text input--advanced-password input--password-origin"
+                            id="testform-2881-advancedpassword-1"
+                            type="password"
+                            name="tx_form_formframework[testform-2881][advancedpassword-1][password]"
+                            value=""
+                            ${disabledAttribute}
+                        >
+                        <button class="input-password-toggle__button input-password-toggle__button--show" slot="show-password-button" aria-label="Show password">
+                            <toujou-icon class="icon" icon-name="eye" icon-size="normal" icon-color="font"></toujou-icon>
+                        </button>
+                        <button class="input-password-toggle__button input-password-toggle__button--hide" slot="hide-password-button" aria-label="hide password">
+                            <toujou-icon class="icon" icon-name="eye-off" icon-size="normal" icon-color="font"></toujou-icon>
+                        </button>
+                    </toujou-input-password-toggle>
+                </div>
+
+                <div class="input-group-advanced-password__group">
+                    <label class="input-label" for="testform-2881-advancedpassword-1">Confirmation Password</label>
+                    <span class="input-description">This is the advanced password</span>
+
+                    <toujou-input-password-toggle class="input-password-toggle">
+                        <input
+                            placeholder="Enter matching passwords"
+                            data-element-type="AdvancedPassword"
+                            slot="input"
+                            class="input-medium form-control input input--text input--advanced-password input--password-confirmation"
+                            id="testform-2881-advancedpassword-1-confirmation"
+                            type="password"
+                            name="tx_form_formframework[testform-2881][advancedpassword-1][confirmation]"
+                            value=""
+                            ${disabledAttribute}
+                        >
+                        <button class="input-password-toggle__button input-password-toggle__button--show" slot="show-password-button" aria-label="Show password">
+                            <toujou-icon class="icon" icon-name="eye" icon-size="normal" icon-color="font"></toujou-icon>
+                        </button>
+                        <button class="input-password-toggle__button input-password-toggle__button--hide" slot="hide-password-button" aria-label="hide password">
+                            <toujou-icon class="icon" icon-name="eye-off" icon-size="normal" icon-color="font"></toujou-icon>
+                        </button>
+                    </toujou-input-password-toggle>
+
+                </div>
+
+                <span class="pristine-error form__error" style="display: none;"></span>
+            </toujou-input-group>
+
         </form>
     `;
 };
