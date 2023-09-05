95ad5305e8d55c16b9264f49125dc55a1eec010f
LuisPato <lpato@dfau.de>
2023-09-05 15:54:36 +0200

    Updates

---
+++ assets/preview-d366d8c6.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-032274c5.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-ce63866d.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-ce63866d.js","./iframe-032274c5.js","./index-e0ddb630.js","./_commonjsHelpers-87174ba5.js","./index-7e1fd7c9.js","./_commonjs-dynamic-modules-7a9bd914.js","./index-d475d2ea.js","./index-d37d4223.js","./index-ce357362.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-d366d8c6.js.map
