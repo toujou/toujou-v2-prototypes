d90fbd968cab305b98ab473428f811e4c3ef5cb0
LuisPato <lpato@dfau.de>
2023-11-16 10:47:52 +0100

    feat: Add correct content and style the event-schedule on mobile, tablet and desktop Card: https://app.clickup.com/t/86bwcht5d

--- src/stories/02. elements/event-schedule/event-schedule.stories.ts
+++ src/stories/02. elements/event-schedule/event-schedule.stories.ts
@@ -36,20 +36,20 @@ const Template = () => {
                     <ul class="event-schedule-day__list">
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
-                                <p class="event-schedule-item__time">10:30 - 18:30</p>
+                                <p class="event-schedule-item__time">von 10:30</p>
                                 <h4 class="event-schedule-item__title">Hackathon for freedom <span class="event-schedule-item__title-step">(1/3)</span></h4>
                             </a>
                         </li>
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
                                 <p class="event-schedule-item__time">von 08:30</p>
-                                <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(1/4)</span></h4>
+                                <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(1/2)</span></h4>
                             </a>
                         </li>
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
                                 <p class="event-schedule-item__time">bis 18:30</p>
-                                <h4 class="event-schedule-item__title">Ted Talk: Reef fish and pollution</h4>
+                                <h4 class="event-schedule-item__title">Ted Talk: Reef fish and pollution causing the decline of nature</h4>
                                 <toujou-chip class="chip event-schedule-item__chip" chip-size="extra-small" chip-bg-color="error">Abgesagt</toujou-chip>
                             </a>
                         </li>
@@ -61,7 +61,7 @@ const Template = () => {
                         <time class="calendar-page" datetime="2025-11-14" single-day>
                             <div class="calendar-page__date">
                                 <p class="calendar-page__month">Nov</p>
-                                <p class="calendar-page__day">14</p>
+                                <p class="calendar-page__day">15</p>
                             </div>
                         </time>
                     </div>
@@ -69,14 +69,40 @@ const Template = () => {
                     <ul class="event-schedule-day__list">
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
-                                <p class="event-schedule-item__time">10:30 - 18:30</p>
+                                <p class="event-schedule-item__time">All day</p>
                                 <h4 class="event-schedule-item__title">Hackathon for freedom <span class="event-schedule-item__title-step">(2/3)</span></h4>
                             </a>
                         </li>
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
-                                <p class="event-schedule-item__time">von 08:30</p>
-                                <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(2/4)</span></h4>
+                                <p class="event-schedule-item__time">bis 20:30</p>
+                                <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(2/2)</span></h4>
+                            </a>
+                        </li>
+                    </ul>
+                </event-schedule-day>
+
+                <event-schedule-day class="event-schedule-day" role="list">
+                    <div class="calendar-pages event-schedule-day__calendar-page" aria-label="calendar page with date / time information">
+                        <time class="calendar-page" datetime="2025-11-14" single-day>
+                            <div class="calendar-page__date">
+                                <p class="calendar-page__month">Nov</p>
+                                <p class="calendar-page__day">16</p>
+                            </div>
+                        </time>
+                    </div>
+
+                    <ul class="event-schedule-day__list">
+                        <li class="event-schedule-day__item">
+                            <a href="#" class="event-schedule-item">
+                                <p class="event-schedule-item__time">bis 16:30</p>
+                                <h4 class="event-schedule-item__title">Hackathon for freedom <span class="event-schedule-item__title-step">(3/3)</span></h4>
+                            </a>
+                        </li>
+                        <li class="event-schedule-day__item">
+                            <a href="#" class="event-schedule-item">
+                                <p class="event-schedule-item__time">08:30 - 14:30</p>
+                                <h4 class="event-schedule-item__title">Discussion group for the new house</h4>
                             </a>
                         </li>
                     </ul>
@@ -91,8 +117,8 @@ const Template = () => {
                     <div class="calendar-pages event-schedule-day__calendar-page" aria-label="calendar page with date / time information">
                         <time class="calendar-page" datetime="2025-11-14" single-day>
                             <div class="calendar-page__date">
-                                <p class="calendar-page__month">Nov</p>
-                                <p class="calendar-page__day">14</p>
+                                <p class="calendar-page__month">Jan</p>
+                                <p class="calendar-page__day">04</p>
                             </div>
                         </time>
                     </div>
@@ -100,7 +126,7 @@ const Template = () => {
                     <ul class="event-schedule-day__list">
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
-                                <p class="event-schedule-item__time">10:30 - 18:30</p>
+                                <p class="event-schedule-item__time">von 10:300</p>
                                 <h4 class="event-schedule-item__title">Hackathon for freedom <span class="event-schedule-item__title-step">(1/3)</span></h4>
                             </a>
                         </li>
@@ -113,7 +139,7 @@ const Template = () => {
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
                                 <p class="event-schedule-item__time">bis 18:30</p>
-                                <h4 class="event-schedule-item__title">Ted Talk: Reef fish and pollution</h4>
+                                <h4 class="event-schedule-item__title">Ted Talk: Reef fish and pollution causing the decline of nature</h4>
                                 <toujou-chip class="chip event-schedule-item__chip" chip-size="extra-small" chip-bg-color="error">Abgesagt</toujou-chip>
                             </a>
                         </li>
@@ -124,8 +150,8 @@ const Template = () => {
                     <div class="calendar-pages event-schedule-day__calendar-page" aria-label="calendar page with date / time information">
                         <time class="calendar-page" datetime="2025-11-14" single-day>
                             <div class="calendar-page__date">
-                                <p class="calendar-page__month">Nov</p>
-                                <p class="calendar-page__day">14</p>
+                                <p class="calendar-page__month">Jan</p>
+                                <p class="calendar-page__day">05</p>
                             </div>
                         </time>
                     </div>
@@ -133,14 +159,60 @@ const Template = () => {
                     <ul class="event-schedule-day__list">
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
-                                <p class="event-schedule-item__time">10:30 - 18:30</p>
+                                <p class="event-schedule-item__time">All day</p>
                                 <h4 class="event-schedule-item__title">Hackathon for freedom <span class="event-schedule-item__title-step">(2/3)</span></h4>
                             </a>
                         </li>
                         <li class="event-schedule-day__item">
                             <a href="#" class="event-schedule-item">
-                                <p class="event-schedule-item__time">von 08:30</p>
+                                <p class="event-schedule-item__time">All day</p>
                                 <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(2/4)</span></h4>
+                            </a>
+                        </li>
+                    </ul>
+                </event-schedule-day>
+
+                <event-schedule-day class="event-schedule-day" role="list">
+                    <div class="calendar-pages event-schedule-day__calendar-page" aria-label="calendar page with date / time information">
+                        <time class="calendar-page" datetime="2025-11-14" single-day>
+                            <div class="calendar-page__date">
+                                <p class="calendar-page__month">Jan</p>
+                                <p class="calendar-page__day">06</p>
+                            </div>
+                        </time>
+                    </div>
+
+                    <ul class="event-schedule-day__list">
+                        <li class="event-schedule-day__item">
+                            <a href="#" class="event-schedule-item">
+                                <p class="event-schedule-item__time">bis 18:30</p>
+                                <h4 class="event-schedule-item__title">Hackathon for freedom <span class="event-schedule-item__title-step">(3/3)</span></h4>
+                            </a>
+                        </li>
+                        <li class="event-schedule-day__item">
+                            <a href="#" class="event-schedule-item">
+                                <p class="event-schedule-item__time">All day</p>
+                                <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(3/4)</span></h4>
+                            </a>
+                        </li>
+                    </ul>
+                </event-schedule-day>
+
+                <event-schedule-day class="event-schedule-day" role="list">
+                    <div class="calendar-pages event-schedule-day__calendar-page" aria-label="calendar page with date / time information">
+                        <time class="calendar-page" datetime="2025-11-14" single-day>
+                            <div class="calendar-page__date">
+                                <p class="calendar-page__month">Jan</p>
+                                <p class="calendar-page__day">07</p>
+                            </div>
+                        </time>
+                    </div>
+
+                    <ul class="event-schedule-day__list">
+                        <li class="event-schedule-day__item">
+                            <a href="#" class="event-schedule-item">
+                                <p class="event-schedule-item__time">bis 20:00</p>
+                                <h4 class="event-schedule-item__title">Guitar lessons for beginners <span class="event-schedule-item__title-step">(4/4)</span></h4>
                             </a>
                         </li>
                     </ul>
