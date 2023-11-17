77758b65076df860b5d8fdd402a497df1b6a4482
LuisPato <lpato@dfau.de>
2023-11-17 14:05:14 +0100

    Updates

--- assets/preview-2e8323c3.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-21069ddb.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-bd178972.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-bd178972.js","./iframe-21069ddb.js","./index-f1f749bf.js","./_commonjsHelpers-042e6b4d.js","./_commonjs-dynamic-modules-5fa47451.js","./index-d37d4223.js","./index-946e8720.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-2e8323c3.js.map
