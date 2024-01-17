ea1e7832029732b16d72d03ca6e9954c5c3a6369
LuisPato <lpato@dfau.de>
2024-01-17 11:08:27 +0100

    feat: Add story and style the separated-lists elements

---
+++ src/stories/02. elements/lists/lists.stories.ts
@@ -0,0 +1,43 @@
+import { Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+// @ts-ignore
+import ListsDocs from "./lists.docs.mdx";
+
+export default {
+    title: 'COMPONENTS/Lists',
+    parameters: {
+        badges: [TOUJOU_BADGES.DONE],
+        docs: {
+            page: ListsDocs,
+        }
+    },
+    tags: ['autodocs']
+} as Meta;
+
+const Template = () => {
+    return `
+        <h3>Default list</h3>
+        <ul>
+            <li>List item one</li>
+            <li>List item two</li>
+            <li>List item three</li>
+        </ul>
+
+        <h3>Separated list - default</h3>
+        <ul class="separated-list">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - comma</h3>
+        <ul class="separated-list" list-separator="comma">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+    `;
+};
+
+export const Lists = Template.bind({});
