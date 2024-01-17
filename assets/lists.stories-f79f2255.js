bc2e246f036fdfc59c24c85ca2ea0f5b0f2fca31
LuisPato <lpato@dfau.de>
2024-01-17 15:53:58 +0100

    Updates

---
+++ assets/lists.stories-f79f2255.js
@@ -0,0 +1,74 @@
+import{T as p}from"./badgeCustomConfig-e7a687d5.js";import{j as i}from"./jsx-runtime-ccada58e.js";import{u as m}from"./index-4fb8b842.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";function l(t){const e=Object.assign({h1:"h1"},m(),t.components);return i.jsx(e.h1,{id:"lists",children:"Lists"})}function o(t={}){const{wrapper:e}=Object.assign({},m(),t.components);return e?i.jsx(e,Object.assign({},t,{children:i.jsx(l,t)})):l(t)}const w={title:"COMPONENTS/Lists",parameters:{badges:[p.DONE],docs:{page:o}},tags:["autodocs"]},c=()=>`
+        <h3>Default list</h3>
+        <ul>
+            <li>List item one</li>
+            <li>List item two</li>
+            <li>List item three</li>
+        </ul>
+
+        <h3>Separated list - default</h3>
+        <ul class="separated-list">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - comma</h3>
+        <ul class="separated-list" list-separator="comma">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - pipe</h3>
+        <ul class="separated-list" list-separator="pipe">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - centered</h3>
+        <ul class="separated-list" list-separator="pipe" list-alignment="center">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+    `,s=c.bind({});var a,r,n;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  return \`
+        <h3>Default list</h3>
+        <ul>
+            <li>List item one</li>
+            <li>List item two</li>
+            <li>List item three</li>
+        </ul>
+
+        <h3>Separated list - default</h3>
+        <ul class="separated-list">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - comma</h3>
+        <ul class="separated-list" list-separator="comma">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - pipe</h3>
+        <ul class="separated-list" list-separator="pipe">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+
+        <h3>Separated list - centered</h3>
+        <ul class="separated-list" list-separator="pipe" list-alignment="center">
+            <li class="separated-list__item">List item one</li>
+            <li class="separated-list__item">List item two</li>
+            <li class="separated-list__item">List item three</li>
+        </ul>
+    \`;
+}`,...(n=(r=s.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const S=["Lists"];export{s as Lists,S as __namedExportsOrder,w as default};
+//# sourceMappingURL=lists.stories-f79f2255.js.map
