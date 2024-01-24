c5705f2c003880fb58db168904a33894614b1c72
LuisPato <lpato@dfau.de>
2024-01-24 15:43:51 +0100

    Updates

--- assets/preview-55aa2d29.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-b9997ac3.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-d6b2c84b.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-d6b2c84b.js","./iframe-b9997ac3.js","./index-f1f749bf.js","./_commonjsHelpers-042e6b4d.js","./_commonjs-dynamic-modules-5fa47451.js","./index-d37d4223.js","./index-946e8720.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-55aa2d29.js.map
