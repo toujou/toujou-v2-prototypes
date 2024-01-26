f5f8d922d231441f29c2f8fc935f9db419ab5c25
LuisPato <lpato@dfau.de>
2024-01-26 09:16:13 +0100

    Updates

--- assets/preview-0ec80966.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-fb4c016b.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-cd83994b.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-cd83994b.js","./iframe-fb4c016b.js","./index-f1f749bf.js","./_commonjsHelpers-042e6b4d.js","./_commonjs-dynamic-modules-5fa47451.js","./index-d37d4223.js","./index-946e8720.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-0ec80966.js.map
---
+++ assets/preview-516ddae8.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-7fdfc670.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-f48dbce0.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-f48dbce0.js","./iframe-7fdfc670.js","./index-f1f749bf.js","./_commonjsHelpers-042e6b4d.js","./_commonjs-dynamic-modules-5fa47451.js","./index-d37d4223.js","./index-946e8720.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-516ddae8.js.map
