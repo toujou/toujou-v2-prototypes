61e5da60c279ddc5e8d1ece7ec626f0f6822c411
LuisPato <lpato@dfau.de>
2024-04-09 15:23:33 +0200

    fix: Add correct indent to ul and ol elements depending on which parent they have

--- src/stories/04. tests/type-tests/04. type-spacing-tests.stories.ts
+++ src/stories/04. tests/type-tests/04. type-spacing-tests.stories.ts
@@ -45,6 +45,100 @@ const Template = () => {
                 <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
 
             </section>
+
+            <section class="chapter  chapter--textmedia-green chapter--no-padding-bottom" id="default-f195702f6bd322b59fa60d2c76d15e24">
+
+
+
+
+<span id="c2555"></span><h2 type-size="" type-color="green-group" type-alignment="left" data-da-header="true" data-da-id="135" data-da-next="136" data-da-prev="134" data-da-focused="false" data-da-down="136" data-da-up="134">
+                Was bieten wir?
+                </h2>
+\t
+
+
+
+
+
+
+<span id="c2557"></span>
+
+
+
+
+
+
+
+
+
+<toujou-text-block class="text-block" text-block-media-position="beside-right">
+    <toujou-text-block-column class="text-block-column">
+
+    <figure class="text-block__figure">
+
+
+
+
+
+
+
+
+            <img class="text-block__image" src="/fileadmin/_processed_/4/1/csm_image-1-aufwachsen-und-lernen_e2ef48c5b8.jpeg" width="433" height="289" data-width="2500" data-height="1667.1099415205" srcset="/fileadmin/_processed_/4/1/csm_image-1-aufwachsen-und-lernen_e2ef48c5b8.jpeg 433w,/fileadmin/_processed_/4/1/csm_image-1-aufwachsen-und-lernen_b288facd63.jpeg 456w,/fileadmin/_processed_/4/1/csm_image-1-aufwachsen-und-lernen_897b26af08.jpeg 593w,/fileadmin/_processed_/4/1/csm_image-1-aufwachsen-und-lernen_082f581330.jpeg 720w" sizes="(min-width: 960px) 720px,(min-width: 768px) 720px,(min-width: 480px) 720px,500px" data-focus-area="{&quot;x&quot;:833.3333333333333,&quot;y&quot;:712.3333333333333,&quot;width&quot;:833.3333333333333,&quot;height&quot;:712.3333333333333}" alt="" loading="lazy" data-da-img="true" data-da-id="136" data-da-next="137" data-da-prev="135" data-da-focused="false" data-da-down="146" data-da-up="135">
+
+
+
+
+
+
+
+
+    </figure>
+
+        <div class="text-block__content">
+            <p>Unser Familienzentrum ist&nbsp;ein&nbsp;Bildungs-, Begegnungs- und Erfahrungsort. Unser Ziel ist es, Kinder&nbsp;in&nbsp;ihrer Entwicklung zu begleiten und zu fördern und auch&nbsp;Eltern dabei Unterstützung anzubieten.&nbsp;</p><ul class="list" data-da-list="true" data-da-id="137" data-da-next="138" data-da-prev="136" data-da-focused="false"> \t<li data-da-list-item="true" data-da-id="138" data-da-next="139" data-da-prev="137" data-da-focused="false">Bildungsangebote</li> \t<li data-da-list-item="true" data-da-id="139" data-da-next="140" data-da-prev="138" data-da-focused="false">Informationsangebote</li> \t<li data-da-list-item="true" data-da-id="140" data-da-next="141" data-da-prev="139" data-da-focused="false">Beratungsangebote</li> \t<li data-da-list-item="true" data-da-id="141" data-da-next="142" data-da-prev="140" data-da-focused="false">Vortragsabende</li> \t<li data-da-list-item="true" data-da-id="142" data-da-next="143" data-da-prev="141" data-da-focused="false">Elterngruppen</li> \t<li data-da-list-item="true" data-da-id="143" data-da-next="144" data-da-prev="142" data-da-focused="false">Spielgruppen&nbsp;</li> </ul><p>Viele unserer Angebote sind kostenfrei und Sie können ohne Anmeldung teilnehmen. Für einige Kurse müssen wir aber eine Gebühr verlangen. Für kostenpflichtige Angebote melden Sie sich bitte mit unserem Anmeldeformular an.&nbsp;Für alle weiteren anmeldepflichtigen Veranstaltungen können Sie sich per Mail (<a href="mailto:familienzentrum-kim@lebenshilfe-ansbach.de" data-da-link-item="true" data-da-id="144" data-da-next="145" data-da-prev="143" data-da-focused="false" data-da-down="145" data-da-up="135">familienzentrum-kim@lebenshilfe-ansbach.de</a>) oder Telefon unter der 09852 61621-100 anmelden.&nbsp;</p>
+<p><a href="/fileadmin/Dokumente/kim-anmeldebogen-veranstaltungen.pdf" class="download-link-green" data-da-link-item="true" data-da-id="145" data-da-next="146" data-da-prev="144" data-da-focused="false" data-da-down="146" data-da-up="144"><strong data-da-skipelement="true">Anmeldebogen</strong>Veranstaltungen</a></p>
+        </div>
+    </toujou-text-block-column>
+</toujou-text-block>
+
+
+
+
+
+
+
+
+
+
+
+    </section>
+
+
+    <section class="chapter  chapter--big-drop-bg-right-green" id="default-270272d1ea0f9e9c314693f6f3fde8a0">
+
+
+
+
+<span id="c2553"></span><h2 type-size="" type-color="green-group" type-alignment="left" data-da-header="true" data-da-id="126" data-da-next="127" data-da-prev="125" data-da-focused="false" data-da-right="2" data-da-down="2" data-da-up="125">
+                Herzlich Willkommen im Familienzentrum KiM!
+                </h2>
+\t
+
+
+
+
+
+
+
+<span id="c2552"></span>
+
+<p>Im Haus befinden sich vier&nbsp;verschiedene&nbsp;Einrichtungen:</p><ul class="list" data-da-list="true" data-da-id="127" data-da-next="128" data-da-prev="126" data-da-focused="false"> \t<li data-da-list-item="true" data-da-skipelement="true" data-da-id="128" data-da-next="129" data-da-prev="127" data-da-focused="false"><a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank" data-da-ul-descendant="true" data-da-link-item="true" data-da-id="129" data-da-next="130" data-da-prev="128" data-da-focused="false" data-da-down="131" data-da-up="126">die Interdisziplinäre Frühförderstelle</a></li> \t<li data-da-list-item="true" data-da-skipelement="true" data-da-id="130" data-da-next="131" data-da-prev="129" data-da-focused="false"><a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank" data-da-ul-descendant="true" data-da-link-item="true" data-da-id="131" data-da-next="132" data-da-prev="130" data-da-focused="false" data-da-down="133" data-da-up="129">die inklusive Kindertageseinrichtung</a></li> \t<li data-da-list-item="true" data-da-skipelement="true" data-da-id="132" data-da-next="133" data-da-prev="131" data-da-focused="false">sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/" data-da-ul-descendant="true" data-da-link-item="true" data-da-id="133" data-da-next="134" data-da-prev="132" data-da-focused="false" data-da-right="134" data-da-down="135" data-da-up="131">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank" data-da-ul-descendant="true" data-da-link-item="true" data-da-id="134" data-da-next="135" data-da-prev="133" data-da-focused="false" data-da-down="135" data-da-left="133" data-da-up="126">Heilpädagogischer Tagesstätte</a>​​​​​​​.</li> </ul><p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+
+
+
+
+    </section>
         </main>
     `;
 };
