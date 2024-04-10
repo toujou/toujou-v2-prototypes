9d2df6203fd928a4326793bf4a6c51fba753efc4
LuisPato <lpato@dfau.de>
2024-04-10 09:19:15 +0200

    Updates

---
+++ assets/04. type-spacing-tests.stories-cc9910b1.js
@@ -0,0 +1,249 @@
+const s={title:"TESTS/Type Tests"},t=()=>`
+        <main id="main" skippy-links-label="Inhaltsbereich">
+
+            <section class="chapter  chapter--big-drop-bg-right-green" id="default-270272d1ea0f9e9c314693f6f3fde8a0">
+
+                <span id="c2553"></span>
+                <h2 type-size="" type-color="green-group" type-alignment="left">Herzlich Willkommen im Familienzentrum KiM!</h2>
+
+                <span id="c2552"></span>
+                <p>Im Haus befinden sich vier&nbsp;verschiedene&nbsp;Einrichtungen:</p>
+
+                <ul class="list">
+                    <li>
+                        <a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank">die Interdisziplinäre Frühförderstelle</a>
+                    </li>
+                    <li>
+                        <a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank">die inklusive Kindertageseinrichtung</a>
+                    </li>
+                    <li>
+                        sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank">Heilpädagogischer Tagesstätte</a>.
+                    </li>
+                </ul>
+
+                <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+                <ol class="list">
+                    <li>
+                        <a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank">die Interdisziplinäre Frühförderstelle</a>
+                    </li>
+                    <li>
+                        <a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank">die inklusive Kindertageseinrichtung</a>
+                    </li>
+                    <li>
+                        sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank">Heilpädagogischer Tagesstätte</a>.
+                    </li>
+                </ol>
+
+                <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+            </section>
+
+            <section class="chapter  chapter--textmedia-green chapter--no-padding-bottom" id="default-f195702f6bd322b59fa60d2c76d15e24">
+
+                <span id="c2555"></span>
+                <h2 type-size="" type-color="green-group" type-alignment="left">Was bieten wir?</h2>
+
+                <span id="c2557"></span>
+
+                <toujou-text-block class="text-block" text-block-media-position="beside-right">
+                    <toujou-text-block-column class="text-block-column">
+
+                        <figure class="text-block__figure">
+                            <img class="text-block__image" src="https://picsum.photos/">
+                        </figure>
+
+                        <div class="text-block__content">
+                            <p>Unser Familienzentrum ist&nbsp;ein&nbsp;Bildungs-, Begegnungs- und Erfahrungsort. Unser Ziel ist es, Kinder&nbsp;in&nbsp;ihrer Entwicklung zu begleiten und zu fördern und auch&nbsp;Eltern dabei Unterstützung anzubieten.&nbsp;</p><ul class="list" data-da-list="true" data-da-id="137" data-da-next="138" data-da-prev="136" data-da-focused="false"> 	<li data-da-list-item="true" data-da-id="138" data-da-next="139" data-da-prev="137" data-da-focused="false">Bildungsangebote</li> 	<li data-da-list-item="true" data-da-id="139" data-da-next="140" data-da-prev="138" data-da-focused="false">Informationsangebote</li> 	<li data-da-list-item="true" data-da-id="140" data-da-next="141" data-da-prev="139" data-da-focused="false">Beratungsangebote</li> 	<li data-da-list-item="true" data-da-id="141" data-da-next="142" data-da-prev="140" data-da-focused="false">Vortragsabende</li> 	<li data-da-list-item="true" data-da-id="142" data-da-next="143" data-da-prev="141" data-da-focused="false">Elterngruppen</li> 	<li data-da-list-item="true" data-da-id="143" data-da-next="144" data-da-prev="142" data-da-focused="false">Spielgruppen&nbsp;</li> </ul><p>Viele unserer Angebote sind kostenfrei und Sie können ohne Anmeldung teilnehmen. Für einige Kurse müssen wir aber eine Gebühr verlangen. Für kostenpflichtige Angebote melden Sie sich bitte mit unserem Anmeldeformular an.&nbsp;Für alle weiteren anmeldepflichtigen Veranstaltungen können Sie sich per Mail (<a href="mailto:familienzentrum-kim@lebenshilfe-ansbach.de" data-da-link-item="true" data-da-id="144" data-da-next="145" data-da-prev="143" data-da-focused="false" data-da-down="145" data-da-up="135">familienzentrum-kim@lebenshilfe-ansbach.de</a>) oder Telefon unter der 09852 61621-100 anmelden.&nbsp;</p>
+                            <p><a href="/fileadmin/Dokumente/kim-anmeldebogen-veranstaltungen.pdf" class="download-link-green" data-da-link-item="true" data-da-id="145" data-da-next="146" data-da-prev="144" data-da-focused="false" data-da-down="146" data-da-up="144"><strong data-da-skipelement="true">Anmeldebogen</strong>Veranstaltungen</a></p>
+                        </div>
+                    </toujou-text-block-column>
+                </toujou-text-block>
+
+            </section>
+
+            <section class="chapter  chapter--big-drop-bg-right-green" id="default-270272d1ea0f9e9c314693f6f3fde8a0">
+
+                <span id="c2553"></span>
+                <h2 type-size="" type-color="green-group" type-alignment="left">Herzlich Willkommen im Familienzentrum KiM!</h2>
+
+                <span id="c2552"></span>
+                <p>Im Haus befinden sich vier&nbsp;verschiedene&nbsp;Einrichtungen:</p>
+
+                <ul class="list">
+                    <li>
+                        <a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank">die Interdisziplinäre Frühförderstelle</a>
+                    </li>
+                    <li>
+                        <a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank" >die inklusive Kindertageseinrichtung</a>
+                    </li>
+                    <li>sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank">Heilpädagogischer Tagesstätte</a></li>
+                 </ul>
+                 <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+            </section>
+
+            <section class="chapter  chapter--default" id="default-874e50f6b51aaf5b238ffc4cb61907dd">
+
+                <span id="c2570"></span>
+
+                    <toujou-text-block class="text-block" text-block-column-count="2" text-block-media-position="">
+                        <toujou-text-block-column class="text-block-column">
+                            <div class="text-block__content">
+
+                                <h2><strong>Beratung und Unterstützung findet in&nbsp;folgenden Bereichen statt</strong></h2>
+
+                                <ul class="list">
+                                    <li>bei der Aufnahme und Gestaltung persönlicher, sozialer Beziehungen</li>
+                                    <li>bei der Selbstversorgung und beim Wohnen</li>
+                                    <li>bei der Tagesgestaltung, Freizeit und der Teilnahme am gesellschaftlichen Leben</li>
+                                    <li>beim Umgang mit den Auswirkungen der Beeinträchtigung</li>
+                                </ul>
+                        </div>
+                    </toujou-text-block-column>
+
+                    <toujou-text-block-column class="text-block-column">
+
+                        <div class="text-block__content">
+
+                            <h2><strong>Voraussetzungen für die Aufnahme ins Gemeinschaftliche Wohnen</strong></h2>
+
+                            <ul class="list">
+                                <li>Das Mindestalter beträgt 18 Jahre</li>
+                                <li>Es muss vorrangig eine geistige Behinderung im Sinne des&nbsp;§&nbsp;2&nbsp;SGB&nbsp;IX&nbsp;vorliegen</li>
+                                <li>Tagsüber muss einer Beschäftigung in einer Werkstatt&nbsp;für behinderte Menschen oder auf dem freien Arbeitsmarkt nachgegangen werden, bzw. eine tagesstrukturierende Maßnahme in Ansbach, Herrieden oder Feuchtwangen besucht werden</li>
+                                <li>Die Notwendigkeit einer Heimunterbringung muss gegeben sein</li>
+                            </ul>
+
+                        </div>
+                    </toujou-text-block-column>
+                </toujou-text-block>
+
+            </section>
+        </main>
+    `,e=t.bind({});var n,a,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return \`
+        <main id="main" skippy-links-label="Inhaltsbereich">
+
+            <section class="chapter  chapter--big-drop-bg-right-green" id="default-270272d1ea0f9e9c314693f6f3fde8a0">
+
+                <span id="c2553"></span>
+                <h2 type-size="" type-color="green-group" type-alignment="left">Herzlich Willkommen im Familienzentrum KiM!</h2>
+
+                <span id="c2552"></span>
+                <p>Im Haus befinden sich vier&nbsp;verschiedene&nbsp;Einrichtungen:</p>
+
+                <ul class="list">
+                    <li>
+                        <a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank">die Interdisziplinäre Frühförderstelle</a>
+                    </li>
+                    <li>
+                        <a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank">die inklusive Kindertageseinrichtung</a>
+                    </li>
+                    <li>
+                        sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank">Heilpädagogischer Tagesstätte</a>.
+                    </li>
+                </ul>
+
+                <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+                <ol class="list">
+                    <li>
+                        <a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank">die Interdisziplinäre Frühförderstelle</a>
+                    </li>
+                    <li>
+                        <a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank">die inklusive Kindertageseinrichtung</a>
+                    </li>
+                    <li>
+                        sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank">Heilpädagogischer Tagesstätte</a>.
+                    </li>
+                </ol>
+
+                <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+            </section>
+
+            <section class="chapter  chapter--textmedia-green chapter--no-padding-bottom" id="default-f195702f6bd322b59fa60d2c76d15e24">
+
+                <span id="c2555"></span>
+                <h2 type-size="" type-color="green-group" type-alignment="left">Was bieten wir?</h2>
+
+                <span id="c2557"></span>
+
+                <toujou-text-block class="text-block" text-block-media-position="beside-right">
+                    <toujou-text-block-column class="text-block-column">
+
+                        <figure class="text-block__figure">
+                            <img class="text-block__image" src="https://picsum.photos/">
+                        </figure>
+
+                        <div class="text-block__content">
+                            <p>Unser Familienzentrum ist&nbsp;ein&nbsp;Bildungs-, Begegnungs- und Erfahrungsort. Unser Ziel ist es, Kinder&nbsp;in&nbsp;ihrer Entwicklung zu begleiten und zu fördern und auch&nbsp;Eltern dabei Unterstützung anzubieten.&nbsp;</p><ul class="list" data-da-list="true" data-da-id="137" data-da-next="138" data-da-prev="136" data-da-focused="false"> \\t<li data-da-list-item="true" data-da-id="138" data-da-next="139" data-da-prev="137" data-da-focused="false">Bildungsangebote</li> \\t<li data-da-list-item="true" data-da-id="139" data-da-next="140" data-da-prev="138" data-da-focused="false">Informationsangebote</li> \\t<li data-da-list-item="true" data-da-id="140" data-da-next="141" data-da-prev="139" data-da-focused="false">Beratungsangebote</li> \\t<li data-da-list-item="true" data-da-id="141" data-da-next="142" data-da-prev="140" data-da-focused="false">Vortragsabende</li> \\t<li data-da-list-item="true" data-da-id="142" data-da-next="143" data-da-prev="141" data-da-focused="false">Elterngruppen</li> \\t<li data-da-list-item="true" data-da-id="143" data-da-next="144" data-da-prev="142" data-da-focused="false">Spielgruppen&nbsp;</li> </ul><p>Viele unserer Angebote sind kostenfrei und Sie können ohne Anmeldung teilnehmen. Für einige Kurse müssen wir aber eine Gebühr verlangen. Für kostenpflichtige Angebote melden Sie sich bitte mit unserem Anmeldeformular an.&nbsp;Für alle weiteren anmeldepflichtigen Veranstaltungen können Sie sich per Mail (<a href="mailto:familienzentrum-kim@lebenshilfe-ansbach.de" data-da-link-item="true" data-da-id="144" data-da-next="145" data-da-prev="143" data-da-focused="false" data-da-down="145" data-da-up="135">familienzentrum-kim@lebenshilfe-ansbach.de</a>) oder Telefon unter der 09852 61621-100 anmelden.&nbsp;</p>
+                            <p><a href="/fileadmin/Dokumente/kim-anmeldebogen-veranstaltungen.pdf" class="download-link-green" data-da-link-item="true" data-da-id="145" data-da-next="146" data-da-prev="144" data-da-focused="false" data-da-down="146" data-da-up="144"><strong data-da-skipelement="true">Anmeldebogen</strong>Veranstaltungen</a></p>
+                        </div>
+                    </toujou-text-block-column>
+                </toujou-text-block>
+
+            </section>
+
+            <section class="chapter  chapter--big-drop-bg-right-green" id="default-270272d1ea0f9e9c314693f6f3fde8a0">
+
+                <span id="c2553"></span>
+                <h2 type-size="" type-color="green-group" type-alignment="left">Herzlich Willkommen im Familienzentrum KiM!</h2>
+
+                <span id="c2552"></span>
+                <p>Im Haus befinden sich vier&nbsp;verschiedene&nbsp;Einrichtungen:</p>
+
+                <ul class="list">
+                    <li>
+                        <a href="/aufwachsen-und-lernen/interdisziplinaere-fruehfoerderstelle/" target="_blank">die Interdisziplinäre Frühförderstelle</a>
+                    </li>
+                    <li>
+                        <a href="/aufwachsen-und-lernen/inklusive-kindertageseinrichtung/" target="_blank" >die inklusive Kindertageseinrichtung</a>
+                    </li>
+                    <li>sowie die&nbsp;<a href="/aufwachsen-und-lernen/schulvorbereitende-einrichtung/">Schulvorbereitende Einrichtung</a>&nbsp;mit angeschlossener&nbsp;<a href="/aufwachsen-und-lernen/heilpaedagogische-tagesstaette/" target="_blank">Heilpädagogischer Tagesstätte</a></li>
+                 </ul>
+                 <p>Das Haus ist inklusiv gestaltet. Das bedeutet auch, dass im Familienzentrum KiM alle Familien und Kinder&nbsp;herzlich willkommen sind. Unser vielfältiges Bildungs- und Betreuungsangebot können Sie und Ihre Kinder wahrnehmen, auch wenn diese keine unserer Einrichtungen besuchen.&nbsp;</p>
+
+            </section>
+
+            <section class="chapter  chapter--default" id="default-874e50f6b51aaf5b238ffc4cb61907dd">
+
+                <span id="c2570"></span>
+
+                    <toujou-text-block class="text-block" text-block-column-count="2" text-block-media-position="">
+                        <toujou-text-block-column class="text-block-column">
+                            <div class="text-block__content">
+
+                                <h2><strong>Beratung und Unterstützung findet in&nbsp;folgenden Bereichen statt</strong></h2>
+
+                                <ul class="list">
+                                    <li>bei der Aufnahme und Gestaltung persönlicher, sozialer Beziehungen</li>
+                                    <li>bei der Selbstversorgung und beim Wohnen</li>
+                                    <li>bei der Tagesgestaltung, Freizeit und der Teilnahme am gesellschaftlichen Leben</li>
+                                    <li>beim Umgang mit den Auswirkungen der Beeinträchtigung</li>
+                                </ul>
+                        </div>
+                    </toujou-text-block-column>
+
+                    <toujou-text-block-column class="text-block-column">
+
+                        <div class="text-block__content">
+
+                            <h2><strong>Voraussetzungen für die Aufnahme ins Gemeinschaftliche Wohnen</strong></h2>
+
+                            <ul class="list">
+                                <li>Das Mindestalter beträgt 18 Jahre</li>
+                                <li>Es muss vorrangig eine geistige Behinderung im Sinne des&nbsp;§&nbsp;2&nbsp;SGB&nbsp;IX&nbsp;vorliegen</li>
+                                <li>Tagsüber muss einer Beschäftigung in einer Werkstatt&nbsp;für behinderte Menschen oder auf dem freien Arbeitsmarkt nachgegangen werden, bzw. eine tagesstrukturierende Maßnahme in Ansbach, Herrieden oder Feuchtwangen besucht werden</li>
+                                <li>Die Notwendigkeit einer Heimunterbringung muss gegeben sein</li>
+                            </ul>
+
+                        </div>
+                    </toujou-text-block-column>
+                </toujou-text-block>
+
+            </section>
+        </main>
+    \`;
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const l=["TypeTags"];export{e as TypeTags,l as __namedExportsOrder,s as default};
