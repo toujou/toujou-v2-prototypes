ae0884f3a86e6c67add05bdda2a0efed386af9f8
LuisPato <lpato@dfau.de>
2024-05-06 15:04:39 +0200

    feat: Add CSS based smooth scroll Card: https://app.clickup.com/t/86bymu94q

--- src/js/globals.js
+++ src/js/globals.js
@@ -2,12 +2,10 @@
 import '@ungap/custom-elements';
 
 /* JS Elements */
-import { initSmoothScroll } from "./globals/_smooth-scroll";
 import { initMainNav } from "./elements/main-nav";
 
 function initGlobalsScripts() {
     initMainNav();
-    initSmoothScroll();
 }
 
 if (document.readyState !== "loading") {
