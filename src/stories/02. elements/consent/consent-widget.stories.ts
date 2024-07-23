997de3649be392c97b8cbae618ff2655d8c01eee
LuisPato <lpato@dfau.de>
2024-07-23 10:51:46 +0200

    feat: Improve consent widget styles and show the "Ok" save button

--- src/stories/02. elements/consent/consent-widget.stories.ts
+++ src/stories/02. elements/consent/consent-widget.stories.ts
@@ -135,7 +135,7 @@ const Template = () => {
                 <div class="consent-widget__bottom-buttons">
                     <button id="consentDenyAllButton" deny-all="" class="button consent-widget__button" button-type="border" button-size="small" button-variant="primary">Alles ablehnen</button>
                     <button id="consentAcceptAllButton" accept-all="" class="button consent-widget__button" button-type="default" button-size="small" button-variant="primary">Alles akzeptieren</button>
-                    <button id="consentSaveButton" class="button consent-widget__button" button-size="small" button-variant="primary" button-type="default">Ales ablehnen</button>
+                    <button id="consentSaveButton" class="button consent-widget__button" button-size="small" button-variant="primary" button-type="default">OK</button>
                 </div>
             </div>
         </toujou-consent-widget>
