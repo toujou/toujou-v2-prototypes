c451c1434312c61e26365792fea2825823f5cd13
LuisPato <lpato@dfau.de>
2024-09-26 10:22:00 +0200

    refactor: Add function to render the average rating compnentn on all different chapters

--- src/stories/02. elements/average-rating/average-rating.stories.ts
+++ src/stories/02. elements/average-rating/average-rating.stories.ts
@@ -33,7 +33,7 @@ interface AverageRatingProps {
     showMaxRating: boolean
 }
 
-const Template: StoryFn<AverageRatingProps> = (args: AverageRatingProps) => {
+const averageRatingComponent = (args: AverageRatingProps) => {
     return `
         <div class="average-rating">
             <h3 class="average-rating__title">This is the average rating header</h3>
@@ -54,58 +54,35 @@ const Template: StoryFn<AverageRatingProps> = (args: AverageRatingProps) => {
                 </p>
             </div>
         </div>
+    `
+}
 
-        <section class="chapter" background-color="font-light" font-color="primary-light">
-            <div class="average-rating">
-                <h3 class="average-rating__title">This is the average rating header</h3>
-                <p class="average-rating__text">This is the average rating content element custom text</p>
-                <div class="average-rating__rating">
-                    <p class="average-rating__rating-text">3.7 ${args.showMaxRating ? '/ 5' : ''}</p>
+const Template: StoryFn<AverageRatingProps> = (args: AverageRatingProps) => {
+    return `
+        ${averageRatingComponent(args)}
 
-                    <toujou-rating-stars
-                        class="rating-stars average-rating__rating-stars"
-                        rating-total="5"
-                        rating-value="3.7">
-                    </toujou-rating-stars>
-
-                    <p class="average-rating__count">
-                        <a href="#" title="Nice rating link" target="_blank" class="average-rating__link">
-                            3 Bewertungen
-                        </a>
-                    </p>
-                </div>
-            </div>
+        <section
+            class="chapter"
+            background-color="font-light"
+            font-color="primary-light"
+        >
+            ${averageRatingComponent(args)}
         </section>
 
-        <section class="chapter" has-background-media style="
-            --chapter-bg-image-mobile: url(https://picsum.photos/1200/1200);
-            --chapter-bg-image-tablet: url(https://picsum.photos/1200/1200);
-            --chapter-bg-image-desktop: url(https://picsum.photos/1200/1200);
-            --chapter-bg-image-wide: url(https://picsum.photos/1200/1200);
-            --chapter-bg-image-original: url(https://picsum.photos/1200/1200);
-        "
-        min-height="half"
-        vertical-position="middle"
+        <section
+            class="chapter"
+            min-height="half"
+            vertical-position="middle"
+            has-background-media
+            style="
+                --chapter-bg-image-mobile: url(https://picsum.photos/1200/1200);
+                --chapter-bg-image-tablet: url(https://picsum.photos/1200/1200);
+                --chapter-bg-image-desktop: url(https://picsum.photos/1200/1200);
+                --chapter-bg-image-wide: url(https://picsum.photos/1200/1200);
+                --chapter-bg-image-original: url(https://picsum.photos/1200/1200);
+            "
         >
-            <div class="average-rating">
-                <h3 class="average-rating__title">This is the average rating header</h3>
-                <p class="average-rating__text">This is the average rating content element custom text</p>
-                <div class="average-rating__rating">
-                    <p class="average-rating__rating-text">3.7 ${args.showMaxRating ? '/ 5' : ''}</p>
-
-                    <toujou-rating-stars
-                        class="rating-stars average-rating__rating-stars"
-                        rating-total="5"
-                        rating-value="3.7">
-                    </toujou-rating-stars>
-
-                    <p class="average-rating__count">
-                        <a href="#" title="Nice rating link" target="_blank" class="average-rating__link">
-                            3 Bewertungen
-                        </a>
-                    </p>
-                </div>
-            </div>
+            ${averageRatingComponent(args)}
         </section>
     `;
 };
