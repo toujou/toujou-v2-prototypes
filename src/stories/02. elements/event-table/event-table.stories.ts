2a84786f1c0dc2804a7fba36de8275be2884f40c
LuisPato <lpato@dfau.de>
2024-09-26 10:26:36 +0200

    refactor: Render event table and evetn schedule component com a single function inside each story

--- src/stories/02. elements/event-table/event-table.stories.ts
+++ src/stories/02. elements/event-table/event-table.stories.ts
@@ -16,7 +16,7 @@ export default {
     tags: ['autodocs']
 } satisfies Meta;
 
-const Template = () => {
+const eventTableComponent = () => {
     return `
         <ol class="item-collection">
             <li class="item-collection__item">
@@ -133,123 +133,15 @@ const Template = () => {
                 </toujou-collection-item>
             </li>
         </ol>
+    `;
+}
+
+const Template = () => {
+    return `
+        ${eventTableComponent()}
 
         <section class="chapter" background-color="primary" font-color="primary-light">
-            <ol class="item-collection">
-                <li class="item-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="event-table" element-design="default" item-orientation="horizontal" role="article">
-                        <a href="#" class="event-table-card">
-                            <div
-                                class="calendar-pages event-table-card__calendar-pages"
-                                aria-label="calendar page with date / time information"
-                            >
-                                <time class="calendar-page" datetime="2025-11-14">
-                                    <div class="calendar-page__date">
-                                        <p class="calendar-page__month">Nov</p>
-                                        <p class="calendar-page__day">14</p>
-                                    </div>
-                                    <div class="calendar-page__times">
-                                        <p class="calendar-page__time calendar-page__time--start">12:30</p>
-                                    </div>
-                                </time>
-                                <span class="calendar-pages__until" aria-hidden="true">
-                                    <toujou-icon class="icon" icon-size="l" icon-name="chevron-right" icon-color="background"></toujou-icon>
-                                </span>
-                                <time class="calendar-page" datetime="2025-11-16">
-                                    <div class="calendar-page__date">
-                                        <p class="calendar-page__month">Nov</p>
-                                        <p class="calendar-page__day">16</p>
-                                    </div>
-                                    <div class="calendar-page__times">
-                                        <p class="calendar-page__time calendar-page__time--start">12:30</p>
-                                    </div>
-                                </time>
-                            </div>
-                            <div class="event-table-card__content">
-                                <h3 class="event-table-card__title">The artist formerly known as Prince</h3>
-                            </div>
-
-                            <toujou-chip class="chip event-table-card__chip" chip-size="extra-small" chip-bg-color="error">Abgesagt</toujou-chip>
-                        </a>
-                    </toujou-collection-item>
-                </li>
-
-                <li class="item-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="event-table" element-design="default" item-orientation="horizontal" role="article">
-                        <a href="#" class="event-table-card">
-                            <div
-                                class="calendar-pages event-table-card__calendar-pages"
-                                aria-label="calendar page with date / time information"
-                            >
-                                <time class="calendar-page" datetime="2025-11-14" single-day>
-                                    <div class="calendar-page__date">
-                                        <p class="calendar-page__month">Nov</p>
-                                        <p class="calendar-page__day">14</p>
-                                    </div>
-                                    <div class="calendar-page__times">
-                                        <p class="calendar-page__time-separator calendar-page__time-separator--word" aria-label="from">from</p>
-                                        <p class="calendar-page__time calendar-page__time--start">12:30</p>
-                                    </div>
-                                </time>
-                            </div>
-                            <div class="event-table-card__content">
-                                <h3 class="event-table-card__title">The artist formerly known as Prince with a nice long description with a nice long description with a nice long description</h3>
-                            </div>
-                        </a>
-                    </toujou-collection-item>
-                </li>
-
-                <li class="item-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="event-table" element-design="default" item-orientation="horizontal" role="article">
-                        <a href="#" class="event-table-card">
-                            <div class="calendar-pages event-table-card__calendar-pages" aria-label="calendar page with date / time information">
-                                <time class="calendar-page" datetime="2025-11-14">
-                                    <div class="calendar-page__date">
-                                        <p class="calendar-page__month">Nov</p>
-                                        <p class="calendar-page__day">14</p>
-                                    </div>
-                                </time>
-                                <span class="calendar-pages__until" aria-hidden="true">
-                                    <toujou-icon class="icon" icon-size="l" icon-name="chevron-right" icon-color="background"></toujou-icon>
-                                </span>
-                                <time class="calendar-page" datetime="2025-11-16">
-                                    <div class="calendar-page__date">
-                                        <p class="calendar-page__month">Nov</p>
-                                        <p class="calendar-page__day">16</p>
-                                    </div>
-                                </time>
-                            </div>
-                            <div class="event-table-card__content">
-                                <h3 class="event-table-card__title">Nice title</h3>
-                            </div>
-
-                            <toujou-chip class="chip event-table-card__chip" chip-size="extra-small" chip-bg-color="error">Abgesagt</toujou-chip>
-                        </a>
-                    </toujou-collection-item>
-                </li>
-
-                <li class="item-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="event-table" element-design="default" item-orientation="horizontal" role="article">
-                        <a href="#" class="event-table-card">
-                            <div
-                                class="calendar-pages event-table-card__calendar-pages"
-                                aria-label="calendar page with date / time information"
-                            >
-                                <time class="calendar-page" datetime="2025-11-14">
-                                    <div class="calendar-page__date">
-                                        <p class="calendar-page__month">Nov</p>
-                                        <p class="calendar-page__day">14</p>
-                                        <p class="calendar-page__year">2025</p>
-                                    </div>
-                                </time>
-                            </div>
-                            <div class="event-table-card__content">
-                                <h3 class="event-table-card__title">The artist formerly known as Prince</h3>
-                            </div>
-                        </a>
-                    </toujou-collection-item>
-                </li>
-            </ol>
+            ${eventTableComponent()}
         </section>
     `
 };
