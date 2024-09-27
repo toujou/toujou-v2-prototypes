17e7f6c84e83c20b185f3f23e16bcc05c355df0a
LuisPato <lpato@dfau.de>
2024-09-27 08:22:13 +0200

    feat: Add story and styles for the fe long element Card: https://app.clickup.com/t/86c0f747x

---
+++ src/stories/02. elements/felogin/felogin-login.stories.ts
@@ -0,0 +1,67 @@
+import { Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+
+// @ts-ignore
+import feloginDocs from './felogin.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/felogin',
+    parameters: {
+        badges: [TOUJOU_BADGES.TESTING],
+        docs: {
+            page: feloginDocs,
+        },
+    },
+    tags: ['autodocs']
+} satisfies Meta;
+
+const feloginBlock = () => {
+    return `
+        <div class="felogin">
+            <h3 class="felogin__headline">Benutzeranmeldung</h3>
+            <p class="felogin__intro">Bitte geben Sie Ihren Benutzernamen und Ihr Passwort ein, um sich an der Website anzumelden.</p>
+
+            <form
+                spellcheck="false"
+                method="post"
+                target="_top"
+                class="toujou-form form felogin__form"
+                action="#"
+            >
+                <fieldset class="fieldset">
+                    <legend class="fieldset__legend">Anmelden</legend>
+
+                    <toujou-input-group class="input-group">
+                        <label class="input-label">Benutzername:</label>
+                        <input type="text" name="user" class="input input--text">
+                    </toujou-input-group>
+                    <toujou-input-group class="input-group">
+                        <label class="input-label">Passwort:</label>
+                        <input data-rsa-encryption="" type="password" name="pass" value="" class="input input--text">
+                    </toujou-input-group>
+                    <nav class="form-navigation">
+                        <input class="button form-navigation__button" type="submit" value="Anmelden" name="submit">
+                    </nav>
+                </fieldset>
+
+            </form>
+
+            <p class="felogin__password-recover">
+                <a href="#" class="felogin__password-recover-link">Passwort vergessen?</a>
+            </p>
+        </div>
+    `;
+}
+
+const Template = () => {
+    return `
+       ${feloginBlock()}
+
+       <section class="chapter" background-color="primary">
+            ${feloginBlock()}
+        </section>
+    `;
+};
+
+export const Login = Template.bind({});
---
+++ src/stories/02. elements/felogin/felogin-password.stories.ts
@@ -0,0 +1,63 @@
+import { Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+
+// @ts-ignore
+import feloginDocs from './felogin.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/felogin',
+    parameters: {
+        badges: [TOUJOU_BADGES.TESTING],
+        docs: {
+            page: feloginDocs,
+        },
+    },
+    tags: ['autodocs']
+} satisfies Meta;
+
+const feloginBlock = () => {
+    return `
+        <div class="felogin">
+            <h3 class="felogin__headline">Password vergessen</h3>
+            <p class="felogin__intro">Bitte geben Sie Ihren Benutzernamen oder Ihre E-Mail-Adresse ein. Anweisungen zum Zurücksetzen Ihres Passworts werden Ihnen umgehend per E-Mail zugesandt.</p>
+
+            <form
+                spellcheck="false"
+                method="post"
+                target="_top"
+                class="toujou-form form felogin__form"
+                action="#"
+            >
+                <fieldset class="fieldset">
+                    <legend class="fieldset__legend">Passwort zurücksetzen</legend>
+
+                    <toujou-input-group class="input-group">
+                        <label class="input-label">Benutzername oder E-Mail-Adresse:</label>
+                        <input type="text" name="tx_felogin_login[userIdentifier]" class="input input--text">
+                    </toujou-input-group>
+                    <nav class="form-navigation">
+                        <input class="button form-navigation__button" type="submit" value="Passwort zurücksetzen" name="submit">
+                    </nav>
+                </fieldset>
+
+            </form>
+
+            <p class="felogin__password-recover">
+                <a href="#" class="felogin__password-recover-link">Zurück zum Anmeldeformular</a>
+            </p>
+        </div>
+    `;
+}
+
+const Template = () => {
+    return `
+       ${feloginBlock()}
+
+       <section class="chapter" background-color="primary">
+            ${feloginBlock()}
+        </section>
+    `;
+};
+
+export const ForgotPassword = Template.bind({});
