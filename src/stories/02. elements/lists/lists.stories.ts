009e975b020560a9bef70776d4dccb312557e8c8
LuisPato <lpato@dfau.de>
2024-10-01 13:23:40 +0200

    feat: Style text blocks inside chapters

--- src/stories/02. elements/lists/lists.stories.ts
+++ src/stories/02. elements/lists/lists.stories.ts
@@ -17,85 +17,145 @@ export default {
 
 const renderLists = () => {
     return `
-        <h3>Default list</h3>
-        <ul>
-            <li>List item one</li>
-            <li>List item two</li>
-            <li>List item three</li>
-        </ul>
+        <toujou-text-block class="text-block" text-block-column-count="2" text-block-media-position="">
+            <toujou-text-block-column class="text-block-column">
+                <div class="text-block__content">
+                    <h3>Default UL</h3>
+                        <ul class="list">
+                            <li>one</li>
+                            <li>two
+                                <ul class="list">
+                                    <li>item</li>
+                                    <li>item</li>
+                                </ul>
+                            </li>
+                            <li>three</li>
+                        </ul>
 
-        <ol>
-            <li>List item a</li>
-            <li>List item b</li>
-            <li>List item c</li>
-        </ol>
+                    <h3>Primary UL</h3>
+                    <ul class="list list--primary">
+                        <li>one</li>
+                        <li>two
+                            <ul class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ul>
+                        </li>
+                        <li>three</li>
+                    </ul>
 
-        <h3>Primary list</h3>
-        <ul class="list list--primary">
-            <li>List item one</li>
-            <li>List item two</li>
-            <li>List item three</li>
-        </ul>
+                    <h3>Secondary UL</h3>
+                    <ul class="list list--secondary">
+                        <li>one</li>
+                        <li>two
+                            <ul class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ul>
+                        </li>
+                        <li>three</li>
+                    </ul>
 
-        <ol class="list list--primary">
-            <li>List item a</li>
-            <li>List item b</li>
-            <li>List item c</li>
-        </ol>
+                    <h3>Font UL</h3>
+                    <ul class="list list--font">
+                        <li>one</li>
+                        <li>two
+                            <ul class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ul>
+                        </li>
+                        <li>three</li>
+                    </ul>
+                </div>
+            </toujou-text-block-column>
 
-        <h3>Secondary list</h3>
-        <ul class="list list--secondary">
-            <li>List item one</li>
-            <li>List item two</li>
-            <li>List item three</li>
-        </ul>
+            <toujou-text-block-column class="text-block-column">
+                <div class="text-block__content">
 
-        <ol class="list list--secondary">
-            <li>List item a</li>
-            <li>List item b</li>
-            <li>List item c</li>
-        </ol>
+                    <h3>Default OL</h3>
+                    <ol class="list">
+                        <li>one</li>
+                        <li>two
+                            <ol class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ol>
+                        </li>
+                        <li>three</li>
+                    </ol>
 
-        <h3>Font list</h3>
-        <ul class="list list--font">
-            <li>List item one</li>
-            <li>List item two</li>
-            <li>List item three</li>
-        </ul>
+                    <h3>Primary OL</h3>
+                    <ol class="list list--primary">
+                        <li>one</li>
+                        <li>two
+                            <ol class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ol>
+                        </li>
+                        <li>three</li>
+                    </ol>
 
-        <h3>Separated list - default</h3>
-        <ul class="separated-list">
-            <li class="separated-list__item">List item one</li>
-            <li class="separated-list__item">List item two</li>
-            <li class="separated-list__item">List item three</li>
-        </ul>
+                    <h3>Secondary OL</h3>
+                    <ol class="list list--secondary">
+                        <li>one</li>
+                        <li>two
+                            <ol class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ol>
+                        </li>
+                        <li>three</li>
+                    </ol>
 
-        <ol class="list list--font">
-            <li>List item a</li>
-            <li>List item b</li>
-            <li>List item c</li>
-        </ol>
+                    <h3>Font OL</h3>
+                    <ol class="list list--font">
+                        <li>one</li>
+                        <li>two
+                            <ol class="list">
+                                <li>item</li>
+                                <li>item</li>
+                            </ol>
+                        </li>
+                        <li>three</li>
+                    </ol>
+                </div>
+            </toujou-text-block-column>
 
-        <h3>Separated list - comma</h3>
-        <ul class="separated-list" list-separator="comma">
-            <li class="separated-list__item">List item one</li>
-            <li class="separated-list__item">List item two</li>
-            <li class="separated-list__item">List item three</li>
-        </ul>
+            <br>
 
-        <h3>Separated list - pipe</h3>
-        <ul class="separated-list" list-separator="pipe">
-            <li class="separated-list__item">List item one</li>
-            <li class="separated-list__item">List item two</li>
-            <li class="separated-list__item">List item three</li>
-        </ul>
+            </toujou-text-block>
+                <h3>Separated list - default</h3>
+                <ul class="separated-list">
+                    <li class="separated-list__item">List item one</li>
+                    <li class="separated-list__item">List item two</li>
+                    <li class="separated-list__item">List item three</li>
+                </ul>
+                <br>
 
-        <h3>Separated list - centered</h3>
-        <ul class="separated-list" list-separator="pipe" list-alignment="center">
-            <li class="separated-list__item">List item one</li>
-            <li class="separated-list__item">List item two</li>
-            <li class="separated-list__item">List item three</li>
-        </ul>
+                <h3>Separated list - comma</h3>
+                <ul class="separated-list" list-separator="comma">
+                    <li class="separated-list__item">List item one</li>
+                    <li class="separated-list__item">List item two</li>
+                    <li class="separated-list__item">List item three</li>
+                </ul>
+                <br>
+
+                <h3>Separated list - pipe</h3>
+                <ul class="separated-list" list-separator="pipe">
+                    <li class="separated-list__item">List item one</li>
+                    <li class="separated-list__item">List item two</li>
+                    <li class="separated-list__item">List item three</li>
+                </ul>
+                <br>
+
+                <h3>Separated list - centered</h3>
+                <ul class="separated-list" list-separator="pipe" list-alignment="center">
+                    <li class="separated-list__item">List item one</li>
+                    <li class="separated-list__item">List item two</li>
+                    <li class="separated-list__item">List item three</li>
+                </ul>
     `;
 }
 
