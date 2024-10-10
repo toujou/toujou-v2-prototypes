db18dfc03fbe430ccf25d9c9cb1bdc70079819ea
LuisPato <lpato@dfau.de>
2024-10-10 13:51:15 +0200

    Updates

--- assets/08.datepicker.stories-BlBbr9vs.js
+++ assets/08.datepicker.stories-BlBbr9vs.js
@@ -1,4 +1,4 @@
-import{T as d}from"./badgeCustomConfig-oxGPBDYz.js";import{M as l}from"./form.docs-DVjkPWBU.js";import"./jsx-runtime-DPda6Ybg.js";import"./index-DhrokLn_.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-4_AlzpT7.js";const r={"YYYY.MM.DD":"Y.m.d","YYYY/MM/DD":"Y/m/d","DD.MM.YYYY":"d.m.Y","DD/MM/YYYY":"d/m/Y"},k={title:"COMPONENTS/Forms",parameters:{badges:[d.DONE],docs:{page:l}},argTypes:{dateFormat:{name:"Date format",description:"Set the date format",options:Object.values(r),control:{type:"radio"},defaultValue:r["YYYY.MM.DD"],required:!0},minDate:{name:"Earliest date",description:"Earliest selectable date",control:{type:"date"}},maxDate:{name:"Latest date",description:"Latest selectable date",control:{type:"date"}},showTime:{name:"Show time",description:"Show time on the datepicker",control:{type:"boolean"},defaultValue:["false"],required:!0}},tags:["autodocs"]};function s(e){if(isNaN(e))return"";const t=new Date(e);if(isNaN(t.getTime()))return"";const o=t.getFullYear(),p=(t.getMonth()+1).toString().padStart(2,"0"),c=t.getDate().toString().padStart(2,"0");return`${o}-${p}-${c}`}const u=e=>{console.log("minDate",e.minDate);const t=s(e.minDate);console.log("maxDate",e.maxDate);const o=s(e.maxDate);return`
+import{T as d}from"./badgeCustomConfig-oxGPBDYz.js";import{M as u}from"./form.docs-DVjkPWBU.js";import"./jsx-runtime-DPda6Ybg.js";import"./index-DhrokLn_.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-4_AlzpT7.js";const r={"YYYY.MM.DD":"Y.m.d","YYYY/MM/DD":"Y/m/d","DD.MM.YYYY":"d.m.Y","DD/MM/YYYY":"d/m/Y"},s={single:"single",range:"range"},k={title:"COMPONENTS/Forms",parameters:{badges:[d.DONE],docs:{page:u}},argTypes:{dateFormat:{name:"Date format",description:"Set the date format",options:Object.values(r),control:{type:"radio"},defaultValue:r["YYYY.MM.DD"],required:!0},minDate:{name:"Earliest date",description:"Earliest selectable date",control:{type:"date"}},maxDate:{name:"Latest date",description:"Latest selectable date",control:{type:"date"}},showTime:{name:"Show time",description:"Show time on the datepicker",control:{type:"boolean"},defaultValue:["false"],required:!0},type:{name:"Type",description:"Set the input type",options:Object.values(s),control:{type:"radio"},defaultValue:s.single,required:!0}},tags:["autodocs"]};function i(e){if(isNaN(e))return"";const t=new Date(e);if(isNaN(t.getTime()))return"";const o=t.getFullYear(),l=(t.getMonth()+1).toString().padStart(2,"0"),c=t.getDate().toString().padStart(2,"0");return`${o}-${l}-${c}`}const D=e=>{console.log("minDate",e.minDate);const t=i(e.minDate);console.log("maxDate",e.maxDate);const o=i(e.maxDate);return`
         <link rel="stylesheet" type="text/css" href="../../../../node_modules/flatpickr/dist/themes/light.css">
 
         <style>
@@ -23,7 +23,7 @@ import{T as d}from"./badgeCustomConfig-oxGPBDYz.js";import{M as l}from"./form.do
                     date-format="${e.dateFormat}"
                     minDate="${t}"
                     maxDate="${o}"
-                    mode="range"
+                    mode="${e.type}"
                     ${e.showTime?"enableTime":""}
                 >
                     <input
@@ -38,7 +38,7 @@ import{T as d}from"./badgeCustomConfig-oxGPBDYz.js";import{M as l}from"./form.do
             </toujou-input-group>
 
         </form>
-    `},a=u.bind({});a.args={dateFormat:r["YYYY.MM.DD"],minDate:Date.now(),maxDate:Date.now()+864e5*365,showTime:!1};var i,n,m;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`(args: DatepickerProps) => {
+    `},a=D.bind({});a.args={dateFormat:r["YYYY.MM.DD"],minDate:Date.now(),maxDate:Date.now()+864e5*365,showTime:!1,type:s.single};var n,m,p;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`(args: DatepickerProps) => {
   console.log('minDate', args.minDate);
   const formattedMinDate = formatTimestamp(args.minDate);
   console.log('maxDate', args.maxDate);
@@ -68,7 +68,7 @@ import{T as d}from"./badgeCustomConfig-oxGPBDYz.js";import{M as l}from"./form.do
                     date-format="\${args.dateFormat}"
                     minDate="\${formattedMinDate}"
                     maxDate="\${formattedMaxDate}"
-                    mode="range"
+                    mode="\${args.type}"
                     \${args.showTime ? 'enableTime' : ''}
                 >
                     <input
@@ -84,4 +84,4 @@ import{T as d}from"./badgeCustomConfig-oxGPBDYz.js";import{M as l}from"./form.do
 
         </form>
     \`;
-}`,...(m=(n=a.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const M=["DatePicker"];export{a as DatePicker,M as __namedExportsOrder,k as default};
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const T=["DatePicker"];export{a as DatePicker,T as __namedExportsOrder,k as default};
