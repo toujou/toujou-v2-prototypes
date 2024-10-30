292040b70fee8d0a06cf2600ea13589b9d129c09
LuisPato <lpato@dfau.de>
2024-10-30 14:39:13 +0100

    chore: Comment out code to swap between theme styles

--- .storybook/config/decoratorsConfig.js
+++ .storybook/config/decoratorsConfig.js
@@ -1,9 +1,12 @@
-// Create "Toujou Theme" globals dropdown on the topbar
-import { setThemeStylesheets } from "../configUtils/setThemeStylesheets";
+// This is commented out because it works well on the preview, but not on the deployed storybook
+// (Because the files get different names, with hashes...)
 
-export const decoratorsConfig = [
-    (Story, context) => {
-        setThemeStylesheets(context);
-        return Story();
-    },
-];
+// Create "Toujou Theme" globals dropdown on the topbar
+// import { setThemeStylesheets } from "../configUtils/setThemeStylesheets";
+//
+// export const decoratorsConfig = [
+//     (Story, context) => {
+//         setThemeStylesheets(context);
+//         return Story();
+//     },
+// ];
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,7 +8,7 @@ import './componentImports/kojo-components';
 // Config
 import { parametersConfig } from "./config/parametersConfig";
 import { previewConfig } from "./config/previewConfig";
-import { decoratorsConfig } from "./config/decoratorsConfig";
+// import { decoratorsConfig } from "./config/decoratorsConfig";
 
 // Hacks
 import './configUtils/mainNavHack';
@@ -16,7 +16,7 @@ import './configUtils/mainNavHack';
 // Exports
 const preview = previewConfig;
 export const parameters = parametersConfig;
-export const decorators = decoratorsConfig;
+// export const decorators = decoratorsConfig;
 
 export const tags = ["autodocs"];
 export default preview;
