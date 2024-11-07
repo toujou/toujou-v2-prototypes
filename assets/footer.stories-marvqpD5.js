63e5404c387839275ea4d401bdb739ec792719b9
LuisPato <lpato@dfau.de>
2024-11-07 14:44:34 +0100

    Updates

--- assets/footer.stories-marvqpD5.js
+++ assets/footer.stories-marvqpD5.js
@@ -1,13 +1,13 @@
-import{T as c}from"./badgeCustomConfig-DUXiHpL4.js";import{j as o}from"./jsx-runtime-DPda6Ybg.js";import{useMDXComponents as i}from"./index-4_AlzpT7.js";import"./index-DhrokLn_.js";import"./_commonjsHelpers-Cpj98o6Y.js";function n(s){const e={code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...i(),...s.components};return o.jsxs(o.Fragment,{children:[o.jsx(e.h1,{id:"footer",children:"footer"}),`
-`,o.jsx(e.p,{children:"The default footer element render 4 columns:"}),`
-`,o.jsxs(e.ul,{children:[`
-`,o.jsx(e.li,{children:"logo"}),`
-`,o.jsx(e.li,{children:"Service navigation"}),`
-`,o.jsx(e.li,{children:"Footer navigation"}),`
-`,o.jsx(e.li,{children:"Contact"}),`
+import{T as r}from"./badgeCustomConfig-DUXiHpL4.js";import{j as o}from"./jsx-runtime-DPda6Ybg.js";import{useMDXComponents as c}from"./index-4_AlzpT7.js";import"./index-DhrokLn_.js";import"./_commonjsHelpers-Cpj98o6Y.js";function a(e){const s={code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...c(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(s.h1,{id:"footer",children:"footer"}),`
+`,o.jsx(s.p,{children:"The default footer element render 4 columns:"}),`
+`,o.jsxs(s.ul,{children:[`
+`,o.jsx(s.li,{children:"logo"}),`
+`,o.jsx(s.li,{children:"Service navigation"}),`
+`,o.jsx(s.li,{children:"Footer navigation"}),`
+`,o.jsx(s.li,{children:"Contact"}),`
 `]}),`
-`,o.jsx(e.h2,{id:"markup-example",children:"Markup example"}),`
-`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-html",children:`<footer class="footer">
+`,o.jsx(s.h2,{id:"markup-example",children:"Markup example"}),`
+`,o.jsx(s.pre,{children:o.jsx(s.code,{className:"language-html",children:`<footer class="footer">
     <footer-content class="footer-content">
         <footer-column class="footer-column" column-name="logo">
             <img class="footer__logo" src="https://via.placeholder.com/240x80/cccccc/969696?text=Logo" alt="quote image" />
@@ -52,7 +52,7 @@ import{T as c}from"./badgeCustomConfig-DUXiHpL4.js";import{j as o}from"./jsx-run
     </footer-content>
 </footer>
 `})}),`
-`,o.jsx(e.p,{children:'It is possible to create a custom footer by adding a "Footer" Page to the "Storage Folder" Folder in the BE'})]})}function f(s={}){const{wrapper:e}={...i(),...s.components};return e?o.jsx(e,{...s,children:o.jsx(n,{...s})}):n(s)}const x={title:"COMPONENTS/Footer",parameters:{badges:[c.DONE],docs:{page:f},layout:"fullscreen"},tags:["autodocs"]},_=()=>`
+`,o.jsx(s.p,{children:'It is possible to create a custom footer by adding a "Footer" Page to the "Storage Folder" Folder in the BE'})]})}function f(e={}){const{wrapper:s}={...c(),...e.components};return s?o.jsx(s,{...e,children:o.jsx(a,{...e})}):a(e)}const k={title:"COMPONENTS/Footer",parameters:{badges:[r.DONE],docs:{page:f},layout:"fullscreen"},tags:["autodocs"]},_=()=>`
         <footer class="footer">
             <footer-content class="footer-content">
                 <footer-column class="footer-column" column-name="logo">
@@ -125,11 +125,17 @@ import{T as c}from"./badgeCustomConfig-DUXiHpL4.js";import{j as o}from"./jsx-run
                         <a href="#" class="footer__socials-link" aria-label="Our Instagram account">
                             <toujou-icon class="icon" icon-name="sm-instagram" icon-size="l"></toujou-icon>
                         </a>
+                        <a href="#" class="footer__socials-link" aria-label="Our Snapchat account">
+                            <toujou-icon class="icon" icon-name="sm-snapchat" icon-size="l"></toujou-icon>
+                        </a>
+                        <a href="#" class="footer__socials-link" aria-label="Our WhatsApp account">
+                            <toujou-icon class="icon" icon-name="sm-whatsapp" icon-size="l"></toujou-icon>
+                        </a>
                     </div>
                 </footer-column>
             </footer-content>
         </footer>
-    `,t=_.bind({});var l,a,r;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`() => {
+    `,t=_.bind({});var n,l,i;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
   return \`
         <footer class="footer">
             <footer-content class="footer-content">
@@ -203,9 +209,15 @@ import{T as c}from"./badgeCustomConfig-DUXiHpL4.js";import{j as o}from"./jsx-run
                         <a href="#" class="footer__socials-link" aria-label="Our Instagram account">
                             <toujou-icon class="icon" icon-name="sm-instagram" icon-size="l"></toujou-icon>
                         </a>
+                        <a href="#" class="footer__socials-link" aria-label="Our Snapchat account">
+                            <toujou-icon class="icon" icon-name="sm-snapchat" icon-size="l"></toujou-icon>
+                        </a>
+                        <a href="#" class="footer__socials-link" aria-label="Our WhatsApp account">
+                            <toujou-icon class="icon" icon-name="sm-whatsapp" icon-size="l"></toujou-icon>
+                        </a>
                     </div>
                 </footer-column>
             </footer-content>
         </footer>
     \`;
-}`,...(r=(a=t.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const k=["Footer"];export{t as Footer,k as __namedExportsOrder,x as default};
+}`,...(i=(l=t.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const x=["Footer"];export{t as Footer,x as __namedExportsOrder,k as default};
