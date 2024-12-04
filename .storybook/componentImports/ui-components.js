f574ea8a3ef875cca40d5a57d8d8ad2ead6100e7
Luis Pato <42058106+luis-pato@users.noreply.github.com>
2024-12-04 14:09:39 +0100

    feat: use the ui-components @toujou/toujou-topbar package (#16)

--- .storybook/componentImports/ui-components.js
+++ .storybook/componentImports/ui-components.js
@@ -22,3 +22,4 @@ import "@toujou/toujou-collection-load-more/lib";
 import "@toujou/toujou-details/lib";
 import "@toujou/toujou-clamped-content/lib";
 import "@toujou/toujou-burger-button/lib";
+import "@toujou/toujou-topbar/lib";
