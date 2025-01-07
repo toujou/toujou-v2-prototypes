da96cfb1332051df25400926b96b9b9e68e7c7c4
LuisPato <lpato@dfau.de>
2025-01-07 13:48:10 +0100

    feat: build assets with chapter paddings and margins styles and new contact dialog component

---
+++ types/js/globals/contact-dialog.d.ts
@@ -0,0 +1,9 @@
+declare class ToujouContactDialog {
+    private readonly dialogEl;
+    private readonly closeButton;
+    constructor(dialogEl: HTMLDialogElement);
+    private _addEventListeners;
+    private _onOpenContactDialog;
+    private _closeDialog;
+    private _onDialogClick;
+}
