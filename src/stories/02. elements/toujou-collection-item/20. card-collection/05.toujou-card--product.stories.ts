f8a1379ea63449dac9b0f9d3b57d3e2b1bdf3a5f
LuisPato <lpato@dfau.de>
2025-01-16 13:40:35 +0100

    feat: Style the product collection item

--- src/stories/02. elements/toujou-collection-item/20. card-collection/05.toujou-card--product.stories.ts
+++ src/stories/02. elements/toujou-collection-item/20. card-collection/05.toujou-card--product.stories.ts
@@ -39,13 +39,61 @@ const Template: StoryFn<ToujouCardStoryProps> = (args: ToujouCardStoryProps) => 
         <main>
             <ul class="card-collection">
                 <li class="card-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="product" element-design="${args.elementDesign}" item-orientation="vertical" role="article">
+                    <toujou-collection-item
+                        class="collection-item"
+                        item-type="product"
+                        element-design="${args.elementDesign}"
+                        item-orientation="vertical" role="article"
+                    >
+                        <header class="collection-item__top">
+                            <figure class="collection-item__figure">
+                                <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                            </figure>
+
+                            <div class="collection-item__categories">
+                                ${renderCardCategoryChips(['Tutorials'], args.elementDesign)}
+                            </div>
+
+                            <toujou-media-info class="media-info">
+                                <button slot="open-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
+                                    <toujou-icon class="icon" icon-color="background" icon-name="info" icon-size="ms"></toujou-icon>
+                                </button>
+                                <button slot="close-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
+                                    <toujou-icon class="icon" icon-color="background" icon-name="close" icon-size="ms"></toujou-icon>
+                                </button>
+                                <figpaction slot="figcaption" class="media-info__figcaption">This is a beautiful description</figpaction>
+                                <small slot="copyright" class="media-info__copyright">@Nice photographer</small>
+                            </toujou-media-info>
+                        </header>
+                        <div class="collection-item__bottom">
+                            <h3 class="collection-item__title">Product A</h3>
+
+                            <p class="collection-item__abstract">
+                                <span class="collection-item__abstract-text">
+                                    Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel.
+                                </span>
+                            </p>
+                            <a href="#" class="collection-item__button">
+                                <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                                zur Beschreibung
+                            </a>
+                        </div>
+                    </toujou-collection-item>
+                </li>
+                <li class="card-collection__item">
+                    <toujou-collection-item
+                        class="collection-item"
+                        item-type="product"
+                        element-design="${args.elementDesign}"
+                        item-orientation="vertical"
+                        role="article"
+                    >
                         <header class="collection-item__top">
                             <figure class="collection-item__figure">
                                 <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
                             </figure>
                             <div class="collection-item__categories">
-                                ${renderCardCategoryChips(['Tutorials'], args.elementDesign)}
+                                ${renderCardCategoryChips(['Tutorials', 'Tests'], args.elementDesign)}
                             </div>
                             <toujou-media-info class="media-info">
                                 <button slot="open-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
@@ -58,48 +106,73 @@ const Template: StoryFn<ToujouCardStoryProps> = (args: ToujouCardStoryProps) => 
                                 <small slot="copyright" class="media-info__copyright">@Nice photographer</small>
                             </toujou-media-info>
                         </header>
-                        <a href="#" class="collection-item__bottom">
-                            <h3 class="collection-item__title">Product A</h3>
-                        </a>
-                    </toujou-collection-item>
-                </li>
-                <li class="card-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="product" element-design="${args.elementDesign}" item-orientation="vertical" role="article">
-                        <header class="collection-item__top">
-                            <figure class="collection-item__figure">
-                                <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
-                            </figure>
-                            <div class="collection-item__categories">
-                                ${renderCardCategoryChips(['Rabatt'], args.elementDesign)}
-                            </div>
-                        </header>
-                        <a href="#" class="collection-item__bottom">
+                        <div class="collection-item__bottom">
                             <h3 class="collection-item__title">Product B</h3>
-                        </a>
+
+                            <p class="collection-item__abstract">
+                                <span class="collection-item__abstract-text">
+                                    Super product, best price, amazing opportunity!
+                                </span>
+                            </p>
+                            <a href="#" class="collection-item__button">
+                                <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                                zur Beschreibung
+                            </a>
+                        </div>
                     </toujou-collection-item>
                 </li>
                 <li class="card-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="product" element-design="${args.elementDesign}" item-orientation="vertical" role="article">
+                    <toujou-collection-item
+                        class="collection-item"
+                        item-type="product"
+                        element-design="${args.elementDesign}"
+                        item-orientation="vertical" role="article"
+                    >
                         <header class="collection-item__top">
                             <figure class="collection-item__figure">
                                 <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
                             </figure>
                         </header>
-                        <a href="#" class="collection-item__bottom">
+                        <div class="collection-item__bottom">
                             <h3 class="collection-item__title">Product C</h3>
-                        </a>
+
+                            <p class="collection-item__abstract">
+                                <span class="collection-item__abstract-text">
+                                    Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel.
+                                </span>
+                            </p>
+                            <a href="#" class="collection-item__button">
+                                <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                                zur Beschreibung
+                            </a>
+                        </div>
                     </toujou-collection-item>
                 </li>
                 <li class="card-collection__item">
-                    <toujou-collection-item class="collection-item" item-type="product" element-design="${args.elementDesign}" item-orientation="vertical" role="article">
+                    <toujou-collection-item
+                        class="collection-item"
+                        item-type="product"
+                        element-design="${args.elementDesign}"
+                        item-orientation="vertical" role="article"
+                    >
                         <header class="collection-item__top">
                             <figure class="collection-item__figure">
                                 <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
                             </figure>
                         </header>
-                        <a href="#" class="collection-item__bottom">
+                        <div class="collection-item__bottom">
                             <h3 class="collection-item__title">Product D</h3>
-                        </a>
+
+                            <p class="collection-item__abstract">
+                                <span class="collection-item__abstract-text">
+                                    Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel.
+                                </span>
+                            </p>
+                            <a href="#" class="collection-item__button">
+                                <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                                zur Beschreibung
+                            </a>
+                        </div>
                     </toujou-collection-item>
                 </li>
             </ul>
