b9360549b1e90cd869ce1a74d7db68f545f4b151
LuisPato <lpato@dfau.de>
2025-02-27 14:44:59 +0100

    feat: Add story and styles for the tourism locationlist element Card: https://app.clickup.com/t/86c2aypnh

---
+++ src/stories/02. elements/location-list/location-list.stories.ts
@@ -0,0 +1,35 @@
+import { Meta } from '@storybook/web-components';
+// @ts-ignore
+import { TOUJOU_BADGES } from '../../../../.storybook/configUtils/badgeCustomConfig.js'
+
+// @ts-ignore
+import locationListDocs from './location-list.docs.mdx';
+
+export default {
+    title: 'COMPONENTS/Tourism/Location List',
+    parameters: {
+        badges: [TOUJOU_BADGES.TESTING],
+        docs: {
+            page: locationListDocs,
+        },
+    },
+    tags: ['autodocs']
+} satisfies Meta;
+
+const Template = () => {
+    return `
+        <main>
+            <toujou-location-list class="location-list">
+                <toujou-icon class="icon" icon-name="map" icon-size="m" icon-color="font"></toujou-icon>
+                <ul class="location-list__list separated-list" list-separator="dot">
+                    <li class="separated-list__item">Location A</li>
+                    <li class="separated-list__item">Location B</li>
+                    <li class="separated-list__item">Location C</li>
+                    <li class="separated-list__item">Location D</li>
+                </ul>
+            </toujou-location-list>
+        </main>
+    `;
+};
+
+export const LocationList = Template.bind({});
