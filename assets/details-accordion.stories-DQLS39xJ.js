72e75dd263f7f43eba439805dc0b6c70104a785c
LuisPato <lpato@dfau.de>
2025-03-06 07:53:36 +0100

    Updates

--- assets/details-accordion.stories-DQLS39xJ.js
+++ assets/details-accordion.stories-DQLS39xJ.js
@@ -1,4 +1,4 @@
-import{T as a}from"./badgeCustomConfig-DUXiHpL4.js";import{M as o}from"./details-accordion.docs--zKgnySI.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-BcJeOPUZ.js";import"./index-B6tQ6Rz7.js";import"./_commonjsHelpers-Cpj98o6Y.js";const g={title:"COMPONENTS/Accordion",parameters:{badges:[a.DONE],docs:{page:o}},argTypes:{singleExpandMode:{table:{category:"Details accordion Settings",defaultValue:{summary:"false"}},name:"Single expand mode",description:"Allow just one accordion items to be open at a time",control:{type:"boolean"},defaultValue:["false"],required:!0},expandedItem:{table:{category:"Details accordion Settings",defaultValue:{summary:"first"}},name:"Expanded items",description:"Define which accordion items are open when the page loads",options:["none","first","all"],control:{type:"radio"},defaultValue:["first"],required:!0},elementDesign:{table:{category:"Details Settings",defaultValue:{summary:"default"}},name:"Accordion element design",description:"Set the element design for the accordion element",options:["default","primary","secondary","inverted"],control:{type:"radio"},defaultValue:["default"],required:!0}},tags:["autodocs"]},r=e=>`
+import{T as l}from"./badgeCustomConfig-DUXiHpL4.js";import{M as o}from"./details-accordion.docs--zKgnySI.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-BcJeOPUZ.js";import"./index-B6tQ6Rz7.js";import"./_commonjsHelpers-Cpj98o6Y.js";const g={title:"COMPONENTS/Accordion",parameters:{badges:[l.DONE],docs:{page:o}},argTypes:{singleExpandMode:{table:{category:"Details accordion Settings",defaultValue:{summary:"false"}},name:"Single expand mode",description:"Allow just one accordion items to be open at a time",control:{type:"boolean"},defaultValue:["false"],required:!0},expandedItem:{table:{category:"Details accordion Settings",defaultValue:{summary:"first"}},name:"Expanded items",description:"Define which accordion items are open when the page loads",options:["none","first","all"],control:{type:"radio"},defaultValue:["first"],required:!0},elementDesign:{table:{category:"Details Settings",defaultValue:{summary:"default"}},name:"Accordion element design",description:"Set the element design for the accordion element",options:["default","primary","secondary","inverted"],control:{type:"radio"},defaultValue:["default"],required:!0}},tags:["autodocs"]},a=e=>`
         <main>
             <toujou-details-accordion class="details-accordion" ${e.singleExpandMode?"single-expand-mode":""}>
                 <toujou-details id="details-1" class="details" ${e.expandedItem==="first"||e.expandedItem==="all"?"is-open":""} element-design="${e.elementDesign}">
@@ -29,6 +29,88 @@ import{T as a}from"./badgeCustomConfig-DUXiHpL4.js";import{M as o}from"./details
                             <toujou-text-block-column class="text-block-column">
                                 <figure class="text-block__figure">
                                     <img class="text-block__image" src="https://picsum.photos/800/800">
+                                </figure>
+                                <div class="text-block__content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit amet. A pellentesque sit amet porttitor eget. Feugiat nisl pretium fusce id velit. Justo donec enim diam vulputate ut pharetra sit. Enim diam vulputate ut pharetra sit amet aliquam. Viverra nam libero justo laoreet sit amet cursus. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Magna etiam tempor orci eu lobortis elementum. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Massa vitae tortor condimentum lacinia. Sed risus pretium quam vulputate dignissim. Et ultrices neque ornare aenean euismod elementum nisi.</p>
+                                </div>
+                            </toujou-text-block-column>
+                        </toujou-text-block>
+                    </div>
+                </toujou-details>
+                
+                <toujou-details id="details-2" class="details" ${e.expandedItem==="all"?"is-open":""} element-design="${e.elementDesign}">
+                    <h3 slot="summary" class="details__title">Item with slider</h3>
+                    <toujou-icon class="icon details__chevron" icon-name="chevron-down" slot="chevron"></toujou-icon>
+
+                    <div slot="content">
+                        <toujou-text-block class="text-block" text-block-media-position="inside-right">
+                            <toujou-text-block-column class="text-block-column">
+                                <figure class="text-block__figure">
+                                    <toujou-slider
+            class="slider"
+            aria-label="Toujou slider example"
+            element-design="primary"
+            slides-to-show="1"
+            slider-type="default"
+            slider-gap="var(--slider-slides-gap)"
+            slider-aspect-ratio="0.5625"
+            arrow-first-aria-label="Go to first slide"
+            arrow-last-aria-label="Go to last slide"
+            arrow-prev-aria-label="Go to previous slide"
+            arrow-next-aria-label="Go to next slide"
+            content-type="accordion"
+        >
+            <div class="splide slider__slider">
+                <div class="splide__arrows slider-controls">
+                    <button class="splide__arrow splide__arrow--prev slider-control slider-control--prev">
+                        <toujou-icon class="icon slider-control__icon slider-control__icon--prev" icon-name="arrow-left"></toujou-icon>
+                    </button>
+                    <button class="splide__arrow splide__arrow--next slider-control slider-control--next">
+                        <toujou-icon class="icon slider-control__icon slider-control__icon--next" icon-name="arrow-right"></toujou-icon>
+                    </button>
+                </div>
+
+                <div class="splide__track slider__track">
+                    <ul class="splide__list slider__list">
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1600/900" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1600/901" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/400/200" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1603/904" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1600/910" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="splide__progress slider-progress">
+                    <div class="splide__progress__bar slider-progress__bar"></div>
+                </div>
+            </div>
+
+            <toujou-slider-count class="slider-count">
+                <span class="slider-count__current"></span>
+                <span class="slider-count__separator">of</span>
+                <span class="slider-count__total"></span>
+            </toujou-slider-count>
+        </toujou-slider>
                                 </figure>
                                 <div class="text-block__content">
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit amet. A pellentesque sit amet porttitor eget. Feugiat nisl pretium fusce id velit. Justo donec enim diam vulputate ut pharetra sit. Enim diam vulputate ut pharetra sit amet aliquam. Viverra nam libero justo laoreet sit amet cursus. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Magna etiam tempor orci eu lobortis elementum. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Massa vitae tortor condimentum lacinia. Sed risus pretium quam vulputate dignissim. Et ultrices neque ornare aenean euismod elementum nisi.</p>
@@ -74,7 +156,7 @@ import{T as a}from"./badgeCustomConfig-DUXiHpL4.js";import{M as o}from"./details
                 </toujou-details>
             </toujou-details-accordion>
         </main>
-    `,t=r.bind({});t.args={singleExpandMode:!1,expandedItem:"first",elementDesign:"default"};var i,n,s;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`(args: DetailsAccordionStoryProps) => {
+    `,i=a.bind({});i.args={singleExpandMode:!1,expandedItem:"first",elementDesign:"default"};var t,s,n;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`(args: DetailsAccordionStoryProps) => {
   return \`
         <main>
             <toujou-details-accordion class="details-accordion" \${args.singleExpandMode ? 'single-expand-mode' : ''}>
@@ -106,6 +188,88 @@ import{T as a}from"./badgeCustomConfig-DUXiHpL4.js";import{M as o}from"./details
                             <toujou-text-block-column class="text-block-column">
                                 <figure class="text-block__figure">
                                     <img class="text-block__image" src="https://picsum.photos/800/800">
+                                </figure>
+                                <div class="text-block__content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit amet. A pellentesque sit amet porttitor eget. Feugiat nisl pretium fusce id velit. Justo donec enim diam vulputate ut pharetra sit. Enim diam vulputate ut pharetra sit amet aliquam. Viverra nam libero justo laoreet sit amet cursus. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Magna etiam tempor orci eu lobortis elementum. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Massa vitae tortor condimentum lacinia. Sed risus pretium quam vulputate dignissim. Et ultrices neque ornare aenean euismod elementum nisi.</p>
+                                </div>
+                            </toujou-text-block-column>
+                        </toujou-text-block>
+                    </div>
+                </toujou-details>
+                
+                <toujou-details id="details-2" class="details" \${args.expandedItem === 'all' ? 'is-open' : ''} element-design="\${args.elementDesign}">
+                    <h3 slot="summary" class="details__title">Item with slider</h3>
+                    <toujou-icon class="icon details__chevron" icon-name="chevron-down" slot="chevron"></toujou-icon>
+
+                    <div slot="content">
+                        <toujou-text-block class="text-block" text-block-media-position="inside-right">
+                            <toujou-text-block-column class="text-block-column">
+                                <figure class="text-block__figure">
+                                    <toujou-slider
+            class="slider"
+            aria-label="Toujou slider example"
+            element-design="primary"
+            slides-to-show="1"
+            slider-type="default"
+            slider-gap="var(--slider-slides-gap)"
+            slider-aspect-ratio="0.5625"
+            arrow-first-aria-label="Go to first slide"
+            arrow-last-aria-label="Go to last slide"
+            arrow-prev-aria-label="Go to previous slide"
+            arrow-next-aria-label="Go to next slide"
+            content-type="accordion"
+        >
+            <div class="splide slider__slider">
+                <div class="splide__arrows slider-controls">
+                    <button class="splide__arrow splide__arrow--prev slider-control slider-control--prev">
+                        <toujou-icon class="icon slider-control__icon slider-control__icon--prev" icon-name="arrow-left"></toujou-icon>
+                    </button>
+                    <button class="splide__arrow splide__arrow--next slider-control slider-control--next">
+                        <toujou-icon class="icon slider-control__icon slider-control__icon--next" icon-name="arrow-right"></toujou-icon>
+                    </button>
+                </div>
+
+                <div class="splide__track slider__track">
+                    <ul class="splide__list slider__list">
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1600/900" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1600/901" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/400/200" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1603/904" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                        <li class="splide__slide slider-slide"">
+                            <toujou-single-media class="single-media slider__item" slot="slider-item">
+                                <img class="single-media__image" src="https://picsum.photos/1600/910" alt="Nice image">
+                            </toujou-single-media>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="splide__progress slider-progress">
+                    <div class="splide__progress__bar slider-progress__bar"></div>
+                </div>
+            </div>
+
+            <toujou-slider-count class="slider-count">
+                <span class="slider-count__current"></span>
+                <span class="slider-count__separator">of</span>
+                <span class="slider-count__total"></span>
+            </toujou-slider-count>
+        </toujou-slider>
                                 </figure>
                                 <div class="text-block__content">
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit amet. A pellentesque sit amet porttitor eget. Feugiat nisl pretium fusce id velit. Justo donec enim diam vulputate ut pharetra sit. Enim diam vulputate ut pharetra sit amet aliquam. Viverra nam libero justo laoreet sit amet cursus. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Magna etiam tempor orci eu lobortis elementum. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Massa vitae tortor condimentum lacinia. Sed risus pretium quam vulputate dignissim. Et ultrices neque ornare aenean euismod elementum nisi.</p>
@@ -152,4 +316,4 @@ import{T as a}from"./badgeCustomConfig-DUXiHpL4.js";import{M as o}from"./details
             </toujou-details-accordion>
         </main>
     \`;
-}`,...(s=(n=t.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const p=["DetailsAccordion"];export{t as DetailsAccordion,p as __namedExportsOrder,g as default};
+}`,...(n=(s=i.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const h=["DetailsAccordion"];export{i as DetailsAccordion,h as __namedExportsOrder,g as default};
