5fc15346c9a8330e4ea05d5fc018de824f88ba2d
LuisPato <lpato@dfau.de>
2025-03-24 10:27:02 +0100

    fix: Change the skippy-links background color Ticket: https://support.dfau.de/#ticket/zoom/17049

--- src/tests/contact-box/a11y/contact-box.a11y.cy.ts
+++ src/tests/contact-box/a11y/contact-box.a11y.cy.ts
@@ -3,12 +3,16 @@
 
 describe('contact-box a11y', () => {
     beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-contact-box--contact-box');
+        cy.visit('/iframe.html?viewMode=story&id=components-contact-dialog--contact-dialog');
         cy.injectAxe();
     });
 
     it('has no detectable a11y violation on load', () => {
         cy.get('.button').click();
+
+        // Wait for the .contact-box to be visible
+        cy.get('.contact-box').should('be.visible');
+
         cy.checkA11y('.contact-box');
     });
 })
