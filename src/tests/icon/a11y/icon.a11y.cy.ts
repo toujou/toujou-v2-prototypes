a206a60ae55fe94c256f4ca9b7b095b50c5984ba
LuisPato <lpato@dfau.de>
2025-03-24 13:24:48 +0100

    test: refactor and fix the icon tests

--- src/tests/icon/a11y/icon.a11y.cy.ts
+++ src/tests/icon/a11y/icon.a11y.cy.ts
@@ -1,160 +1,39 @@
 /// <reference types="cypress" />
 /// <reference types="cypress-axe" />
 
-describe('icon a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons');
-        cy.injectAxe();
+describe("icon a11y", () => {
+    const colors = [
+        "font-dark",
+        "font-light",
+        "primary",
+        "primary-dark",
+        "primary-light",
+        "secondary",
+        "secondary-dark",
+        "secondary-light",
+        "background",
+        "success",
+        "warning",
+        "error",
+    ];
+
+    it("has no detectable a11y violation on default state", () => {
+        cy.visit("/iframe.html?viewMode=story&id=components-icons--icons");
+        cy.injectAxe(); // Move this after the visit
+        cy.get(".icon");
+        // @ts-ignore
+        cy.checkA11yWithWait(".icon");
     });
 
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
+    colors.forEach((color) => {
+        it(`has no detectable a11y violation with color: ${color}`, () => {
+            cy.visit(`/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:${color}`);
+            cy.injectAxe(); // Move this after the visit
+            cy.get(".icon");
+            // @ts-ignore
+            cy.checkA11yWithWait(".icon");
+        });
     });
-})
+});
 
-describe('icon font-color dark a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:font-dark');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon font-color light a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:font-light');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon primary a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:primary');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon primary dark a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:primary-dark');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon primary light a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:primary-light');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon secondary a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:secondary');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon secondary dark a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:secondary-dark');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon secondary light a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:secondary-light');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon background a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:background');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon success a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:success');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon warning a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:warning');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-describe('icon error a11y', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons&args=iconColor:error');
-        cy.injectAxe();
-    });
-
-    it('has no detectable a11y violation on load', () => {
-        cy.get('.icon');
-        cy.checkA11y('.icon');
-    });
-})
-
-export {}
+export {};
--- src/tests/icon/e2e/icon.cy.ts
+++ src/tests/icon/e2e/icon.cy.ts
@@ -1,199 +1,62 @@
 /// <reference types="cypress" />
 
-const colors = Cypress.env('colors');
-const tokens = Cypress.env('tokens');
+const colors = Cypress.env("colors");
+const tokens = Cypress.env("tokens");
 
-describe('toujou-icon', () => {
+describe("toujou-icon", () => {
     beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons');
+        cy.visit("/iframe.html?viewMode=story&id=components-icons--icons");
     });
 
-    it('has correct class', () => {
-        cy.get('toujou-icon').invoke('attr', 'class').should('eq', 'icon');
-    });
-})
-
-describe('toujou-icon - sizes', () => {
-    beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons');
-    });
-
-    it('has correct xxxl size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'xxxl');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.xxxl);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.xxxl);
-    });
-
-    it('has correct xxl size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'xxl');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.xxl);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.xxl);
-    });
-
-    it('has correct xl size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'xl');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.xl);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.xl);
-    });
-
-    it('has correct l size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'l');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.l);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.l);
-    });
-
-    it('has correct m size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'm');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.m);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.m);
-    });
-
-    it('has correct ms size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'ms');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.ms);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.ms);
-    });
-
-    it('has correct normal size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'normal');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.normal);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.normal);
-    });
-
-    it('has correct s size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 's');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.s);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.s);
-    });
-
-    it('has correct xs size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'xs');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.xs);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.xs);
-    });
-
-    it('has correct xxs size', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-size', 'xxs');
-        });
-        cy.get('.icon').should('have.css', 'height', tokens.type.size.xxs);
-        cy.get('.icon').should('have.css', 'width', tokens.type.size.xxs);
+    it("has correct class", () => {
+        cy.get("toujou-icon").should("have.class", "icon");
     });
 });
 
-describe('toujou-icon - colors', () => {
+describe.only("toujou-icon - sizes", () => {
+    const sizes = ["xxxl", "xxl", "xl", "l", "m", "ms", "normal", "s", "xs", "xxs"];
+
     beforeEach(() => {
-        cy.visit('/iframe.html?viewMode=story&id=components-icons--icons');
+        cy.visit("/iframe.html?viewMode=story&id=components-icons--icons");
     });
 
-    it('has correct font color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'font');
+    sizes.forEach((size) => {
+        it(`has correct ${size} size`, () => {
+            cy.get(".icon").first().invoke("attr", "icon-size", size);
+            cy.get(".icon").first()
+                .should("have.css", "height", tokens.type.size[size])
+                .and("have.css", "width", tokens.type.size[size]);
         });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorFont);
-    });
-
-    it('has correct font dark color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'font-dark');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorFontDark);
-    });
-
-    it('has correct font light color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'font-light');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorFontLight);
-    });
-
-    it('has correct primary color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'primary');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorPrimary);
-    });
-
-    it('has correct primary dark color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'primary-dark');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorPrimaryDark);
-    });
-
-    it('has correct primary light color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'primary-light');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorPrimaryLight);
-    });
-
-    it('has correct secondary color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'secondary');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorSecondary);
-    });
-
-    it('has correct secondary dark color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'secondary-dark');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorSecondaryDark);
-    });
-
-    it('has correct secondary light color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'secondary-light');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorSecondaryLight);
-    });
-
-    it('has correct background color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'background');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorBg);
-    });
-
-    it('has correct success color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'success');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorSuccess);
-    });
-
-    it('has correct warning color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'warning');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorWarning);
-    });
-
-    it('has correct error color', () => {
-        cy.get('.icon').then((e) => {
-            e[0].setAttribute('icon-color', 'error');
-        });
-        cy.get('.icon').should('have.css', 'background-color', colors.colorError);
     });
 });
 
-export {}
+describe("toujou-icon - colors", () => {
+    const colorMappings = {
+        "font": colors.colorFont,
+        "font-dark": colors.colorFontDark,
+        "font-light": colors.colorFontLight,
+        "primary": colors.colorPrimary,
+        "primary-dark": colors.colorPrimaryDark,
+        "primary-light": colors.colorPrimaryLight,
+        "secondary": colors.colorSecondary,
+        "secondary-dark": colors.colorSecondaryDark,
+        "secondary-light": colors.colorSecondaryLight,
+        "background": colors.colorBg,
+        "success": colors.colorSuccess,
+        "warning": colors.colorWarning,
+        "error": colors.colorError,
+    };
+
+    beforeEach(() => {
+        cy.visit("/iframe.html?viewMode=story&id=components-icons--icons");
+    });
+
+    Object.entries(colorMappings).forEach(([color, expectedValue]) => {
+        it(`has correct ${color} color`, () => {
+            cy.get(".icon").first().invoke("attr", "icon-color", color);
+            cy.get(".icon").first().should("have.css", "background-color", expectedValue);
+        });
+    });
+});
+
+export {};
