fa1407310ffdc1945379be161ecc88f8649c990c
LuisPato <lpato@dfau.de>
2025-05-23 11:02:11 +0200

    Updates

--- assets/blockquote.stories-BY6uIB47.js
+++ assets/blockquote.stories-BY6uIB47.js
@@ -1,6 +1,8 @@
-import{T as i}from"./badgeCustomConfig-DUXiHpL4.js";import{M as a}from"./blockquote.docs-cfio2LY0.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-DpH9cx3q.js";import"./index-DO8khQO4.js";import"./_commonjsHelpers-Cpj98o6Y.js";const g={title:"COMPONENTS/Blockquote",parameters:{badges:[i.DONE],docs:{page:a}},argTypes:{direction:{table:{category:"Blockquote settings",defaultValue:{summary:"vertical"}},name:"Direction",description:"Set the blockquote's direction",options:["vertical","horizontal"],control:{type:"radio"},required:!0},elementDesign:{table:{category:"Blockquote settings",defaultValue:{summary:"default"}},name:"Element design",description:"Set the blockquote's element design",options:["default","primary","secondary","inverted"],control:{type:"radio"},required:!0}}},t=o=>`
+import{T as u}from"./badgeCustomConfig-DUXiHpL4.js";import{M as a}from"./blockquote.docs-cfio2LY0.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-DpH9cx3q.js";import"./index-DO8khQO4.js";import"./_commonjsHelpers-Cpj98o6Y.js";const g={title:"COMPONENTS/Blockquote",parameters:{badges:[u.DONE],docs:{page:a}},argTypes:{direction:{table:{category:"Blockquote settings",defaultValue:{summary:"vertical"}},name:"Direction",description:"Set the blockquote's direction",options:["vertical","horizontal"],control:{type:"radio"},required:!0},elementDesign:{table:{category:"Blockquote settings",defaultValue:{summary:"default"}},name:"Element design",description:"Set the blockquote's element design",options:["default","primary","secondary","inverted"],control:{type:"radio"},required:!0}}},t=o=>`
         <toujou-blockquote role="figure" class="blockquote" blockquote-direction="${o.direction}" element-design="${o.elementDesign}">
-            <img slot="image" class="blockquote__image" src="https://picsum.photos/200/200" alt="quote image" />
+            <a href="#" class="blockquote__image-link" slot="image">
+                <img slot="image" class="blockquote__image" src="https://picsum.photos/200/200" alt="quote image" />
+            </a>
             <blockquote slot="quote" class="blockquote__blockquote">
                 <div class="blockquote__content">
                     <p>Before you criticize someone, <a href="#">you should walk a mile</a> in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.</p>
@@ -8,7 +10,7 @@ import{T as i}from"./badgeCustomConfig-DUXiHpL4.js";import{M as a}from"./blockqu
             </blockquote>
             <figcaption slot="author" class="blockquote__author">Jack Handey</figcaption>
         </toujou-blockquote>
-    `,n=o=>`
+    `,l=o=>`
         <main>
             <toujou-blockquote-grid class="blockquote-grid" blockquote-grid-count="1">
                 ${t(o)}
@@ -20,7 +22,7 @@ import{T as i}from"./badgeCustomConfig-DUXiHpL4.js";import{M as a}from"./blockqu
                 </toujou-blockquote-grid>
             </section>
         </main>
-    `,e=n.bind({});e.args={direction:"vertical",elementDesign:"default"};var r,c,u;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`(args: BlockquoteStoryProps) => {
+    `,e=l.bind({});e.args={direction:"vertical",elementDesign:"default"};var r,c,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`(args: BlockquoteStoryProps) => {
   return \`
         <main>
             <toujou-blockquote-grid class="blockquote-grid" blockquote-grid-count="1">
@@ -34,4 +36,4 @@ import{T as i}from"./badgeCustomConfig-DUXiHpL4.js";import{M as a}from"./blockqu
             </section>
         </main>
     \`;
-}`,...(u=(c=e.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const b=["Blockquote"];export{e as Blockquote,b as __namedExportsOrder,g as default};
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const b=["Blockquote"];export{e as Blockquote,b as __namedExportsOrder,g as default};
