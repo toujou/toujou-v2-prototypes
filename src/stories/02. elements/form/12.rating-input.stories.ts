58bfed4be5711a8405acd396c15c132fc428e10a
LuisPato <lpato@dfau.de>
2025-07-23 09:02:55 +0200

    feat: Improve the rating-radios styles to better match EXT markup Card: https://app.clickup.com/t/86c3nupyp

--- src/stories/02. elements/form/12.rating-input.stories.ts
+++ src/stories/02. elements/form/12.rating-input.stories.ts
@@ -109,29 +109,28 @@ const Template = () => {
                 <span class="pristine-error form__error">This is an error message!</span>
         	</toujou-input-group>
 
-            <toujou-input-group class="input-group input-group--rating-radios">
-                <label for="testform-1000091-email-1" class="input-label">Rating Radios</label>
-                <span class="input-description">This is a description</span>
-                
-                <toujou-rating-radios class="rating-radios" id="test-review-form-3971-rating-1" name="test-review-form-3971-rating-1" show-numeric-rating="">
-                    <fieldset class="rating-radios__fieldset" aria-label="Your rating">
-                        <input data-element-type="Rating" aria-label="1 Stern" class="rating-radios__input" id="star1-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="1">
-                        <label class="rating-radios__label" for="star1-rating-1" title="1 Stern" aria-label="1 Stern"></label>
+            <toujou-input-group class="input-group">
+                <label class="input-label" for="test-review-form-3971-rating-1">Rating</label>
+            
+                <toujou-rating-radios id="test-review-form-3971-rating-1" name="test-review-form-3971-rating-1" show-numeric-rating="">
+                    <fieldset class="rating-radio__wrapper" aria-label="Your rating">
 
-                        <input data-element-type="Rating" aria-label="2 Sterne" class="rating-radios__input" id="star2-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="2">
-                        <label class="rating-radios__label" for="star2-rating-1" title="2 Sterne" aria-label="2 Sterne"></label>
+                        <input data-element-type="Rating" aria-label="1 Stern" class=" rating-radio" id="star1-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="1">
+                        <label class="rating__star-label" for="star1-rating-1" title="1 Stern" aria-label="1 Stern"></label>
 
-                        <input data-element-type="Rating" aria-label="3 Sterne" class="rating-radios__input" id="star3-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="3">
-                        <label class="rating-radios__label" for="star3-rating-1" title="3 Sterne" aria-label="3 Sterne"></label>
+                        <input data-element-type="Rating" aria-label="2 Sterne" class=" rating-radio" id="star2-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="2">
+                        <label class="rating__star-label" for="star2-rating-1" title="2 Sterne" aria-label="2 Sterne"></label>
 
-                        <input data-element-type="Rating" aria-label="4 Sterne" class="rating-radios__input" id="star4-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="4">
-                        <label class="rating-radios__label" for="star4-rating-1" title="4 Sterne" aria-label="4 Sterne"></label>
+                        <input data-element-type="Rating" aria-label="3 Sterne" class=" rating-radio" id="star3-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="3">
+                        <label class="rating__star-label" for="star3-rating-1" title="3 Sterne" aria-label="3 Sterne"></label>
 
-                        <input data-element-type="Rating" aria-label="5 Sterne" class="rating-radios__input" id="star5-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="5">
-                        <label class="rating-radios__label" for="star5-rating-1" title="5 Sterne" aria-label="5 Sterne"></label>
+                        <input data-element-type="Rating" aria-label="4 Sterne" class=" rating-radio" id="star4-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="4">
+                        <label class="rating__star-label" for="star4-rating-1" title="4 Sterne" aria-label="4 Sterne"></label>
+
+                        <input data-element-type="Rating" aria-label="5 Sterne" class=" rating-radio" id="star5-rating-1" type="radio" name="tx_form_formframework[test-review-form-3971][rating-1]" value="5">
+                        <label class="rating__star-label" for="star5-rating-1" title="5 Sterne" aria-label="5 Sterne"></label>
                     </fieldset>
                 </toujou-rating-radios>
-                <span class="pristine-error form__error">This is an error message!</span>
             </toujou-input-group>
 
         </form>
