b55fbe36182a6031c7d911d48f62d48607e47e7d
LuisPato <lpato@dfau.de>
2025-08-13 07:24:43 +0200

    build: build assets

--- dist/js/video-autoplay.js
+++ dist/js/video-autoplay.js
@@ -1,2 +1,2 @@
-function t(){const t=window.matchMedia("(prefers-reduced-motion: reduce)").matches;document.querySelectorAll('video[data-is-autoplay="1"]').forEach((e=>{t?e.setAttribute("controls",""):e.setAttribute("autoplay","")}))}"loading"!==document.readyState?setTimeout((()=>{t()})):document.addEventListener("DOMContentLoaded",(()=>{t()}));
+function t(){const t=window.matchMedia("(prefers-reduced-motion: reduce)").matches;document.querySelectorAll('video[data-is-autoplay="1"]').forEach((e=>{t?e.setAttribute("controls",""):(e.setAttribute("autoplay",""),e.play().then((()=>{})))}))}"loading"!==document.readyState?setTimeout((()=>{t()})):document.addEventListener("DOMContentLoaded",(()=>{t()}));
 //# sourceMappingURL=video-autoplay.js.map
