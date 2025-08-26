45a749c144109a97bfdf07110048a47094edb907
Sascha Neumann <github@09f.de>
2025-08-26 12:23:04 +0200

    build: build assets

---
+++ dist/js/property-D5hxO1nL.js
@@ -0,0 +1,2 @@
+const e=(e,i)=>"method"===i.kind&&i.descriptor&&!("value"in i.descriptor)?{...i,finisher(r){r.createProperty(i.key,e)}}:{kind:"field",key:Symbol(),placement:"own",descriptor:{},originalKey:i.key,initializer(){"function"==typeof i.initializer&&(this[i.key]=i.initializer.call(this))},finisher(r){r.createProperty(i.key,e)}};function i(i){return(r,t)=>{return void 0!==t?(n=i,o=t,void r.constructor.createProperty(o,n)):e(i,r);var n,o}}export{i as e};
+//# sourceMappingURL=property-D5hxO1nL.js.map
