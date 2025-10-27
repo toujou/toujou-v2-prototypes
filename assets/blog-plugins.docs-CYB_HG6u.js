25386f97d7e4951cdd1b994973842cc28fbeea2b
LuisPato <lpato@dfau.de>
2025-10-27 07:36:46 +0100

    Updates

---
+++ assets/blog-plugins.docs-CYB_HG6u.js
@@ -0,0 +1,48 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{useMDXComponents as s}from"./index-DiBHS0bC.js";function n(t){const o={blockquote:"blockquote",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(o.h1,{id:"blog-plugin---authors",children:"Blog plugin - authors"}),`
+`,e.jsx(o.p,{children:"The blog plugin is available as a content element on the BE. It can show:"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsx(o.li,{children:"Blog: Archive"}),`
+`,e.jsx(o.li,{children:"Blog: Authors"}),`
+`,e.jsx(o.li,{children:"Blog: Comment Form"}),`
+`,e.jsx(o.li,{children:"Blog: Comments"}),`
+`,e.jsx(o.li,{children:"Blog: Footer"}),`
+`,e.jsx(o.li,{children:"Blog: Header"}),`
+`,e.jsx(o.li,{children:"Blog: Latest Posts"}),`
+`,e.jsx(o.li,{children:"Blog: List by author"}),`
+`,e.jsx(o.li,{children:"Blog: List by category"}),`
+`,e.jsx(o.li,{children:"Blog: List by tags"}),`
+`,e.jsx(o.li,{children:"Blog: List of posts"}),`
+`,e.jsx(o.li,{children:"Blog: Metadata"}),`
+`,e.jsx(o.li,{children:"Blog: Related posts"}),`
+`,e.jsx(o.li,{children:"Blog: Sidebar"}),`
+`]}),`
+`,e.jsx(o.p,{children:"Each of these plugin types renders its own template / partial"}),`
+`,e.jsx(o.h2,{id:"plugin-types",children:"Plugin types"}),`
+`,e.jsx(o.h3,{id:"blog-archive",children:"Blog: Archive"}),`
+`,e.jsx(o.p,{children:"Show a list of years / months and the amount of posts for each month"}),`
+`,e.jsx(o.h3,{id:"blog-authors",children:"Blog: Authors"}),`
+`,e.jsx(o.p,{children:'Shows a "section" for each author displaying the author avatar, name and location, description and social media links.'}),`
+`,e.jsx(o.h3,{id:"blog-comment-form",children:"Blog: Comment Form"}),`
+`,e.jsx(o.p,{children:"For the form to add a blog post comment"}),`
+`,e.jsx(o.h3,{id:"blog-comment",children:"Blog: Comment"}),`
+`,e.jsx(o.p,{children:"Shows a list of comments for a post"}),`
+`,e.jsx(o.h3,{id:"blog-footer",children:"Blog: Footer"}),`
+`,e.jsx(o.p,{children:"Shows a section with a list of categories and tags"}),`
+`,e.jsx(o.h3,{id:"blog-header",children:"Blog: Header"}),`
+`,e.jsx(o.p,{children:"Shows the blog image with the blog title centered"}),`
+`,e.jsx(o.h3,{id:"blog-latest-posts",children:"Blog: Latest posts"}),`
+`,e.jsx(o.p,{children:"Shows a card collection of the latest posts"}),`
+`,e.jsx(o.h3,{id:"blog-list-by-author",children:"Blog: List by author"}),`
+`,e.jsxs(o.blockquote,{children:[`
+`,e.jsx(o.p,{children:"Not implemented"}),`
+`]}),`
+`,e.jsx(o.h3,{id:"blog-list-by-category",children:"Blog: List by category"}),`
+`,e.jsx(o.p,{children:"Shows a list of all categories"}),`
+`,e.jsx(o.h3,{id:"blog-list-by-tags",children:"Blog: List by tags"}),`
+`,e.jsx(o.p,{children:"Shows a list of all tags"}),`
+`,e.jsx(o.h3,{id:"blog-metadata",children:"Blog: Metadata"}),`
+`,e.jsx(o.p,{children:"Shows a section with information about the blog post: Author, categories, tags, date and number of comments"}),`
+`,e.jsx(o.h3,{id:"blog-related-posts",children:"Blog: Related posts"}),`
+`,e.jsx(o.p,{children:'Shows a card collection with related posts. "Related posts" are posts that share the categories with the current post'}),`
+`,e.jsx(o.h3,{id:"blog-sidebar",children:"Blog: Sidebar"}),`
+`,e.jsx(o.p,{children:"Shows a section with a list of the latest posts, all categories and tags, the latest comments and the archive"})]})}function h(t={}){const{wrapper:o}={...s(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(n,{...t})}):n(t)}export{h as M};
---
+++ assets/chip.stories-CHrjttuV.js
@@ -0,0 +1,98 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{useMDXComponents as a}from"./index-DiBHS0bC.js";import"./iframe-Cx1EYarR.js";import"./preload-helper-C1FmrZbK.js";function c(r){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...a(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(i.h1,{id:"toujou-chip",children:"<toujou-chip>"}),`
+`,e.jsxs(i.p,{children:["The ",e.jsx(i.code,{children:"<toujou-chip>"})," is simple chip element which allows us to render a small piece of information, like a filter option or a category. It may be displayed in many colors, sizes, with or without icon, ..."]}),`
+`,e.jsx(i.h2,{id:"example",children:"Example"}),`
+`,e.jsx("toujou-chip",{class:"chip","chip-bg-color":"primary","chip-border-radius":"small","chip-size":"normal",children:"Jetzt 30% Rabatt"}),`
+`,e.jsx(i.h2,{id:"markup-example",children:"Markup example"}),`
+`,e.jsx(i.pre,{children:e.jsx(i.code,{className:"language-html",children:`<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="small" chip-size="normal">Jetzt 30% Rabatt</toujou-chip>
+`})}),`
+`,e.jsx(i.h2,{id:"attributes",children:"Attributes"}),`
+`,e.jsx(i.p,{children:"This element has a several attributes which allow us to customizae it to our needs"}),`
+`,e.jsxs(i.h3,{id:"background-color-required",children:["background color ",e.jsx("toujou-chip",{class:"chip","chip-bg-color":"warning",children:"required"})]}),`
+`,e.jsx(i.p,{children:"Define the background color of the chip element"}),`
+`,e.jsxs(i.p,{children:[`| Value | Example |
+|---|---|:---|
+| transparent | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="transparent" chip-border-radius="small" chip-size="normal">'}),` |
+| primary | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="small" chip-size="normal">'}),` |
+| primary-light | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary-light" chip-border-radius="small" chip-size="normal">'}),` |
+| primary-very-light | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary-very-light" chip-border-radius="small" chip-size="normal">'}),` |
+| secondary | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="secondary" chip-border-radius="small" chip-size="normal">'}),` |
+| secondary-light | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="secondary-light" chip-border-radius="small" chip-size="normal">'}),` |
+| secondary-very-light | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="secondary-very-light" chip-border-radius="small" chip-size="normal">'}),` |
+| font | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="font" chip-border-radius="small" chip-size="normal">'}),` |
+| font-light | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="font-light" chip-border-radius="small" chip-size="normal">'}),` |
+| font-very-light | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="font-very-light" chip-border-radius="small" chip-size="normal">'}),` |
+| success | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="success" chip-border-radius="small" chip-size="normal">'}),` |
+| warning | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="warning" chip-border-radius="small" chip-size="normal">'}),` |
+| error | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="error" chip-border-radius="small" chip-size="normal">'})," |"]}),`
+`,e.jsxs(i.h3,{id:"border-radius-required",children:["border radius ",e.jsx("toujou-chip",{class:"chip","chip-bg-color":"warning",children:"required"})]}),`
+`,e.jsx(i.p,{children:"Define the border-radius amount of the chip element"}),`
+`,e.jsxs(i.p,{children:[`| Value | Example |
+|---|---|:---|
+| none | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="none" chip-size="normal">'}),` |
+| small | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="small" chip-size="normal">'}),` |
+| medium | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="medium" chip-size="normal">'}),` |
+| round | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="round" chip-size="normal">'})," |"]}),`
+`,e.jsxs(i.h3,{id:"size-required",children:["size ",e.jsx("toujou-chip",{class:"chip","chip-bg-color":"warning",children:"required"})]}),`
+`,e.jsx(i.p,{children:"Define the chip element's text size"}),`
+`,e.jsxs(i.p,{children:[`| Value | Example |
+|---|---|:---|
+| extra-small | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="none" chip-size="extra-small">'}),` |
+| small | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="none" chip-size="small">'}),` |
+| normal | `,e.jsx(i.code,{children:'<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="none" chip-size="normal">'})," |"]}),`
+`,e.jsx(i.h3,{id:"is-clickable",children:"is clickable"}),`
+`,e.jsx(i.p,{children:"If this attribute is set the chip element will have a hover effect"}),`
+`,e.jsxs(i.p,{children:[`Example:
+`,e.jsx("toujou-chip",{"chip-icon-position":"left",class:"chip","chip-bg-color":"primary","chip-border-radius":"small","chip-size":"normal","is-clickable":!0,children:"Jetzt 30% Rabatt"})]}),`
+`,e.jsx(i.p,{children:"Markup Example:"}),`
+`,e.jsx(i.pre,{children:e.jsx(i.code,{className:"language-html",children:`<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="small" chip-size="normal" is-clickable="">
+`})}),`
+`,e.jsx(i.h3,{id:"icon-position",children:"icon position"}),`
+`,e.jsxs(i.p,{children:["Set if the icon should be shown on the left or right side of the text. It must be used in combination with a ",e.jsx(i.code,{children:"toujou-icon"})," element (see example)"]}),`
+`,e.jsx(i.p,{children:"Example:"}),`
+`,e.jsx("toujou-chip",{"chip-icon-position":"left",class:"chip","chip-bg-color":"primary","chip-border-radius":"small","chip-size":"normal",children:e.jsxs(i.p,{children:[e.jsx("toujou-icon",{class:"icon chip__icon","icon-name":"home"})," Jetzt 30% Rabatt"]})}),`
+`,e.jsx(i.p,{children:"Markup Example:"}),`
+`,e.jsx(i.pre,{children:e.jsx(i.code,{className:"language-html",children:`<toujou-chip chip-icon-position="left" class="chip" chip-bg-color="primary" chip-border-radius="small" chip-size="normal">
+    <toujou-icon class="icon chip__icon" icon-name="home"></toujou-icon> Jetzt 30% Rabatt
+</toujou-chip>
+`})}),`
+`,e.jsx(i.h3,{id:"is-uppercase",children:"is uppercase"}),`
+`,e.jsx(i.p,{children:"Set the chip element's text as uppercase"}),`
+`,e.jsxs(i.p,{children:[`Example:
+`,e.jsx("toujou-chip",{"chip-icon-position":"left",class:"chip","chip-bg-color":"primary","chip-border-radius":"small","chip-size":"normal","is-uppercase":!0,children:"Jetzt 30% Rabatt"})]}),`
+`,e.jsx(i.p,{children:"Markup Example:"}),`
+`,e.jsx(i.pre,{children:e.jsx(i.code,{className:"language-html",children:`<toujou-chip class="chip" chip-bg-color="primary" chip-border-radius="small" chip-size="normal" is-clickable="" is-uppercase>
+`})})]})}function h(r={}){const{wrapper:i}={...a(),...r.components};return i?e.jsx(i,{...r,children:e.jsx(c,{...r})}):c(r)}const s="Jetzt 30% Rabatt",j={title:"COMPONENTS/Chip",parameters:{docs:{page:h}},argTypes:{bgColor:{table:{category:"Chip Settings",defaultValue:{summary:"primary-very-light"}},name:"Background color",description:"Set the chip's background-color",options:["transparent","primary","primary-light","primary-very-light","secondary","secondary-light","secondary-very-light","font","font-light","font-very-light","success","warning","error"],control:{type:"select"},required:!0},borderRadius:{table:{category:"Chip Settings",defaultValue:{summary:"small"}},name:"Border radius",description:"Set the chip's border-radius",options:["none","small","medium","round"],control:{type:"select"},required:!0},size:{table:{category:"Chip Settings",defaultValue:{summary:"small"}},name:"Size",description:"Set the chip's size",options:["extra-small","small","normal"],control:{type:"select"},required:!0},isClickable:{table:{category:"Chip Settings",defaultValue:{summary:"false"}},name:"Is clickable",description:"Set if the chip is clickable or not",control:{type:"boolean"},required:!0},hasIcon:{table:{category:"Chip Settings",defaultValue:{summary:"false"}},name:"With icon",description:"Toggle the chip's icon visibility",control:{type:"boolean"},required:!0},iconPosition:{table:{category:"Chip Settings",defaultValue:{summary:"false"}},name:"Icon position",description:"Set the icon on the right or left side of the chip",options:["left","right"],control:{type:"radio"},required:!0},isUppercase:{table:{category:"Chip Settings",defaultValue:{summary:"false"}},name:"Is uppercase",description:"Set the chip's text to all uppercase",control:{type:"boolean"},required:!0}}},p=r=>{const i=document.createElement("toujou-chip");return r.hasIcon==="true"||r.hasIcon===!0?(i.innerHTML=`<toujou-icon class="icon chip__icon" icon-name="home"></toujou-icon> ${s}`,i.setAttribute("chip-icon-position",r.iconPosition)):i.textContent=s,i.classList.add("chip"),i.setAttribute("chip-bg-color",r.bgColor),i.setAttribute("chip-border-radius",r.borderRadius),i.setAttribute("chip-size",r.size),(r.isClickable==="true"||r.isClickable===!0)&&i.setAttribute("is-clickable",""),(r.isUppercase==="true"||r.isUppercase===!0)&&i.setAttribute("is-uppercase",""),i},o=p.bind({});o.args={bgColor:"primary-very-light",borderRadius:"small",size:"small",isClickable:!1,hasIcon:!1,iconPosition:"left",isUppercase:!1};var t,n,l;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`(args: ChipStoryProps) => {
+  // @ts-ignore
+  const toujouChip = document.createElement('toujou-chip');
+
+  // ICON
+  if (args.hasIcon === 'true' || args.hasIcon === true) {
+    toujouChip.innerHTML = \`<toujou-icon class="icon chip__icon" icon-name="home"></toujou-icon> \${DEFAULT_BUTTON_TEXT}\`;
+    toujouChip.setAttribute(\`chip-icon-position\`, args.iconPosition);
+  } else {
+    toujouChip.textContent = DEFAULT_BUTTON_TEXT;
+  }
+
+  // CLASSES
+  toujouChip.classList.add('chip');
+
+  // BG COLOR
+  toujouChip.setAttribute('chip-bg-color', args.bgColor);
+
+  // BORDER RADIUS
+  toujouChip.setAttribute('chip-border-radius', args.borderRadius);
+
+  // SIZE
+  toujouChip.setAttribute('chip-size', args.size);
+
+  // IS CLICKABLE
+  if (args.isClickable === 'true' || args.isClickable === true) {
+    toujouChip.setAttribute(\`is-clickable\`, '');
+  }
+
+  // ALL CAPS
+  if (args.isUppercase === 'true' || args.isUppercase === true) {
+    toujouChip.setAttribute(\`is-uppercase\`, '');
+  }
+  return toujouChip;
+}`,...(l=(n=o.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const g=["Chip"];export{o as Chip,g as __namedExportsOrder,j as default};
