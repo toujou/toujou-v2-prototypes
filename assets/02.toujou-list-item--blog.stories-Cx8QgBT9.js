b23fddc80ddb4dd6595cf0f84e1a1aa522bcaaca
LuisPato <lpato@dfau.de>
2025-11-04 06:56:12 +0100

    Updates

---
+++ assets/02.toujou-list-item--blog.stories-Cx8QgBT9.js
@@ -0,0 +1,249 @@
+import{M as c}from"./toujou-collection-item.docs-CT40bzEc.js";import{r as e}from"./_renderCardCategoriChips-D3lhH8UM.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-DeVac3uJ.js";import"./iframe-DpvgdX74.js";import"./preload-helper-C1FmrZbK.js";const p={title:"COMPONENTS/CollectionLists",parameters:{docs:{page:c}},tags:["autodocs"]},s=()=>`
+        <main>
+        <ol class="item-collection">
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            ${e(["Tutorials"],"primary")}
+                        </div>
+                        <toujou-media-info class="media-info">
+                            <button slot="open-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
+                                <toujou-icon class="icon" icon-color="background" icon-name="info" icon-size="ms"></toujou-icon>
+                            </button>
+                            <button slot="close-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
+                                <toujou-icon class="icon" icon-color="background" icon-name="close" icon-size="ms"></toujou-icon>
+                            </button>
+                            <figpaction slot="figcaption" class="media-info__figcaption">This is a beautiful description</figpaction>
+                            <small slot="copyright" class="media-info__copyright">@Nice photographer</small>
+                        </toujou-media-info>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Eine etwas längere Headline über zwei Zeilen</h3>
+                        <p class="collection-item__subtitles">
+                            <span class="collection-item__subtitle">11.03.2022</span>
+                            <span class="collection-item__subtitle">Jerôme Grad</span>
+                        </p>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel and some more text to make sure it is long enough.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            ${e(["Tutorials"],"secondary")}
+                        </div>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Headline</h3>
+                        <p class="collection-item__subtitles">
+                            <span class="collection-item__subtitle">11.03.2022</span>
+                            <span class="collection-item__subtitle">Jerôme Grad</span>
+                        </p>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            ${e(["Tutorials"],"primary")}
+                        </div>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Eine etwas längere Headline über zwei Zeilen</h3>
+                        <p class="collection-item__subtitles">
+                            <span class="collection-item__subtitle">11.03.2022</span>
+                            <span class="collection-item__subtitle">Jerôme Grad</span>
+                        </p>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            ${e(["Tutorials"],"primary")}
+                        </div>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Eine etwas längere Headline über zwei Zeilen</h3>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+        </ol>
+    </main>
+    `,i=s.bind({});var t,o,l;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return \`
+        <main>
+        <ol class="item-collection">
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            \${renderCardCategoryChips(['Tutorials'], 'primary')}
+                        </div>
+                        <toujou-media-info class="media-info">
+                            <button slot="open-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
+                                <toujou-icon class="icon" icon-color="background" icon-name="info" icon-size="ms"></toujou-icon>
+                            </button>
+                            <button slot="close-button" class="media-info__toggle" aria-label="Toggle the image caption and / or copyright">
+                                <toujou-icon class="icon" icon-color="background" icon-name="close" icon-size="ms"></toujou-icon>
+                            </button>
+                            <figpaction slot="figcaption" class="media-info__figcaption">This is a beautiful description</figpaction>
+                            <small slot="copyright" class="media-info__copyright">@Nice photographer</small>
+                        </toujou-media-info>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Eine etwas längere Headline über zwei Zeilen</h3>
+                        <p class="collection-item__subtitles">
+                            <span class="collection-item__subtitle">11.03.2022</span>
+                            <span class="collection-item__subtitle">Jerôme Grad</span>
+                        </p>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel and some more text to make sure it is long enough.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            \${renderCardCategoryChips(['Tutorials'], 'secondary')}
+                        </div>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Headline</h3>
+                        <p class="collection-item__subtitles">
+                            <span class="collection-item__subtitle">11.03.2022</span>
+                            <span class="collection-item__subtitle">Jerôme Grad</span>
+                        </p>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            \${renderCardCategoryChips(['Tutorials'], 'primary')}
+                        </div>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Eine etwas längere Headline über zwei Zeilen</h3>
+                        <p class="collection-item__subtitles">
+                            <span class="collection-item__subtitle">11.03.2022</span>
+                            <span class="collection-item__subtitle">Jerôme Grad</span>
+                        </p>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen, worauf aus DSGVO-Sicht und für den Spamschutz zu achten ist: Das erläutern wir in diesem Blogartikel.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+            <li class="item-collection__item">
+                <toujou-collection-item class="collection-item" item-type="blog" element-design="default" item-orientation="horizontal" role="article">
+                    <header class="collection-item__top">
+                        <figure class="collection-item__figure">
+                            <img src="https://picsum.photos/640" alt="nice image" class="collection-item__image">
+                        </figure>
+                        <div class="collection-item__categories">
+                            \${renderCardCategoryChips(['Tutorials'], 'primary')}
+                        </div>
+                    </header>
+                    <div class="collection-item__bottom">
+                        <h3 class="collection-item__title">Eine etwas längere Headline über zwei Zeilen</h3>
+                        <p class="collection-item__abstract">
+                            <span class="collection-item__abstract-text">
+                                Wie Sie ein Kontaktformular (und andere Onlineformulare) erstellen.
+                            </span>
+                        </p>
+                        <a href="#" class="collection-item__button">
+                            <toujou-icon class="icon" icon-name="arrow-right" icon-size="ms"></toujou-icon>
+                            zur Beschreibung
+                        </a>
+                    </div>
+                </toujou-collection-item>
+            </li>
+        </ol>
+    </main>
+    \`;
+}`,...(l=(o=i.parameters)==null?void 0:o.docs)==null?void 0:l.source}}};const g=["ListItemBlog"];export{i as ListItemBlog,g as __namedExportsOrder,p as default};
---
+++ assets/additional-info.stories-B_cN22oE.js
@@ -0,0 +1,14 @@
+import{j as n}from"./jsx-runtime-BjG_zV1W.js";import{useMDXComponents as c}from"./index-DeVac3uJ.js";import"./iframe-DpvgdX74.js";import"./preload-helper-C1FmrZbK.js";function e(o){const i={h1:"h1",p:"p",...c(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i.h1,{id:"additional-info",children:"Additional-info"}),`
+`,n.jsx(i.p,{children:"Additional information element used on several other components, for instance on the tourism tripdates"})]})}function d(o={}){const{wrapper:i}={...c(),...o.components};return i?n.jsx(i,{...o,children:n.jsx(e,{...o})}):e(o)}const f={title:"COMPONENTS/Additional Info",parameters:{docs:{page:d}},argTypes:{noMargins:{table:{category:"Additional Info Settings"},name:"No margins",description:"Render the element without any margins",control:{type:"boolean"},required:!0}},tags:["autodocs"]},m=o=>`
+        <p class="additional-info" ${o.noMargins?"no-margins":""}>
+            <toujou-icon class="icon" icon-name="campaign" icon-size="m" icon-color="font"></toujou-icon>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, assumenda commodi corporis dicta, dolorum eos exercitationem hic!
+        </p>   
+    `,t=m.bind({});t.args={noMargins:!1};var a,s,r;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`(args: AdditionalInfoProps) => {
+  return \`
+        <p class="additional-info" \${args.noMargins ? 'no-margins' : ''}>
+            <toujou-icon class="icon" icon-name="campaign" icon-size="m" icon-color="font"></toujou-icon>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, assumenda commodi corporis dicta, dolorum eos exercitationem hic!
+        </p>   
+    \`;
+}`,...(r=(s=t.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const h=["AdditionalInfo"];export{t as AdditionalInfo,h as __namedExportsOrder,f as default};
