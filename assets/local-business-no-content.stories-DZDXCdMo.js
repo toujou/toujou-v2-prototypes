a0852e6704cc01f9ca8df83be3fbe523777ed4ef
LuisPato <lpato@dfau.de>
2025-11-19 10:55:27 +0100

    Updates

---
+++ assets/local-business-no-content.stories-DZDXCdMo.js
@@ -0,0 +1,7 @@
+import{M as n}from"./local-business.docs-CsIYC247.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-f5PwCmci.js";import"./iframe-D7yF4ca0.js";import"./preload-helper-C1FmrZbK.js";const u={title:"PAGES/LocalBusiness",parameters:{docs:{page:n}}},r=()=>`
+       <h1>Local business page - no content</h1>
+    `,e=r.bind({});var o,s,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  return \`
+       <h1>Local business page - no content</h1>
+    \`;
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const l=["LocalBusinessNoContent"];export{e as LocalBusinessNoContent,l as __namedExportsOrder,u as default};
