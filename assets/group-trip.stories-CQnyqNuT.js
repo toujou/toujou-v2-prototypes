8b80da46b8cb75ca6e6ee76df0ef876e6eddd2fa
LuisPato <lpato@dfau.de>
2026-01-09 07:04:19 +0100

    Updates

--- assets/group-trip.stories-CQnyqNuT.js
+++
@@ -1,7 +0,0 @@
-import{M as t}from"./trip.docs-iAmtz8F4.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-7lKrsJoK.js";import"./iframe-Msd5TUV3.js";import"./preload-helper-C1FmrZbK.js";const u={title:"PAGES/Trip",parameters:{docs:{page:t}}},s=()=>`
-       <h1>Group trip page</h1>
-    `,r=s.bind({});var e,o,p;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`() => {
-  return \`
-       <h1>Group trip page</h1>
-    \`;
-}`,...(p=(o=r.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const d=["GroupTrip"];export{r as GroupTrip,d as __namedExportsOrder,u as default};
