13522e351f0c209f0bdc848d94f7faa4dfbac3f0
LuisPato <lpato@dfau.de>
2026-06-09 10:19:58 +0200

    feat: Install @toujou/toujou-tabs package Card: https://app.clickup.com/t/2dkv9v9

--- .storybook/componentImports/kojo-components.js
+++ .storybook/componentImports/kojo-components.js
@@ -4,7 +4,6 @@ import "../../src/components/toujou-slider/toujou-slider";
 import "../../src/components/toujou-portfolio-gallery/toujou-portfolio-gallery";
 import "../../src/components/toujou-cover-slider/toujou-cover-slider";
 import "../../src/components/toujou-language-picker-dropdown/toujou-language-picker-dropdown";
-import "../../src/components/toujou-tabs/toujou-tabs.ts";
 import "../../src/js/globals/contact-dialog";
 
 // COPIED COMPONENTS FROM TOUJOU
