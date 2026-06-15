6ae534e8978b93e60befea37fd9a5fec86011601
LuisPato <lpato@dfau.de>
2026-06-15 06:50:25 +0200

    feat: Rename mock_components into mock-components

--- .storybook/component-imports/mock-components.js
+++ .storybook/component-imports/mock-components.js
@@ -1,2 +1,2 @@
-// Mock components - see src/components/mock_components/README.md
-import "../../src/components/mock_components/toujou-fav-item/src/toujou-fav-item";
+// Mock components - see src/components/mock-components/README.md
+import "../../src/components/mock-components/toujou-fav-item/src/toujou-fav-item";
