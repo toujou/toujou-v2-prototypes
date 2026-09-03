abaa974667315d692a298db80ba397f5e398295d
LuisPato <lpato@dfau.de>
2026-09-03 09:07:49 +0200

    chore: cleanup

--- src/stories/02-elements/footer-layouts/footer-layouts.stories.ts
+++ src/stories/02-elements/footer-layouts/footer-layouts.stories.ts
@@ -22,32 +22,15 @@ export default {
 
 interface FooterLayoutsStoryProps { columnCount: string; }
 
-
 const Template: StoryFn<FooterLayoutsStoryProps> = (args: FooterLayoutsStoryProps) => {
-    const columns = [
-        {
-            name: 'outer-left',
-            title: 'Outer left',
-            id: 'c3907',
-        },
-        {
-            name: 'inner-left',
-            title: 'Inner left',
-            id: 'c3908',
-        },
-        {
-            name: 'inner-right',
-            title: 'Inner right',
-            id: 'c3909',
-        },
-        {
-            name: 'outer-right',
-            title: 'Outer right',
-            id: 'c3910',
-        },
+    const columnsData = [
+        { name: 'outer-left', title: 'Outer left', id: 'c3907'},
+        { name: 'inner-left', title: 'Inner left', id: 'c3908' },
+        { name: 'inner-right', title: 'Inner right', id: 'c3909' },
+        { name: 'outer-right', title: 'Outer right', id: 'c3910' },
     ];
 
-    const contentColumns = columns
+    const renderContentColumn = columnsData
         .slice(0, Number(args.columnCount))
         .map((column) => `
             <div class="footer-layout__column"data-column-name="${column.name}">
@@ -57,20 +40,19 @@ const Template: StoryFn<FooterLayoutsStoryProps> = (args: FooterLayoutsStoryProp
         `)
         .join('');
 
-
     return `
         <footer class="footer footer-layout" data-footer-layout="fourColumns" data-footer-column-count="${args.columnCount}">
             <div class="footer-layout__content">
                 <div class="footer-layout__row" data-row-name="intro">
                     <div class="footer-layout__column" data-column-name="intro">
                         <span id="c3906"></span>
-                        <h3>Test footer page</h3>
+                        <h3>Intro</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
             
                 <div class="footer-layout__row" data-row-name="content">
-                    ${contentColumns}
+                    ${renderContentColumn}
                 </div>
             
                 <div class="footer-layout__row" data-row-name="end">
