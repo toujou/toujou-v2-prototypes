70d14c5be73c4b4e74df3a10aba79f1a4bbccd4a
LuisPato <lpato@dfau.de>
2026-09-22 08:32:26 +0200

    Updates

---
+++ assets/08.datepicker.stories-BL8cyMTC.js
@@ -0,0 +1,85 @@
+const p={"YYYY.MM.DD":"Y.m.d","YYYY/MM/DD":"Y/m/d","DD.MM.YYYY":"d.m.Y","DD/MM/YYYY":"d/m/Y"},m={single:"single",range:"range"},u={title:"COMPONENTS/Forms",argTypes:{dateFormat:{name:"Date format",description:"Set the date format",options:Object.values(p),control:{type:"radio"},required:!0},minDate:{name:"Earliest date",description:"Earliest selectable date",control:{type:"date"}},maxDate:{name:"Latest date",description:"Latest selectable date",control:{type:"date"}},showTime:{name:"Show time",description:"Show time on the datepicker",control:{type:"boolean"},required:!0},type:{name:"Type",description:"Set the input type",options:Object.values(m),control:{type:"radio"},required:!0}}};function o(e){if(isNaN(e))return"";const t=new Date(e);if(isNaN(t.getTime()))return"";const r=t.getFullYear(),c=(t.getMonth()+1).toString().padStart(2,"0"),d=t.getDate().toString().padStart(2,"0");return`${r}-${c}-${d}`}const l=e=>{const t=o(e.minDate),r=o(e.maxDate);return`
+        <link rel="stylesheet" type="text/css" href="../../../../node_modules/flatpickr/dist/themes/light.css">
+
+        <style>
+            body {
+                background-color: var(--color-bg);
+            }
+        </style>
+        <form
+            enctype="multipart/form-data"
+            method="post"
+            class="form"
+            id="testform-1000091"
+            action="#"
+            novalidate>
+
+            <toujou-input-group class="input-group input-group--date-jquery" data-format="Y-m-d">
+                <label class="input-label" for="testform-1000091-datepicker-2">Date picker (jQuery)</label>
+                <span class="input-description">This is a description</span>
+
+                 <toujou-datepicker
+                    class="datepicker"
+                    date-format="${e.dateFormat}"
+                    minDate="${t}"
+                    maxDate="${r}"
+                    mode="${e.type}"
+                    ${e.showTime?"enableTime":""}
+                >
+                    <input
+                        name="dateFrom"
+                        class="form-control input input--datepicker"
+                        placeholder="Select a date"
+                        type="text"
+                    />
+                </toujou-datepicker>
+
+               <span class="pristine-error form__error">This is an error message!</span>
+            </toujou-input-group>
+
+        </form>
+    `},a=l.bind({});a.args={dateFormat:p["YYYY.MM.DD"],minDate:Date.now(),maxDate:Date.now()+864e5*365,showTime:!1,type:m.single};var n,s,i;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`(args: DatepickerProps) => {
+  const formattedMinDate = formatTimestamp(args.minDate);
+  const formattedMaxDate = formatTimestamp(args.maxDate);
+  return \`
+        <link rel="stylesheet" type="text/css" href="../../../../node_modules/flatpickr/dist/themes/light.css">
+
+        <style>
+            body {
+                background-color: var(--color-bg);
+            }
+        </style>
+        <form
+            enctype="multipart/form-data"
+            method="post"
+            class="form"
+            id="testform-1000091"
+            action="#"
+            novalidate>
+
+            <toujou-input-group class="input-group input-group--date-jquery" data-format="Y-m-d">
+                <label class="input-label" for="testform-1000091-datepicker-2">Date picker (jQuery)</label>
+                <span class="input-description">This is a description</span>
+
+                 <toujou-datepicker
+                    class="datepicker"
+                    date-format="\${args.dateFormat}"
+                    minDate="\${formattedMinDate}"
+                    maxDate="\${formattedMaxDate}"
+                    mode="\${args.type}"
+                    \${args.showTime ? 'enableTime' : ''}
+                >
+                    <input
+                        name="dateFrom"
+                        class="form-control input input--datepicker"
+                        placeholder="Select a date"
+                        type="text"
+                    />
+                </toujou-datepicker>
+
+               <span class="pristine-error form__error">This is an error message!</span>
+            </toujou-input-group>
+
+        </form>
+    \`;
+}`,...(i=(s=a.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const f=["DatePicker"];export{a as DatePicker,f as __namedExportsOrder,u as default};
